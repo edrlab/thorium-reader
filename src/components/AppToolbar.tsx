a272373622b0d61e7b419dbaf2d493e51e9be0fa
Scarsnik <leo.stefani@wanadoo.fr>
2017-04-26 15:02:49 +0200

    addition of the menu with doc and news

--- src/components/AppToolbar.tsx
+++ src/components/AppToolbar.tsx
@@ -8,13 +8,21 @@ import IconMenu from "material-ui/IconMenu";
 import MenuItem from "material-ui/MenuItem";
 import { blue500 } from "material-ui/styles/colors";
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from "material-ui/Toolbar";
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 import { setLocale } from "../actions/i18n";
 import { lazyInject } from "../di";
 import { IAppState } from "../reducers/app";
+import { Translator } from "../i18n/translator";
+
+var fs = require('fs');
+var commonmark = require('commonmark');
 
 interface AppToolbarState {
     locale: string;
+    open: boolean;
+    dialogContent: string;
 }
 
 const styles = {
@@ -40,53 +48,110 @@ export default class AppToolbar extends React.Component<undefined, AppToolbarSta
     @lazyInject("store")
     private store: Store<IAppState>;
 
+    @lazyInject(Translator)
+    private translator: Translator;
+
     constructor() {
         super();
         this.state = {
             locale: this.store.getState().i18n.locale,
+            open: false,
+            dialogContent: undefined
         };
 
         this.handleLocaleChange = this.handleLocaleChange.bind(this);
     }
 
+    public componentDidMount() {
+        this.store.subscribe(() => {
+            this.setState({
+                locale: this.store.getState().i18n.locale,
+            });
+        });
+    }
+
     public render(): React.ReactElement<{}>  {
+        const __ = this.translator.translate;
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={this.handleClose}
+            />
+        ];
+
+        var that = this;
+        var helpUrl = "./src/resources/docs/" + this.state.locale + "/help.md";
+        var aboutUrl = "./src/resources/docs/" + this.state.locale + "/about.md";
+
         return (
-            <Toolbar>
-                <ToolbarGroup firstChild={true}>
-                    <DropDownMenu value={this.state.locale} onChange={this.handleLocaleChange}>
-                        <MenuItem value="en" primaryText="English" />
-                        <MenuItem value="fr" primaryText="French" />
-                    </DropDownMenu>
-                </ToolbarGroup>
-                <ToolbarGroup>
-                    <ToolbarTitle text="Options" />
-                    <FontIcon className="muidocs-icon-custom-sort" />
-                    <ToolbarSeparator />
-                    <IconButton touch={true}>
-                        <FontIcon
-                            className="fa fa-plus-circle"
-                            style={styles.iconButton}
-                            color={blue500}>
-                            <input type="file" style={styles.fileInput} />
-                        </FontIcon>
-                    </IconButton>
-                    <IconMenu
-                        iconButtonElement={
-                            <IconButton touch={true}>
-                                <FontIcon
-                                    className="fa fa-home"
-                                    style={styles.iconButton}
-                                    color={blue500} />
-                            </IconButton>
-                        }
+
+            <div>
+                <Toolbar>
+                    <ToolbarGroup firstChild={true}>
+                        <DropDownMenu value={this.state.locale} onChange={this.handleLocaleChange}>
+                            <MenuItem value="en" primaryText="English" />
+                            <MenuItem value="fr" primaryText="French" />
+                        </DropDownMenu>
+                    </ToolbarGroup>
+                    <ToolbarGroup>
+                        <ToolbarTitle text="Options" />
+                        <FontIcon className="muidocs-icon-custom-sort" />
+                        <ToolbarSeparator />
+                        <IconButton touch={true}>
+                            <FontIcon
+                                className="fa fa-plus-circle"
+                                style={styles.iconButton}
+                                color={blue500}>
+                                <input type="file" style={styles.fileInput} />
+                            </FontIcon>
+                        </IconButton>
+                        <IconMenu
+                            iconButtonElement={
+                                <IconButton touch={true}>
+                                    <FontIcon
+                                        className="fa fa-ellipsis-v"
+                                        style={styles.iconButton}
+                                        color={blue500} />
+                                </IconButton>
+                            }
+                        >
+                            <MenuItem primaryText= {__("toolbar.help")} onClick={function(){that.handleOpen(helpUrl)}} leftIcon={<FontIcon className="fa fa-question-circle" color={blue500} />} />
+                            <MenuItem primaryText={__("toolbar.news")} onClick={function(){that.handleOpen(aboutUrl)}} leftIcon={<FontIcon className="fa fa-gift" color={blue500} />} />
+                            <MenuItem primaryText={__("toolbar.sync")} leftIcon={<FontIcon className="fa fa-refresh" color={blue500} />} />
+                        </IconMenu>
+                    </ToolbarGroup>
+                </Toolbar>
+
+                <Dialog
+                    title="Dialog With Actions"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
                     >
-                        <MenuItem primaryText="Download" />
-                        <MenuItem primaryText="More Info" />
-                    </IconMenu>
-                </ToolbarGroup>
-            </Toolbar>
+                    {this.state.dialogContent}
+                </Dialog>
+            </div>
         );
     }
+
+    handleOpen = (url:string) => {
+        var content = fs.readFileSync(url).toString()
+        var reader = new commonmark.Parser();
+        var writer = new commonmark.HtmlRenderer();
+
+        var parsed = reader.parse(content);
+        var result = writer.render(parsed);
+
+        this.setState({open: true});
+        this.setState({dialogContent : result});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
     private handleLocaleChange(event: any, index: any, locale: string) {
         this.store.dispatch(setLocale(locale));
     }
---
+++ src/resources/docs/fr/help.md
@@ -0,0 +1,6 @@
+## Comment ajouter un livre :
+
+Pour ajouter un livre il faut :
+- cliquer sur le bouton 'Ajouter à la bibliothèque'
+- dans la fenetre qui s'ouvre, choisir le fichier correspondant
+- cliquer sur ajouter
