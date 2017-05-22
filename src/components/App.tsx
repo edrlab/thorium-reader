e9048e8e96b851f011da524cb2ecb4dea3ab8588
Scarsnik <leo.stefani@wanadoo.fr>
2017-05-22 12:39:44 +0200

    retrieve of the catalog in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,36 @@
 import * as React from "react";
 
+import { ipcRenderer } from "electron";
+
 import { lightBaseTheme, MuiThemeProvider } from "material-ui/styles";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 import { Store } from "redux";
 
+
 import { lazyInject } from "readium-desktop/renderer/di";
+
+import { CATALOG_GET_RESPONSE } from "readium-desktop/events/ipc";
+import { Catalog } from "readium-desktop/models/catalog";
+import { CatalogMessage } from "readium-desktop/models/ipc";
 
 import { setLocale } from "readium-desktop/actions/i18n";
 import { Translator } from "readium-desktop/i18n/translator";
-import { IAppState } from "readium-desktop/reducers/app";
+import { IAppState as IViewState} from "readium-desktop/reducers/app";
 
 import AppToolbar from "readium-desktop/components/AppToolbar";
 import Library from "readium-desktop/components/Library";
 
+interface IAppState {
+    catalog: Catalog;
+}
+
 const lightMuiTheme = getMuiTheme(lightBaseTheme);
 const defaultLocale = "fr";
 
-export default class App extends React.Component<undefined, undefined> {
+export default class App extends React.Component<undefined, IAppState> {
     @lazyInject("store")
-    private store: Store<IAppState>;
+    private store: Store<IViewState>;
 
     @lazyInject("translator")
     private translator: Translator;
@@ -33,6 +44,12 @@ export default class App extends React.Component<undefined, undefined> {
         }
 
         this.translator.setLocale(locale);
+
+        this.state = {
+            catalog: undefined,
+        };
+
+        this.retrieveCatalog();
     }
 
     public componentDidMount() {
@@ -41,12 +58,18 @@ export default class App extends React.Component<undefined, undefined> {
         });
     }
 
+    public retrieveCatalog() {
+        ipcRenderer.on(CATALOG_GET_RESPONSE, (event: any, msg: CatalogMessage) => {
+            this.setState({catalog: msg.catalog});
+        });
+    }
+
     public render(): React.ReactElement<{}> {
         return (
             <MuiThemeProvider muiTheme={lightMuiTheme}>
                 <div>
                     <AppToolbar />
-                    <Library />
+                    <Library catalog={this.state.catalog}/>
                 </div>
             </MuiThemeProvider>
         );
--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -2,7 +2,6 @@ import * as fs from "fs";
 
 import * as React from "react";
 import { Store } from "redux";
-import { ipcRenderer } from "electron";
 
 import { Card, CardMedia, CardTitle} from "material-ui/Card";
 import FlatButton from "material-ui/FlatButton";
@@ -15,26 +14,21 @@ import { lazyInject } from "readium-desktop/renderer/di";
 
 import { Translator } from "readium-desktop/i18n/translator";
 import { IAppState } from "readium-desktop/reducers/app";
-import {
-    CATALOG_GET_REQUEST,
-    CATALOG_GET_RESPONSE
-} from "readium-desktop/events/ipc";
-import {
-    CatalogMessage
-} from "readium-desktop/models/ipc";
+
 import { Catalog } from "readium-desktop/models/catalog";
 
 import * as ReactCardFlip from "react-card-flip";
 
 import * as request from "request";
 
-import { default as Parser } from "opds-feed-parser";
-
 interface ILibraryState {
     locale: string;
     list: boolean;
-    catalog: Catalog;
     isFlipped: boolean[];
+}
+
+interface ILibraryProps {
+    catalog: Catalog;
 }
 
 const styles = {
@@ -106,6 +100,10 @@ const styles = {
         title: {
             display: "inline-block",
         },
+        spinner: {
+            top: "200px",
+            fontSize: "40px",
+        },
     },
 };
 
@@ -123,13 +121,10 @@ function downloadEPUB(url: string, title: string) {
             fileName = fileName + "(" + i + ")";
         }
         let file = fs.createWriteStream("./epubs/" + fileName + ".epub");
-        request.get(url).on("response", (response) => {
-
-        })
-        .pipe(file);
+        request.get(url).pipe(file);
 }
 
-export default class Library extends React.Component<undefined, ILibraryState> {
+export default class Library extends React.Component<ILibraryProps, ILibraryState> {
     public state: ILibraryState;
 
     @lazyInject("translator")
@@ -138,15 +133,15 @@ export default class Library extends React.Component<undefined, ILibraryState> {
     @lazyInject("store")
     private store: Store<IAppState>;
 
+    private catalog: Catalog;
+
     constructor() {
         super();
         this.state = {
-            catalog: undefined,
             isFlipped: [],
             list: false,
             locale: this.store.getState().i18n.locale,
         };
-        this.handleSync();
     }
 
     public handleFront(id: any) {
@@ -164,6 +159,7 @@ export default class Library extends React.Component<undefined, ILibraryState> {
     public Spinner () {
         return (
             <FontIcon
+                style = {styles.Library.spinner}
                 className="fa fa-spinner fa-spin fa-3x fa-fw"
                 color={blue500}
             />
@@ -255,7 +251,7 @@ export default class Library extends React.Component<undefined, ILibraryState> {
 
     public createCardList() {
         let list: any = [];
-        let catalog = this.state.catalog;
+        let catalog = this.catalog;
         for (let i = 0; i < catalog.publications.length; i++) {
             let newAuthor: string = "";
             let newImage: string = "";
@@ -285,7 +281,7 @@ export default class Library extends React.Component<undefined, ILibraryState> {
 
     public createElementList() {
         let list: any = [];
-        let catalogs = this.state.catalog;
+        let catalogs = this.catalog;
         for (let i = 0; i < catalogs.publications.length; i++) {
             let newAuthor: string = "";
             let newdownloadUrl: string = "";
@@ -323,9 +319,10 @@ export default class Library extends React.Component<undefined, ILibraryState> {
     public render(): React.ReactElement<{}>  {
         const __ = this.translator.translate;
         const that = this;
+        this.catalog = this.props.catalog;
 
         let listToDisplay: any;
-        if (this.state.catalog) {
+        if (this.catalog) {
             if (this.state.list) {
                 listToDisplay = this.createElementList();
             } else {
@@ -363,11 +360,5 @@ export default class Library extends React.Component<undefined, ILibraryState> {
                 </div>
             </div>
         );
-    }
-
-    private handleSync() {
-        ipcRenderer.on(CATALOG_GET_RESPONSE, (event: any, msg: CatalogMessage) => {
-            this.setState({catalog: msg.catalog});
-        });
     }
 }
