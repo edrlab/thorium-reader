9bafd959a037307150a6c6ef8a74264e41d05f88
Cyrille Lebeaupin <clebeaupin@noop.fr>
2017-06-19 10:51:44 +0200

    Optimize load of streamer
    1. Start streamer only if one reader is open
    2. Close streamer if all readers are closed
    3. Remove a publication from streamer if there are no reader pointing on this publication

--- src/main/sagas/index.ts
+++ src/main/sagas/index.ts
@@ -1,6 +1,6 @@
 import {
-    watchDownloadStart,
     watchDownloadFinish,
+    watchDownloadStart,
 } from "readium-desktop/sagas/downloader";
 
 import {
@@ -15,6 +15,7 @@ import {
     watchRendererCatalogRequest,
 } from "readium-desktop/main/sagas/catalog";
 import {
+    watchPublicationCloseRequest,
     watchStreamManifestCloseRequest,
     watchStreamManifestOpenRequest,
 } from "readium-desktop/main/sagas/streamer";
@@ -31,5 +32,6 @@ export function* rootSaga() {
         watchPublicationDownloadUpdate(),
         watchStreamManifestOpenRequest(),
         watchStreamManifestCloseRequest(),
+        watchPublicationCloseRequest(),
     ];
 }
