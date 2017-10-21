0c6ae7e02f10129e5509783414f9800216ec7f1b
danielweck <daniel.weck@gmail.com>
2017-10-21 20:50:40 +0100

    added missing initGlobals() for the r2-streamer-js (registers special (de)serialization modules for TA-JSON), added ReadiumCSS express HTTP server route

--- src/main/streamer.ts
+++ src/main/streamer.ts
@@ -1,5 +1,30 @@
+import * as express from "express";
+import { initGlobals } from "r2-streamer-js/dist/es6-es2015/src/init-globals";
 import { Server } from "r2-streamer-js/dist/es6-es2015/src/http/server";
+
+initGlobals();
 
 // Create readium2 streamer
 // This streamer is used to stream epub content to the renderer
-export const streamer: Server = new Server();
+export const streamer: Server = new Server({
+    disableDecryption: false,
+    disableReaders: true,
+});
+
+// https://expressjs.com/en/4x/api.html#express.static
+const staticOptions = {
+    dotfiles: "ignore",
+    etag: true,
+    fallthrough: false,
+    immutable: true,
+    index: false,
+    maxAge: "1d",
+    redirect: false,
+    // extensions: ["css", "otf"],
+    // setHeaders: function (res, path, stat) {
+    //   res.set('x-timestamp', Date.now())
+    // }
+};
+streamer.expressUse("/readium-css",
+    express.static("node_modules/r2-streamer-js/dist/ReadiumCSS",
+        staticOptions));
