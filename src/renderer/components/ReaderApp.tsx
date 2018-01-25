868f17a7e00802debfd6f2684fba5cd2140add6f
danielweck <daniel.weck@gmail.com>
2018-01-25 17:06:58 +0000

    added working LCP / LSD support, but totally ugly (ReactUI just for testing commands)

--- src/renderer/components/ReaderApp.tsx
+++ src/renderer/components/ReaderApp.tsx
@@ -77,6 +77,8 @@ const electronStore: IStore = new StoreElectron("readium2-testapp", {
 
 const electronStoreLCP: IStore = new StoreElectron("readium2-testapp-lcp", {});
 
+const electronStoreLSD: IStore = new StoreElectron("readium2-testapp-lsd", {});
+
 initGlobals();
 
 const computeReadiumCssJsonMessage = (): string => {
@@ -183,10 +185,24 @@ electronStore.onChanged("styling.readiumcss", (newValue: any, oldValue: any) => 
     }
 });
 
+ipcRenderer.on(R2_EVENT_LCP_LSD_RENEW_RES, (_event: any, okay: boolean, msg: string) => {
+    console.log("R2_EVENT_LCP_LSD_RENEW_RES");
+    console.log(okay);
+    console.log(msg);
+});
+
+ipcRenderer.on(R2_EVENT_LCP_LSD_RETURN_RES, (_event: any, okay: boolean, msg: string) => {
+    console.log("R2_EVENT_LCP_LSD_RETURN_RES");
+    console.log(okay);
+    console.log(msg);
+});
+
 interface ReaderAppState {
-    openManifestUrl?: string;
-    openPublication?: Publication;
-    title?: string,
+    publicationJsonUrl?: string;
+    lcpHint?: string;
+    r2Publication?: Publication;
+    title?: string;
+    lcpPass?: string;
 }
 
 const lightMuiTheme = getMuiTheme(lightBaseTheme);
@@ -210,108 +226,111 @@ export default class ReaderApp extends React.Component<undefined, ReaderAppState
         this.translator.setLocale(locale);
 
         this.state = {
-            openManifestUrl: undefined,
-            openPublication: undefined,
+            publicationJsonUrl: "HTTP://URL",
+            lcpHint: "LCP hint",
+            r2Publication: undefined,
+            title: "TITLE",
+            lcpPass: "LCP pass",
         };
     }
 
-    private loadPublicationIntoViewport() {
-        console.log(window.location.search);
-        console.log(publicationJsonUrl);
-        console.log(this.state);
-        console.log(this.store.getState());
+    private async loadPublicationIntoViewport() {
+        // // tslint:disable-next-line:no-floating-promises
+        // (async () => {
+        // })();
 
-        // tslint:disable-next-line:no-floating-promises
-        (async () => {
+        let response: Response;
+        try {
+            response = await fetch(publicationJsonUrl);
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+        if (!response.ok) {
+            console.log("BAD RESPONSE?!");
+        }
+        // response.headers.forEach((arg0: any, arg1: any) => {
+        //     console.log(arg0 + " => " + arg1);
+        // });
 
-            let response: Response;
-            try {
-                response = await fetch(publicationJsonUrl);
-            } catch (e) {
-                console.log(e);
-                return;
+        let _publicationJSON: any | undefined;
+        try {
+            _publicationJSON = await response.json();
+        } catch (e) {
+            console.log(e);
+        }
+        if (!_publicationJSON) {
+            return;
+        }
+        // const pubJson = global.JSON.parse(publicationStr);
+
+        console.log(_publicationJSON);
+
+        // let _publication: Publication | undefined;
+        const _publication = TAJSON.deserialize<R2Publication>(_publicationJSON, R2Publication);
+
+        if (_publication.Metadata && _publication.Metadata.Title) {
+            // TODO: should get language from view state? (user preferences)
+            const lang = "en";
+            const title = getTitleString( _publication.Metadata.Title, lang);
+
+            // let title: string | undefined;
+            // if (typeof _publication.Metadata.Title === "string") {
+            //     title = _publication.Metadata.Title;
+            // } else {
+            //     const keys = Object.keys(_publication.Metadata.Title as IStringMap);
+            //     if (keys && keys.length) {
+            //         title = (_publication.Metadata.Title as IStringMap)[keys[0]];
+            //     }
+            // }
+
+            if (title) {
+                console.log(title);
+                window.document.title = "Readium2 [ " + title + "]";
+                this.setState({
+                    title,
+                });
             }
-            if (!response.ok) {
-                console.log("BAD RESPONSE?!");
-            }
-            // response.headers.forEach((arg0: any, arg1: any) => {
-            //     console.log(arg0 + " => " + arg1);
-            // });
+        }
 
-            let _publicationJSON: any | undefined;
-            try {
-                _publicationJSON = await response.json();
-            } catch (e) {
-                console.log(e);
-            }
-            if (!_publicationJSON) {
-                return;
-            }
-            // const pubJson = global.JSON.parse(publicationStr);
+        if (_publication.Spine && _publication.Spine.length) {
+            console.log(_publication.Spine);
+        }
+        if (_publication.TOC && _publication.TOC.length) {
+        }
+        if (_publication.PageList && _publication.PageList.length) {
+        }
+        if (_publication.Landmarks && _publication.Landmarks.length) {
+        }
+        if (_publication.LOT && _publication.LOT.length) {
+        }
+        if (_publication.LOI && _publication.LOI.length) {
+        }
+        if (_publication.LOV && _publication.LOV.length) {
+        }
+        if (_publication.LOA && _publication.LOA.length) {
+        }
 
-            console.log(_publicationJSON);
-
-            // let _publication: Publication | undefined;
-            const _publication = TAJSON.deserialize<R2Publication>(_publicationJSON, R2Publication);
-
-            if (_publication.Metadata && _publication.Metadata.Title) {
-                // TODO: should get language from view state? (user preferences)
-                const lang = "en";
-                const title = getTitleString( _publication.Metadata.Title, lang);
-
-                // let title: string | undefined;
-                // if (typeof _publication.Metadata.Title === "string") {
-                //     title = _publication.Metadata.Title;
-                // } else {
-                //     const keys = Object.keys(_publication.Metadata.Title as IStringMap);
-                //     if (keys && keys.length) {
-                //         title = (_publication.Metadata.Title as IStringMap)[keys[0]];
-                //     }
-                // }
-
-                if (title) {
-                    console.log(title);
-                    this.setState({
-                        title,
-                    });
+        const readStore = electronStore.get("readingLocation");
+        let pubDocHrefToLoad: string | undefined;
+        let pubDocSelectorToGoto: string | undefined;
+        if (readStore) {
+            const obj = readStore[pathDecoded];
+            if (obj && obj.doc) {
+                pubDocHrefToLoad = obj.doc;
+                if (obj.loc) {
+                    pubDocSelectorToGoto = obj.loc;
                 }
             }
+        }
 
-            if (_publication.Spine && _publication.Spine.length) {
-                console.log(_publication.Spine);
-            }
-            if (_publication.TOC && _publication.TOC.length) {
-            }
-            if (_publication.PageList && _publication.PageList.length) {
-            }
-            if (_publication.Landmarks && _publication.Landmarks.length) {
-            }
-            if (_publication.LOT && _publication.LOT.length) {
-            }
-            if (_publication.LOI && _publication.LOI.length) {
-            }
-            if (_publication.LOV && _publication.LOV.length) {
-            }
-            if (_publication.LOA && _publication.LOA.length) {
-            }
-
-            const readStore = electronStore.get("readingLocation");
-            let pubDocHrefToLoad: string | undefined;
-            let pubDocSelectorToGoto: string | undefined;
-            if (readStore) {
-                const obj = readStore[pathDecoded];
-                if (obj && obj.doc) {
-                    pubDocHrefToLoad = obj.doc;
-                    if (obj.loc) {
-                        pubDocSelectorToGoto = obj.loc;
-                    }
-                }
-            }
-
-            let preloadPath = "./preload.js";
+        let preloadPath = "preload.js";
+        if (__PACKAGING__ === "1") {
+            preloadPath = path.normalize(path.join("file://", __dirname, preloadPath));
+        } else {
             preloadPath = "r2-navigator-js/dist/" +
-                "es6-es2015" +
-                "/src/electron/renderer/webview/preload.js";
+            "es6-es2015" +
+            "/src/electron/renderer/webview/preload.js";
 
             if (__RENDERER_BASE_URL__ === "file://") {
                 // dist/prod mode (without WebPack HMR Hot Module Reload HTTP server)
@@ -321,34 +340,110 @@ export default class ReaderApp extends React.Component<undefined, ReaderAppState
                 // readerUrl = readerUrl.replace(":8080", ":8081");
                 preloadPath = path.normalize(path.join("file://", process.cwd(), "node_modules", preloadPath));
             }
+        }
 
-            preloadPath = preloadPath.replace(/\\/g, "/");
-            console.log(preloadPath);
+        preloadPath = preloadPath.replace(/\\/g, "/");
+        console.log(preloadPath);
 
-            installNavigatorDOM(_publication, publicationJsonUrl,
-                "publication_viewport",
-                preloadPath,
-                pubDocHrefToLoad, pubDocSelectorToGoto);
-        })();
+        installNavigatorDOM(_publication, publicationJsonUrl,
+            "publication_viewport",
+            preloadPath,
+            pubDocHrefToLoad, pubDocSelectorToGoto);
     }
 
-    public componentDidMount() {
+    private showLcpDialog(message?: string) {
+
+        console.log(lcpHint);
+        if (message) {
+            console.log(message);
+        }
+
+        setTimeout(() => {
+            ipcRenderer.send(R2_EVENT_TRY_LCP_PASS, pathDecoded, this.state.lcpPass, false);
+        }, 3000);
+    }
+
+    public async componentDidMount() {
+        console.log(window.location.search);
+        console.log(publicationJsonUrl);
+        console.log(lcpHint);
+
+        this.setState({
+            publicationJsonUrl: publicationJsonUrl,
+        });
+
+        if (lcpHint) {
+            this.setState({
+                lcpHint: lcpHint,
+                lcpPass: this.state.lcpPass + " [" + lcpHint + "]",
+            });
+        }
+
+        console.log(this.state);
+        console.log(this.store.getState());
+
         // this.store.dispatch(windowActions.init());
         this.store.subscribe(() => {
             const storeState = this.store.getState();
             console.log("storeState (INDEX READER):");
             console.log(storeState);
 
-            this.setState({
-                title: "",
-                openManifestUrl: storeState.reader.manifestUrl,
-                openPublication: storeState.reader.publication,
-            });
+            // this.setState({
+            // });
 
             this.translator.setLocale(this.store.getState().i18n.locale);
         });
 
-        this.loadPublicationIntoViewport();
+        ipcRenderer.on(R2_EVENT_TRY_LCP_PASS_RES, async (_event: any, okay: boolean, msg: string, passSha256Hex: string) => {
+
+            if (!okay) {
+                setTimeout(() => {
+                    this.showLcpDialog(msg);
+                }, 500);
+
+                return;
+            }
+
+            const lcpStore = electronStoreLCP.get("lcp");
+            if (!lcpStore) {
+                const lcpObj: any = {};
+                const pubLcpObj: any = lcpObj[pathDecoded] = {};
+                pubLcpObj.sha = passSha256Hex;
+
+                electronStoreLCP.set("lcp", lcpObj);
+            } else {
+                const pubLcpStore = lcpStore[pathDecoded];
+                if (pubLcpStore) {
+                    pubLcpStore.sha = passSha256Hex;
+                } else {
+                    lcpStore[pathDecoded] = {
+                        sha: passSha256Hex,
+                    };
+                }
+                electronStoreLCP.set("lcp", lcpStore);
+            }
+
+            await this.loadPublicationIntoViewport();
+        });
+
+        if (lcpHint) {
+
+            let lcpPassSha256Hex: string | undefined;
+            const lcpStore = electronStoreLCP.get("lcp");
+            if (lcpStore) {
+                const pubLcpStore = lcpStore[pathDecoded];
+                if (pubLcpStore && pubLcpStore.sha) {
+                    lcpPassSha256Hex = pubLcpStore.sha;
+                }
+            }
+            if (lcpPassSha256Hex) {
+                ipcRenderer.send(R2_EVENT_TRY_LCP_PASS, pathDecoded, lcpPassSha256Hex, true);
+            } else {
+                this.showLcpDialog();
+            }
+        } else {
+            await this.loadPublicationIntoViewport();
+        }
     }
 
     public render(): React.ReactElement<{}> {
@@ -380,6 +475,33 @@ export default class ReaderApp extends React.Component<undefined, ReaderAppState
                         label="Night"
                         onClick={()=>{
                             electronStore.set("styling.night", !electronStore.get("styling.night"));
+                        }}
+                    />
+                    <FlatButton
+                        label="Open settings"
+                        onClick={()=>{
+                            if ((electronStore as any).reveal) {
+                                (electronStore as any).reveal();
+                            }
+                            if ((electronStoreLCP as any).reveal) {
+                                (electronStoreLCP as any).reveal();
+                            }
+                            if ((electronStoreLSD as any).reveal) {
+                                (electronStoreLSD as any).reveal();
+                            }
+                        }}
+                    />
+                    <input type="text" value={this.state.lcpPass} onChange={(event) => {  this.setState({lcpPass: event.target.value});}} size={40} />
+                    <FlatButton
+                        label="LSD Renew"
+                        onClick={()=>{
+                            ipcRenderer.send(R2_EVENT_LCP_LSD_RENEW, pathDecoded, ""); // no explicit end date
+                        }}
+                    />
+                    <FlatButton
+                        label="LSD Return"
+                        onClick={()=>{
+                            ipcRenderer.send(R2_EVENT_LCP_LSD_RETURN, pathDecoded);
                         }}
                     />
                     <div id="publication_viewport" style={Styles.Reader.publicationViewport}> </div>
