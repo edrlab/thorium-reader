ecfd45e2e2475e31071bb8f1acfec0038dd66b3a
Cyrille Lebeaupin <clebeaupin@noop.fr>
2018-11-30 14:35:32 +0100

    Fix library header active links
    Detect active links using react router

--- src/renderer/components/layout/LibraryHeader.tsx
+++ src/renderer/components/layout/LibraryHeader.tsx
@@ -1,36 +1,76 @@
 import * as React from "react";
 
+import * as classNames from "classnames";
+
+import { withRouter } from "react-router";
+
+import { Link } from "react-router-dom";
+
+import { RouteComponentProps } from "react-router-dom";
+
+import { withTranslator, TranslatorProps } from "readium-desktop/renderer/components/utils/translator";
+
 import * as styles from "readium-desktop/renderer/assets/styles/header.css";
 
-import { Translator } from "readium-desktop/common/services/translator";
-import { lazyInject } from "readium-desktop/renderer/di";
+const headerNav = [
+    {
+        route: "/",
+        label: "Mes livres",
+        matchRoutes: ["/", "/library"],
+        styles: [],
+    },
+    {
+        route: "/settings",
+        label: "Préférences",
+        matchRoutes: ["/settings"],
+        styles: [styles.preferences],
+    }
+];
 
-import { Link, HashRouterProps } from "react-router-dom";
+export interface HeaderProps extends RouteComponentProps, TranslatorProps { };
 
-export default class Header extends React.Component<HashRouterProps, undefined> {
+export class Header extends React.Component<HeaderProps, undefined> {
     public render(): React.ReactElement<{}> {
-        const activePage = 0 as number;
-
         return (
             <nav className={styles.main_navigation} role="navigation" aria-label="Menu principal">
                 <ul>
-                    <li {...(activePage === 0 && {className: styles.active})}>
-                        <Link to={"/"} replace={true}>
-                            Mes Livres
-                        </Link>
-                    </li>
-                    {/* <li  {...(activePage === 1 && {className: styles.active})}>
-                        <Link to={"/catalog"} replace={true}>
-                            Catalogues
-                        </Link>
-                    </li> */}
-                    <li className={styles.preferences + (activePage === 2 ? " " + styles.active : "")}>
-                        <Link to={"/settings"} replace={true}>
-                            Préférences
-                        </Link>
-                    </li>
+                    {
+                        headerNav.map((item, index: number) => {
+                            return this.buildNavItem(item, index);
+                        })
+                    }
                 </ul>
             </nav>
         );
     }
+
+    private buildNavItem(item: any, index: number) {
+        if (!this.props.location) {
+            return (<></>);
+        }
+
+        let active = false;
+        let styleClasses = [];
+        const pathname = this.props.match.path;
+
+        for (const matchRoute of item.matchRoutes) {
+            if (pathname == item.route) {
+                active = true;
+                styleClasses.push(styles.active);
+                break;
+            }
+        }
+
+        styleClasses.concat(item.styles);
+
+        return (
+            <li className={classNames(...styleClasses)} key={ index }>
+                <Link to={ item.route } replace={true}>
+                    { this.props.__(item.label) }
+                </Link>
+            </li>
+        );
+    }
 }
+
+export default withTranslator(withRouter(Header));
