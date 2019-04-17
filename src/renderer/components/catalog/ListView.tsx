2d011e0aea72f7faf1d5435aa2bd9ab5dc347ea5
manuellagrand <manuel.lagrand@epitech.eu>
2019-04-17 10:18:30 +0200

    show only two category on our View

--- src/renderer/components/catalog/ListView.tsx
+++ src/renderer/components/catalog/ListView.tsx
@@ -12,8 +12,6 @@ import { CatalogEntryView } from "readium-desktop/common/views/catalog";
 import CatalogMenu from "readium-desktop/renderer/components/publication/menu/CatalogMenu";
 import PublicationListElement from "readium-desktop/renderer/components/publication/PublicationListElement";
 
-import AddEntryForm from "./AddEntryForm";
-
 import * as styles from "readium-desktop/renderer/assets/styles/myBooks.css";
 
 interface ListViewProps {
@@ -28,27 +26,36 @@ export default class ListView extends React.Component<ListViewProps, undefined> 
                 this.props.catalogEntries.map((entry, entryIndex: number) => {
                     return (
                         <section key={ entryIndex }>
-                            <div className={styles.title}>
-                                <h1>{ entry.title }</h1>
-                            </div>
-                            <ul>
-                                { entry.publications.map((pub, i: number) => {
-                                    return (
-                                        <li className={styles.block_book_list} key={ i }>
-                                            <PublicationListElement
-                                                publication={pub}
-                                                menuContent={<CatalogMenu publication={pub}/>}
-                                            />
-                                        </li>
-                                    );
-                                })
-                                }
-                            </ul>
+                        {
+                            entryIndex <= 1 ? (
+                                <div className={styles.title}>
+                                    <h1>{ entry.title }</h1>
+                                </div>
+                            ) :
+                            (<></>)
+                        }
+                        {
+                            entryIndex <= 1 ? (
+                                <ul>
+                                    { entry.publications.map((pub, i: number) => {
+                                        return (
+                                            <li className={styles.block_book_list} key={ i }>
+                                                <PublicationListElement
+                                                    publication={pub}
+                                                    menuContent={<CatalogMenu publication={pub}/>}
+                                                />
+                                            </li>
+                                        );
+                                    })
+                                    }
+                                </ul>
+                            ) :
+                            (<></>)
+                        }
                         </section>
                     );
             })
             }
-            <AddEntryForm />
             </>
         );
     }
