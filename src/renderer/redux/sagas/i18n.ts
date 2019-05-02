336f7019b2f3f6bb016e77305681ec757f3a5ecf
Cyrille Lebeaupin <clebeaupin@noop.fr>
2019-05-02 10:56:38 +0200

    Fix withTranslator HOC component

---
+++ src/renderer/redux/sagas/i18n.ts
@@ -0,0 +1,28 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END==
+
+import { container } from "readium-desktop/renderer/di";
+
+import { Translator } from "readium-desktop/common/services/translator";
+
+import { take } from "redux-saga/effects";
+
+import { i18nActions } from "readium-desktop/common/redux/actions";
+
+export function* localeWatcher() {
+    while (true) {
+        const action = yield take(i18nActions.ActionType.Set);
+        const translator = container.get("translator") as Translator;
+        translator.setLocale(action.payload.locale);
+    }
+}
+
+export function* watchers() {
+    yield [
+        localeWatcher(),
+    ];
+}
