21787176c8fbaa9ff9936fd56d8546eebb978b5f
pleroux <p.leroux@gmx.com>
2019-06-15 11:57:00 +0200

    [fix] in information page : loading md locale by async and changed static version to an variable
    In information component the information locale md is loaded in sync. I have added promise to load file with async method.
    
    I changed the version number by dynamic print with version in package.json
    
    test on ubuntu lts 18 and not on mac-os/win

--- src/renderer/components/settings/Information.tsx
+++ src/renderer/components/settings/Information.tsx
@@ -17,9 +17,10 @@ import { setLocale } from "readium-desktop/common/redux/actions/i18n";
 
 import { TranslatorProps, withTranslator } from "readium-desktop/renderer/components/utils/translator";
 
-import * as fs from "fs";
-
 import * as commonmark from "commonmark";
+
+import * as packageJson from "readium-desktop/../package.json";
+import { readFile } from "readium-desktop/utils/readFileAsync";
 
 interface Props extends TranslatorProps {
     locale: string;
@@ -41,15 +42,17 @@ export class LanguageSettings extends React.Component<Props, States> {
         };
     }
 
-    public componentDidMount() {
+    public async componentDidMount() {
         const { locale } = this.props;
+
         try {
-            const fileContent = fs.readFileSync(`src/resources/information/${locale}.md`, {encoding: "utf8"});
-            const reader = new commonmark.Parser();
-            const writer = new commonmark.HtmlRenderer();
-            const parsed = reader.parse(fileContent);
-            this.parsedMarkdown = writer.render(parsed);
-        } catch {
+            let fileContent = await readFile(`src/resources/information/${locale}.md`);
+            if ((packageJson as any).version) {
+                fileContent = fileContent.replace("{{version}}", (packageJson as any).version);
+            }
+            this.parsedMarkdown = (new commonmark.HtmlRenderer()).render((new commonmark.Parser()).parse(fileContent));
+        } catch (e) {
+            console.error(e);
             this.parsedMarkdown = "<h1>There is no information for your language</h1>";
         }
         this.forceUpdate();
