f84ecd13c1685e84e8c8eae5a67f42645aeb47b0
pleroux <p.leroux@gmx.com>
2019-06-17 16:06:24 +0200

    [fix] I use promisify for fs async

--- src/common/rectangle/window.ts
+++ src/common/rectangle/window.ts
@@ -8,36 +8,29 @@
 import { app, Rectangle, screen } from "electron";
 import * as fs from "fs";
 import * as path from "path";
-import { readFile } from "readium-desktop/utils/readFileAsync";
-import { writeFile } from "readium-desktop/utils/writeFileAsync";
+import { promisify } from "util";
 
 const CONFIG_NAME = "windowRectangle.json";
 const PATH = path.join(app.getPath("userData"), CONFIG_NAME);
 
-const existAsync = (pa: string) =>
-    new Promise<void>((resolve, reject) =>
-        fs.exists(pa, (res) => res ? resolve() : reject()));
-
 export const savedWindowsRectangle = async (rectangle: Rectangle) => {
     try {
-        await writeFile(PATH, JSON.stringify(rectangle));
+        await promisify(fs.writeFile)(PATH, JSON.stringify(rectangle));
     } catch (e) {
         console.error(e);
     }
 };
 
 export const getWindowsRectangle = async (): Promise<Rectangle> => {
-    try {
-        await existAsync(PATH);
-        return JSON.parse(await readFile(PATH));
-    } catch (e) {
-        const c: Rectangle = {
-            height: 800,
-            width: 600,
-            x: screen.getPrimaryDisplay().workAreaSize.width / 3,
-            y: screen.getPrimaryDisplay().workAreaSize.height / 3,
-        };
-        savedWindowsRectangle(c);
-        return c;
+    if (await promisify(fs.exists)(PATH)) {
+        return JSON.parse(await promisify(fs.readFile)(PATH, { encoding: "utf8" }));
     }
+    const c: Rectangle = {
+        height: 800,
+        width: 600,
+        x: screen.getPrimaryDisplay().workAreaSize.width / 3,
+        y: screen.getPrimaryDisplay().workAreaSize.height / 3,
+    };
+    savedWindowsRectangle(c);
+    return c;
 };
