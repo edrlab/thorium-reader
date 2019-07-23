83f9400ce88ab61ffc5b60761f86491cc0a1c1b8
Pierre Leroux <panacpp@gmail.com>
2019-07-23 14:44:28 +0200

    [clean] import

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,12 @@ import * as debug_ from "debug";
 import { app, BrowserWindow, ipcMain } from "electron";
 import * as path from "path";
 import { syncIpc } from "readium-desktop/common/ipc";
-import { readerActions } from "readium-desktop/common/redux/actions";
 import { ActionSerializer } from "readium-desktop/common/services/serializer";
 import { cli } from "readium-desktop/main/cli/process";
 import { createWindow } from "readium-desktop/main/createWindow";
 import { container } from "readium-desktop/main/di";
 import { initApp, registerProtocol } from "readium-desktop/main/init";
 import { RootState } from "readium-desktop/main/redux/states";
-import { CatalogService } from "readium-desktop/main/services/catalog";
 import { _PACKAGING, _RENDERER_APP_BASE_URL } from "readium-desktop/preprocessor-directives";
 import { Store } from "redux";
 
@@ -26,6 +24,7 @@ import { initGlobalConverters_OPDS } from "@r2-opds-js/opds/init-globals";
 import {
     initGlobalConverters_GENERIC, initGlobalConverters_SHARED,
 } from "@r2-shared-js/init-globals";
+
 import { setLocale } from "./common/redux/actions/i18n";
 import { AvailableLanguages } from "./common/services/translator";
 import { cli_ } from "./main/cli/commandLine";
