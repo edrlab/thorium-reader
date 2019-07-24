3313a8df5b4b559a71ffeb1bc189a570806dfcd0
Manu <manuel.lagrand@epitech.eu>
2019-07-24 14:55:55 +0200

    fix: publication info popup dialog, close => keyboard focus back to menu (#468 Fixes #455)

--- src/renderer/components/utils/menu/MenuButton.tsx
+++ src/renderer/components/utils/menu/MenuButton.tsx
@@ -6,16 +6,22 @@
 // ==LICENSE-END==
 
 import * as React from "react";
+import ReactDOM = require("react-dom");
 
 interface MenuButtonProps {
     menuId: string;
     open: boolean;
     toggle: () => void;
+    focusMenuButton?: (ref: React.RefObject<HTMLButtonElement>, menuID: string) => void;
 }
 
 export default class Header extends React.Component<MenuButtonProps, undefined> {
+    private menuButton = React.createRef<HTMLButtonElement>();
+
     public constructor(props: any) {
         super(props);
+
+        this.getFocusBack = this.getFocusBack.bind(this);
     }
 
     public render(): React.ReactElement<{}> {
@@ -25,9 +31,17 @@ export default class Header extends React.Component<MenuButtonProps, undefined> 
                 aria-expanded={open}
                 aria-controls={menuId}
                 onClick={toggle}
+                ref={this.menuButton}
             >
+                {this.getFocusBack()}
                 {children}
             </button>
         );
+    }
+
+    public getFocusBack() {
+        if (this.menuButton && this.props.open) {
+            this.props.focusMenuButton(this.menuButton, this.props.menuId);
+        }
     }
 }
