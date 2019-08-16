71709dab02006e2f9ba98c5f49dc5586d5d3bcef
Pierre Leroux <panacpp@gmail.com>
2019-08-16 13:42:51 +0200

    lock

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,17 @@ import * as debug_ from "debug";
 import { app, ipcMain } from "electron";
 import * as path from "path";
 import { syncIpc } from "readium-desktop/common/ipc";
+import { ActionWithSender } from "readium-desktop/common/models/sync";
 import { ActionSerializer } from "readium-desktop/common/services/serializer";
 import { cli_ } from "readium-desktop/main/cli/commandLine";
 import { cli } from "readium-desktop/main/cli/process";
 import { createWindow } from "readium-desktop/main/createWindow";
 import { container } from "readium-desktop/main/di";
 import { initApp, registerProtocol } from "readium-desktop/main/init";
-import { _PACKAGING, _RENDERER_APP_BASE_URL, _VSCODE_LAUNCH } from "readium-desktop/preprocessor-directives";
+import { lockInstance } from "readium-desktop/main/lock";
+import {
+    _PACKAGING, _RENDERER_APP_BASE_URL, _VSCODE_LAUNCH,
+} from "readium-desktop/preprocessor-directives";
 import { Store } from "redux";
 
 import { setLcpNativePluginPath } from "@r2-lcp-js/parser/epub/lcp";
@@ -24,8 +28,6 @@ import { initGlobalConverters_OPDS } from "@r2-opds-js/opds/init-globals";
 import {
     initGlobalConverters_GENERIC, initGlobalConverters_SHARED,
 } from "@r2-shared-js/init-globals";
-
-import { ActionWithSender } from "readium-desktop/common/models/sync";
 
 if (_PACKAGING !== "0") {
     // Disable debug in packaged app
@@ -58,6 +60,8 @@ if (_VSCODE_LAUNCH === "true") {
 
 function main() {
     initSessions();
+
+    lockInstance();
 
     // Quit when all windows are closed.
     app.on("window-all-closed", () => {
---
+++ src/main/lock.ts
@@ -0,0 +1,31 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END==
+
+import { app } from "electron";
+import { container } from "readium-desktop/main/di";
+import { WinRegistry } from "readium-desktop/main/services/win-registry";
+
+export function lockInstance() {
+    const gotTheLock = app.requestSingleInstanceLock();
+
+    if (!gotTheLock) {
+        app.quit();
+    } else {
+        app.on("second-instance", () => {
+            // Someone tried to run a second instance, we should focus our window.
+
+            const winRegistry = container.get("win-registry") as WinRegistry;
+            const win = winRegistry.getWindow(0);
+            if (win && win.win) {
+                if (win.win.isMinimized()) {
+                    win.win.restore();
+                }
+                win.win.focus();
+            }
+        });
+    }
+}
