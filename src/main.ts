18553b11796278bc780ed7dace22ea98b1001b94
pleroux <p.leroux@gmx.com>
2019-08-23 18:05:40 +0200

    rev import

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,13 @@ import * as debug_ from "debug";
 import { app, ipcMain } from "electron";
 import * as path from "path";
 import { syncIpc } from "readium-desktop/common/ipc";
-import { ActionWithSender } from "readium-desktop/common/models/sync";
 import { ActionSerializer } from "readium-desktop/common/services/serializer";
 import { cli_ } from "readium-desktop/main/cli/commandLine";
 import { cli } from "readium-desktop/main/cli/process";
 import { createWindow } from "readium-desktop/main/createWindow";
 import { container } from "readium-desktop/main/di";
 import { initApp, registerProtocol } from "readium-desktop/main/init";
-import {
-    _PACKAGING, _RENDERER_APP_BASE_URL, _VSCODE_LAUNCH,
-} from "readium-desktop/preprocessor-directives";
+import { _PACKAGING, _RENDERER_APP_BASE_URL, _VSCODE_LAUNCH } from "readium-desktop/preprocessor-directives";
 import { Store } from "redux";
 
 import { setLcpNativePluginPath } from "@r2-lcp-js/parser/epub/lcp";
@@ -27,6 +24,8 @@ import { initGlobalConverters_OPDS } from "@r2-opds-js/opds/init-globals";
 import {
     initGlobalConverters_GENERIC, initGlobalConverters_SHARED,
 } from "@r2-shared-js/init-globals";
+
+import { ActionWithSender } from "readium-desktop/common/models/sync";
 
 if (_PACKAGING !== "0") {
     // Disable debug in packaged app
