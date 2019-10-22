c7c590c5b787a4af45a7176d729c944b8f2e8468
Pierre Leroux <panacpp@gmail.com>
2019-10-22 15:07:54 +0200

    add the same opdsControl on pubInfo that the opdsMenu

--- src/renderer/components/dialog/publicationInfos/opdsControls.tsx
+++ src/renderer/components/dialog/publicationInfos/opdsControls.tsx
@@ -8,13 +8,14 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import * as importAction from "readium-desktop/common/redux/actions/import";
-import { ImportOpdsPublication } from "readium-desktop/common/redux/states/import";
+// import { ImportOpdsPublication } from "readium-desktop/common/redux/states/import";
 import { OpdsPublicationView } from "readium-desktop/common/views/opds";
 import * as styles from "readium-desktop/renderer/assets/styles/bookDetailsDialog.css";
 import {
     TranslatorProps, withTranslator,
 } from "readium-desktop/renderer/components/utils/hoc/translator";
 import { RootState } from "readium-desktop/renderer/redux/states";
+import { TMouseEvent } from "readium-desktop/typings/react";
 import { TDispatch } from "readium-desktop/typings/redux";
 
 interface IProps extends TranslatorProps, ReturnType<typeof mapDispatchToProps>, ReturnType<typeof mapStateToProps> {
@@ -23,29 +24,73 @@ interface IProps extends TranslatorProps, ReturnType<typeof mapDispatchToProps>,
 
 export class OpdsControls extends React.Component<IProps> {
     public render(): React.ReactElement<{}> {
-        const { publication, verifyImport, buttonIsDisabled } = this.props;
+        const { publication, buttonIsDisabled } = this.props;
         const { __ } = this.props;
 
         if (!publication) {
             return <></>;
         }
 
-        return publication.isFree ? (
-            <button
-                onClick={() => verifyImport(publication as ImportOpdsPublication)}
-                className={styles.lire}
-                disabled={buttonIsDisabled}
-            >
-                {__("catalog.addBookToLib")}
-            </button>
-        ) : publication.hasSample && (
-            <button
-                onClick={() => verifyImport(publication as ImportOpdsPublication, true)}
-                className={styles.lire}
-            >
-                {__("opds.menu.addExtract")}
-            </button>
+        return (
+            <>
+                { publication.isFree &&
+                    <button role="menuitem"
+                        onClick={ (e) => this.onAddToCatalogClick(e) }
+                        className={styles.lire}
+                        disabled={buttonIsDisabled}
+                    >
+                        {__("catalog.addBookToLib")}
+                    </button>
+                }
+                {publication.hasSample &&
+                    <button role="menuitem"
+                        onClick={(e) => this.onAddToCatalogClick(e, true)}
+                        className={styles.lire}
+                    >
+                        {__("opds.menu.addExtract")}
+                    </button>
+                }
+                {publication.buyUrl &&
+                    <button
+                        className={styles.lire}
+                    >
+                        <a role="menuitem"
+                            href={publication.buyUrl}
+                        >
+                            {__("opds.menu.goBuyBook")}
+                        </a>
+                    </button>
+                }
+                {publication.borrowUrl &&
+                    <button
+                        className={styles.lire}
+                    >
+                        <a role="menuitem"
+                            href={publication.borrowUrl}
+                        >
+                            {__("opds.menu.goLoanBook")}
+                        </a>
+                    </button>
+                }
+                {publication.subscribeUrl &&
+                    <button
+                        className={styles.lire}
+                    >
+                        <a role="menuitem"
+                            href={publication.subscribeUrl}
+                        >
+                            {__("opds.menu.goSubBook")}
+                        </a>
+                    </button>
+                }
+
+            </>
         );
+    }
+
+    private onAddToCatalogClick(e: TMouseEvent, downloadSample?: boolean) {
+        e.preventDefault();
+        this.props.verifyImport(this.props.publication, downloadSample);
     }
 }
 
@@ -53,7 +98,7 @@ const mapDispatchToProps = (dispatch: TDispatch) => {
     return {
         // src/common/redux/states/import.ts
         // FIXME : REDUX ISN'T TYPED, resolve this force cast
-        verifyImport: (publication: ImportOpdsPublication, downloadSample?: boolean) => {
+        verifyImport: (publication: OpdsPublicationView, downloadSample?: boolean) => {
             dispatch(importAction.verifyImport(
                 {
                     publication,
