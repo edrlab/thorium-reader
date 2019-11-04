c77fe5f5588b373ea4ae6d3b340342a1a3c1e6fe
Daniel Weck <daniel.weck@gmail.com>
2019-11-04 15:17:34 +0000

    restrict LCP passphrase dialog to the bookshelf/library views (reader BrowserWindows were incorrectly displaying it too)

--- src/renderer/redux/sagas/lcp.ts
+++ src/renderer/redux/sagas/lcp.ts
@@ -7,14 +7,31 @@
 
 import { lcpActions } from "readium-desktop/common/redux/actions";
 import { open } from "readium-desktop/common/redux/actions/dialog";
+import { diRendererGet } from "readium-desktop/renderer/di";
 import { SagaIterator } from "redux-saga";
 import { all, call, put, take } from "redux-saga/effects";
+
+// import { PublicationView } from "readium-desktop/common/views/publication";
+// import { ReaderState } from "readium-desktop/renderer/redux/states/reader";
 
 export function* lcpUserKeyCheckRequestWatcher(): SagaIterator {
     while (true) {
         const action: any = yield take(lcpActions.ActionType.UserKeyCheckRequest);
 
         const { hint, publication, message } = action.payload;
+        // const id1 = (publication as PublicationView).identifier;
+
+        const store = diRendererGet("store");
+        if (store.getState().reader.reader) {
+
+            // const id2 = store.getState().reader.reader.publication.identifier;
+            // const id3 = store.getState().reader.reader.identifier;
+
+            // passphrase dialog only in library/bookshelf view
+            // already-opened reader BrowserWindows (distinct renderer processes)
+            // must not display the popup dialog!
+            continue;
+        }
 
         yield put(open("lcp-authentication",
             {
