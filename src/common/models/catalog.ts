a203f9d9a2f6ebe91a4c76931f55764d9bc3ff72
Daniel Weck <daniel.weck@gmail.com>
2019-11-08 08:49:35 +0000

    rollback unnecessary change (minor)

--- src/common/models/catalog.ts
+++ src/common/models/catalog.ts
@@ -8,7 +8,7 @@
 import { Publication } from "./publication";
 
 /**
- * A catalog contains a list of Publications
+ * A catalog contains a list of publications
  */
 export interface Catalog {
     title: string;
