c260c9a1e82f0a31eaea2149a88a3f93a4b7ddc3
Daniel Weck <daniel.weck@gmail.com>
2019-11-12 16:54:43 +0000

    fixed type

--- src/main/streamer.ts
+++ src/main/streamer.ts
@@ -148,7 +148,7 @@ const staticOptions = {
 };
 const MATHJAX_URL_PATH = "math-jax";
 streamer.expressUse("/" + MATHJAX_URL_PATH, express.static(mathJaxPath, staticOptions));
-const transformer = (_publication: Publication, _link: Link, str: string): string => {
+const transformer = (_publication: R2Publication, _link: Link, str: string): string => {
 
     const store = diMainGet("store");
     let settings = store.getState().reader.config;
