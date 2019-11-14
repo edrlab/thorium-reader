0869314fd83847acb4feea63ef678d150bdccff3
Daniel Weck <daniel.weck@gmail.com>
2019-11-14 19:51:09 +0000

    try again

---
+++ scripts/package-ci-version-bump.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+let pkg = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
+pkg = pkg.replace(/^  "version": "(.+)",$/m, `  "version": "$1.${process.argv[3]}",`);
+console.log(pkg.substr(0, 100));
+fs.writeFileSync(process.argv[2], pkg, { encoding: 'utf8' });
