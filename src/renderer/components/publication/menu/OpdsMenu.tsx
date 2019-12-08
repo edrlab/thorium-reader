173f976c53f2e9674d3829e9a6e22acc5c359ffb
Daniel Weck <daniel.weck@gmail.com>
2019-12-08 13:51:04 +0000

    OPDS menu items can dynamically change (during info dialog), so cannot be static in menu (otherwise mismatch with borrow/import/etc choices from info dialog)

--- src/renderer/components/publication/menu/OpdsMenu.tsx
+++ src/renderer/components/publication/menu/OpdsMenu.tsx
@@ -5,27 +5,17 @@
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 // ==LICENSE-END==
 
-import { shell } from "electron";
-import { Location } from "history";
 import * as React from "react";
 import { connect } from "react-redux";
-import { matchPath } from "react-router";
 import { DialogTypeName } from "readium-desktop/common/models/dialog";
-import { dialogActions, importActions } from "readium-desktop/common/redux/actions/";
-import { IOpdsLinkView, IOpdsPublicationView } from "readium-desktop/common/views/opds";
+import { dialogActions } from "readium-desktop/common/redux/actions/";
+import { IOpdsPublicationView } from "readium-desktop/common/views/opds";
 import {
     TranslatorProps, withTranslator,
 } from "readium-desktop/renderer/components/utils/hoc/translator";
-import { buildOpdsBrowserRoute } from "readium-desktop/renderer/opds/route";
 import { RootState } from "readium-desktop/renderer/redux/states";
-import {
-    dispatchHistoryPush, IOpdsBrowse, IRouterLocationState, routes,
-} from "readium-desktop/renderer/routing";
 import { TMouseEventOnButton } from "readium-desktop/typings/react";
 import { TDispatch } from "readium-desktop/typings/redux";
-import { ContentType } from "readium-desktop/utils/content-type";
-
-import { IBreadCrumbItem } from "../../layout/BreadCrumb";
 
 // tslint:disable-next-line: no-empty-interface
 interface IBaseProps extends TranslatorProps {
@@ -49,130 +39,9 @@ export class OpdsMenu extends React.Component<IProps, undefined> {
     public render(): React.ReactElement<{}> {
 
         const {
-            opdsPublicationView,
-            verifyImport,
-            openAccessButtonIsDisabled,
-            sampleButtonIsDisabled,
             __,
-            handleOpdsLink,
         } = this.props;
 
-        const openAccessLinksButton = () =>
-            Array.isArray(opdsPublicationView.openAccessLinks)
-                ? opdsPublicationView.openAccessLinks.map(
-                    (ln, idx) =>
-                        <button
-                            key={`openAccess-${idx}`}
-                            onClick={() => verifyImport(
-                                ln,
-                                opdsPublicationView.r2OpdsPublicationBase64,
-                                opdsPublicationView.title,
-                            )}
-                            disabled={openAccessButtonIsDisabled()}
-                        >
-                            {__("catalog.addBookToLib")}
-                        </button>,
-                )
-                : <></>;
-
-        const sampleOrPreviewLinksButton = () =>
-            Array.isArray(opdsPublicationView.sampleOrPreviewLinks)
-                ? opdsPublicationView.sampleOrPreviewLinks.map(
-                    (ln, idx) =>
-                        <button
-                            key={`sample-${idx}`}
-                            onClick={() => verifyImport(
-                                ln,
-                                opdsPublicationView.r2OpdsPublicationBase64,
-                                opdsPublicationView.title,
-                            )}
-                            disabled={sampleButtonIsDisabled()}
-                        >
-                            {__("opds.menu.addExtract")}
-                        </button>,
-                )
-                : <></>;
-
-        const feedLinksList = () => {
-
-            const buyList = () =>
-                Array.isArray(opdsPublicationView.buyLinks)
-                    ? opdsPublicationView.buyLinks.map(
-                        (ln, idx) =>
-                            <button
-                                key={`buy-${idx}`}
-                                onClick={() => handleOpdsLink(
-                                        opdsPublicationView,
-                                        ln,
-                                        this.props.location,
-                                        this.props.breadcrumb,
-                                        __("opds.menu.goBuyBook"))}
-                            >
-                                {__("opds.menu.goBuyBook")}
-                            </button>,
-                    )
-                    : <></>;
-
-            const borrowList = () =>
-                Array.isArray(opdsPublicationView.borrowLinks)
-                    ? opdsPublicationView.borrowLinks.map(
-                        (ln, idx) =>
-                            <button
-                                key={`borrow-${idx}`}
-                                onClick={() => handleOpdsLink(
-                                        opdsPublicationView,
-                                        ln,
-                                        this.props.location,
-                                        this.props.breadcrumb,
-                                        __("opds.menu.goLoanBook"))}
-                            >
-                                {__("opds.menu.goLoanBook")}
-                            </button>,
-                    )
-                    : <></>;
-
-            const subscribeList = () =>
-                Array.isArray(opdsPublicationView.subscribeLinks)
-                    ? opdsPublicationView.subscribeLinks.map(
-                        (ln, idx) =>
-                            <button
-                                key={`subscribe-${idx}`}
-                                onClick={() => handleOpdsLink(
-                                        opdsPublicationView,
-                                        ln,
-                                        this.props.location,
-                                        this.props.breadcrumb,
-                                        __("opds.menu.goSubBook"))}
-                            >
-                                {__("opds.menu.goSubBook")}
-                            </button>,
-                    )
-                    : <></>;
-
-            if (
-                (Array.isArray(opdsPublicationView.buyLinks)
-                    && opdsPublicationView.buyLinks.length)
-                || (Array.isArray(opdsPublicationView.borrowLinks)
-                    && opdsPublicationView.borrowLinks.length)
-                || (Array.isArray(opdsPublicationView.subscribeLinks)
-                    && opdsPublicationView.subscribeLinks.length)
-            ) {
-                return (
-                    <>
-                        {
-                            buyList()
-                        }
-                        {
-                            borrowList()
-                        }
-                        {
-                            subscribeList()
-                        }
-                    </>
-                );
-            }
-            return (<></>);
-        };
         return (
             <>
                 <button role="menuitem"
@@ -180,15 +49,6 @@ export class OpdsMenu extends React.Component<IProps, undefined> {
                 >
                     {__("opds.menu.aboutBook")}
                 </button>
-                {
-                    openAccessLinksButton()
-                }
-                {
-                    sampleOrPreviewLinksButton()
-                }
-                {
-                    feedLinksList()
-                }
             </>
         );
     }
@@ -208,72 +68,11 @@ const mapDispatchToProps = (dispatch: TDispatch, props: IBaseProps) => {
                 },
             ));
         },
-        verifyImport: (...data: Parameters<typeof importActions.verify.build>) => {
-            dispatch(dialogActions.closeRequest.build());
-            dispatch(importActions.verify.build(...data));
-        },
-        handleOpdsLink: (
-            opdsPublicationView: IOpdsPublicationView,
-            ln: IOpdsLinkView,
-            location: Location<IRouterLocationState>,
-            _breadcrumb: IBreadCrumbItem[],
-            linkLabel: string) => {
-
-            dispatch(dialogActions.closeRequest.build());
-
-            if (ln.type === ContentType.Html || ln.type === ContentType.Xhtml) {
-                shell.openExternal(ln.url);
-            } else if (ln.type === ContentType.Opds2 ||
-                ln.type === ContentType.Opds2Auth ||
-                ln.type === ContentType.Opds2Pub ||
-                ln.type === ContentType.AtomXml) {
-
-                const param = matchPath<IOpdsBrowse>(
-                    location.pathname, routes["/opds/browse"],
-                ).params;
-                const lvl = parseInt(param.level, 10);
-                // const i = (lvl > 1) ? (lvl - 1) : lvl;
-                // const name = breadcrumb[i] && breadcrumb[i].name;
-                const newLvl = lvl === 1 ? 3 : (lvl + 1);
-                const label = `${linkLabel} (${opdsPublicationView.title})`;
-
-                const route = buildOpdsBrowserRoute(
-                    param.opdsId,
-                    label,
-                    ln.url, // this.props.headerLinks?.self
-                    newLvl,
-                );
-
-                dispatchHistoryPush(dispatch)({
-                    ...location,
-                    pathname: route,
-                    // state: {} // we preserve the existing route state
-                });
-            } else {
-                shell.openExternal(ln.url);
-            }
-        },
     };
 };
 
-const mapStateToProps = (state: RootState, props: IBaseProps) => {
+const mapStateToProps = (_state: RootState, _props: IBaseProps) => {
     return {
-        breadcrumb: state.opds.browser.breadcrumb,
-        location: state.router.location,
-        openAccessButtonIsDisabled: () => {
-            return !!state.download.downloads.find(
-                (dl) => props.opdsPublicationView.openAccessLinks.find(
-                    (ln) => ln.url === dl.url,
-                ),
-            );
-        },
-        sampleButtonIsDisabled: () => {
-            return !!state.download.downloads.find(
-                (dl) => props.opdsPublicationView.sampleOrPreviewLinks.find(
-                    (ln) => ln.url === dl.url,
-                ),
-            );
-        },
     };
 };
 
