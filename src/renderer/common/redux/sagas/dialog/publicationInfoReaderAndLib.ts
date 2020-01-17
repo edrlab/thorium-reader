027dac46c734f26ce11dfb75f6cb5c1521036544
Pierre Leroux <panacpp@gmail.com>
2020-01-17 21:16:12 +0100

    up

--- src/renderer/common/redux/sagas/dialog/publicationInfoReaderAndLib.ts
+++ src/renderer/common/redux/sagas/dialog/publicationInfoReaderAndLib.ts
@@ -39,7 +39,6 @@ function* checkReaderAndLibPublicationWatcher() {
         }
 
         yield call(updateReaderAndLibPublicationWatcher);
-        yield take(dialogActions.closeRequest.build);
     }
 }
 
