7e23a2e1fffc537331d8f52006102dc30f7b2490
pleroux <p.leroux@gmx.com>
2020-02-08 16:13:29 +0100

    follow 3c756eb

--- src/main/redux/store/memory.ts
+++ src/main/redux/store/memory.ts
@@ -5,6 +5,10 @@
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 // ==LICENSE-END==
 
+import { app } from "electron";
+import { LocaleConfigIdentifier, LocaleConfigValueType } from "readium-desktop/common/config";
+import { AvailableLanguages } from "readium-desktop/common/services/translator";
+import { PromiseAllSettled } from "readium-desktop/common/utils/promise";
 import { ConfigDocument } from "readium-desktop/main/db/document/config";
 import { ConfigRepository } from "readium-desktop/main/db/repository/config";
 import { CONFIGREPOSITORY_REDUX_WIN_PERSISTENCE } from "readium-desktop/main/di";
@@ -12,6 +16,7 @@ import { reduxSyncMiddleware } from "readium-desktop/main/redux/middleware/sync"
 import { rootReducer } from "readium-desktop/main/redux/reducers";
 import { rootSaga } from "readium-desktop/main/redux/sagas";
 import { RootState } from "readium-desktop/main/redux/states";
+import { ObjectKeys } from "readium-desktop/utils/object-keys-values";
 import { applyMiddleware, createStore, Store } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "remote-redux-devtools";
@@ -20,25 +25,68 @@ import { reduxPersistMiddleware } from "../middleware/persistence";
 
 const REDUX_REMOTE_DEVTOOLS_PORT = 7770;
 
-export async function initStore(configRepository: ConfigRepository<Partial<RootState>>): Promise<Store<RootState>> {
+const defaultLocale = (): LocaleConfigValueType => {
+    const loc = app.getLocale().split("-")[0];
+    const langCodes = ObjectKeys(AvailableLanguages);
+    const lang = langCodes.find((l) => l === loc) || "en";
 
-    let preloadedStateRepository: ConfigDocument<Partial<RootState>>;
+    return {
+        locale: lang,
+    };
+};
+
+export async function initStore(configRepository: ConfigRepository<any>): Promise<Store<RootState>> {
+
+    let reduxStateRepository: ConfigDocument<Partial<RootState>>;
+    let i18nStateRepository: ConfigDocument<LocaleConfigValueType>;
+
     try {
-        preloadedStateRepository = await configRepository.get(CONFIGREPOSITORY_REDUX_WIN_PERSISTENCE);
+        const reduxStateRepositoryPromise = configRepository.get(CONFIGREPOSITORY_REDUX_WIN_PERSISTENCE);
+
+        const i18nStateRepositoryPromise = configRepository.get(LocaleConfigIdentifier);
+
+        const [
+            reduxStateRepositoryResult,
+            i18nStateRepositoryResult,
+        ] = await PromiseAllSettled(
+            [
+                reduxStateRepositoryPromise,
+                i18nStateRepositoryPromise,
+            ],
+        );
+
+        if (reduxStateRepositoryResult.status === "fulfilled") {
+            reduxStateRepository = reduxStateRepositoryResult.value;
+        }
+        if (i18nStateRepositoryResult.status === "fulfilled") {
+            i18nStateRepository = i18nStateRepositoryResult.value;
+        }
     } catch (_err) {
         // ignore
         // first init
     }
-    const preloadedState = preloadedStateRepository?.value?.win
-        ? preloadedStateRepository.value
-        : undefined;
+
+    const preloadedState = {
+        ...reduxStateRepository?.value?.win
+            ? reduxStateRepository.value
+            : undefined,
+        ...{
+            i18n: i18nStateRepository?.value?.locale
+                ? i18nStateRepository.value
+                : defaultLocale(),
+        },
+    };
 
     const sagaMiddleware = createSagaMiddleware();
 
     const store = createStore(
         rootReducer,
         preloadedState,
-        composeWithDevTools({ realtime: true, port: REDUX_REMOTE_DEVTOOLS_PORT })(
+        composeWithDevTools(
+            {
+                port: REDUX_REMOTE_DEVTOOLS_PORT,
+            },
+        )(
             applyMiddleware(
                 reduxSyncMiddleware,
                 sagaMiddleware,
