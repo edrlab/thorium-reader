f1251186bd99cd7f9c6bca5fe9f3563f548a9d31
Daniel Weck <daniel.weck@gmail.com>
2020-03-28 17:40:26 +0000

    fix: NPM package updates, including R2 components which fix issues with large resources (images, fonts, audio/video) not being handled reliably

--- src/main/streamer.ts
+++ src/main/streamer.ts
@@ -171,7 +171,7 @@ const staticOptions = {
 };
 const MATHJAX_URL_PATH = "math-jax";
 streamer.expressUse("/" + MATHJAX_URL_PATH, express.static(mathJaxPath, staticOptions));
-const transformer = (_publication: R2Publication, _link: Link, str: string): string => {
+const transformer = (_publication: R2Publication, _link: Link, _url: string | undefined, str: string): string => {
 
     const store = diMainGet("store");
     const settings = store.getState().reader.config;
