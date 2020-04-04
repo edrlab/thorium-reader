08e699501a308a66a6fe2eb707a28a8af4800ffe
Daniel Weck <daniel.weck@gmail.com>
2020-04-04 07:57:28 +0100

    hotfix(l10n): follow-up to PR #989 (Dutch translation) added missing new label

--- src/typings/en.translation.d.ts
+++ src/typings/en.translation.d.ts
@@ -32,8 +32,8 @@ declare namespace typed_i18n {
     readonly "title": string,
     readonly "undo": string
   },
-  readonly "quit": string,
-  readonly "hide": string
+  readonly "hide": string,
+  readonly "quit": string
 };
   (_: "app.edit", __?: {}): {
   readonly "copy": string,
@@ -50,8 +50,8 @@ declare namespace typed_i18n {
   (_: "app.edit.redo", __?: {}): string;
   (_: "app.edit.selectAll", __?: {}): string;
   (_: "app.edit.title", __?: {}): string;
-  (_: "app.edit.undo", __?: {}): string; (_: "app.quit", __?: {}): string;
-  (_: "app.hide", __?: {}): string;
+  (_: "app.edit.undo", __?: {}): string; (_: "app.hide", __?: {}): string;
+  (_: "app.quit", __?: {}): string;
   (_: "catalog", __?: {}): {
   readonly "about": { readonly "button": string, readonly "title": string },
   readonly "addBookToLib": string,
