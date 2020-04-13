bc70ea9a3b79f95a37a62318eba263115c75ff27
Pierre Leroux <pierre@leroux.tech>
2020-04-13 15:16:32 +0200

    refactoring of deepClone

--- src/utils/deepClone.ts
+++ src/utils/deepClone.ts
@@ -5,21 +5,12 @@
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 // ==LICENSE-END==
 
-const objFromEntries = Object.fromEntries;
-
-export const deepClone = (o: any): any => {
-
-    const match = (_o: any): any =>
+export const deepClone = (_o: any): any =>
         _o && typeof _o === "object"
             ? Array.isArray(_o)
-                ? _cloneArr(_o)
-                : objFromEntries(_cloneObj(Object.entries(_o)))
+                ? _o.map((_value) => deepClone(_value))
+                : Object.fromEntries(
+                    Object.entries(_o).map(
+                        ([_key, _value]) =>
+                            [_key, deepClone(_value)]))
             : _o;
-
-    const _cloneArr = (_a: any[]): any[] => _a.map((_value) => match(_value));
-
-    const _cloneObj = (_o: Array<[string, any]>): Array<[string, any]> =>
-        _o.map(([_key, _value]) => [_key, match(_value)]);
-
-    return match(o);
-};
