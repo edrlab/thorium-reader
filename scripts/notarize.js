40ed7ac45f02aba5fd4f590c2890cf22f9f2cdcf
Daniel Weck <daniel.weck@gmail.com>
2020-04-24 20:48:23 +0100

    chore(lint): spaces ... [skip ci]

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin' || process.env.SKIP_NOTARIZE) {
-    return;
-  }
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== 'darwin' || process.env.SKIP_NOTARIZE) {
+        return;
+    }
 
-  const appName = context.packager.appInfo.productFilename;
-  return await notarize({
-    appBundleId: 'org.edrlab.thorium',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-    ascProvider: process.env.APPLEIDTEAM
-  });
+    const appName = context.packager.appInfo.productFilename;
+    return await notarize({
+        appBundleId: 'org.edrlab.thorium',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: process.env.APPLEID,
+        appleIdPassword: process.env.APPLEIDPASS,
+        ascProvider: process.env.APPLEIDTEAM
+    });
 };
