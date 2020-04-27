98ed451a89a0e3e62cfe77451ae8ee888b495857
Daniel Weck <daniel.weck@gmail.com>
2020-04-27 19:39:35 +0100

    minor mods

--- scripts/fix-plists.sh
+++ scripts/fix-plists.sh
@@ -12,10 +12,24 @@ code "./release/mas/Thorium.app/Contents/Frameworks/Thorium Helper (Renderer).ap
 code "./release/mas/Thorium.app/Contents/Frameworks/Thorium Helper.app/Contents/Info.plist"
 code "./release/mas/Thorium.app/Contents/Info.plist"
 
-npx electron-osx-sign \
+DEBUG=* npx electron-osx-sign \
+--gatekeeper-assess=false \
+--hardened-runtime=false \
+--type=development \
+--platform=darwin \
+--identity="Mac Developer: Daniel Weck (BP324G48P6)" \
+--entitlements="./scripts/entitlements.mac.mas.plist" \
+--entitlements-inherit="./scripts/entitlements.mac.mas.inherit.plist" \
+--provisioning-profile="./dev.provisionprofile" \
+"./release/mas/Thorium.app"
+
+DEBUG=* npx electron-osx-sign \
 --gatekeeper-assess=false \
 --hardened-runtime=false \
 --type=distribution \
 --platform=mas \
---entitlements="scripts/entitlements.mac.mas.plist" \
+--identity="3rd Party Mac Developer Installer: European Digital Reading Lab (327YA3JNGT)" \
+--entitlements="./scripts/entitlements.mac.mas.plist" \
+--entitlements-inherit="./scripts/entitlements.mac.mas.inherit.plist" \
+--provisioning-profile="./prod.provisionprofile" \
 "./release/mas/Thorium.app"
