e69a0c78448197d888cb6580631f67c947ca3058
Pierre Leroux <pierre@leroux.tech>
2020-06-04 16:13:00 +0200

    fix previous commit

--- src/utils/counter.ts
+++ src/utils/counter.ts
@@ -9,7 +9,7 @@ const counter = () => {
     let _counter = 0;
 
     return () => {
-        return Number.isSafeInteger(++_counter) ? counter : _counter = 0;
+        return Number.isSafeInteger(++_counter) ? counter : _counter = 1;
     };
 };
 export const getCount = counter();
