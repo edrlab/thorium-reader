91fbe1ab37421561a90012b056a8bf8beb65ec8c
Naglis Jonaitis <naglis@mailbox.org>
2020-08-28 23:51:35 +0300

    Add Lithuanian localization

--- src/common/services/translator.ts
+++ src/common/services/translator.ts
@@ -13,6 +13,7 @@ import * as esCatalog from "readium-desktop/resources/locales/es.json";
 import * as frCatalog from "readium-desktop/resources/locales/fr.json";
 import * as jaCatalog from "readium-desktop/resources/locales/ja.json";
 import * as nlCatalog from "readium-desktop/resources/locales/nl.json";
+import * as ltCatalog from "readium-desktop/resources/locales/lt.json";
 
 import * as deLang from "readium-desktop/resources/locale-names/deLang.json";
 import * as enLang from "readium-desktop/resources/locale-names/enLang.json";
@@ -20,6 +21,7 @@ import * as esLang from "readium-desktop/resources/locale-names/esLang.json";
 import * as frLang from "readium-desktop/resources/locale-names/frLang.json";
 import * as jaLang from "readium-desktop/resources/locale-names/jaLang.json";
 import * as nlLang from "readium-desktop/resources/locale-names/nlLang.json";
+import * as ltLang from "readium-desktop/resources/locale-names/ltLang.json";
 import { TFunction } from "readium-desktop/typings/en.translation";
 
 // -----------------------------------------------------------
@@ -95,6 +97,9 @@ i18nextInstance.init({
         ja: {
             translation: jaCatalog,
         },
+        lt: {
+            translation: ltCatalog,
+        },
     },
     // lng: undefined,
     fallbackLng: "en",
@@ -122,6 +127,7 @@ i18nextInstance.addResourceBundle("de", "translation", deLang, true);
 i18nextInstance.addResourceBundle("es", "translation", esLang, true);
 i18nextInstance.addResourceBundle("nl", "translation", nlLang, true);
 i18nextInstance.addResourceBundle("ja", "translation", jaLang, true);
+i18nextInstance.addResourceBundle("lt", "translation", ltLang, true);
 
 const i18nextInstanceEN = i18nextInstance.cloneInstance();
 i18nextInstanceEN.changeLanguage("en").then((_t) => {
@@ -140,6 +146,7 @@ export const AvailableLanguages = {
     es: "Español",
     nl: "Dutch",
     ja: "日本語",
+    lt: "Lietuvių",
 };
 
 interface LocalizedContent {
