dfa4fd333571efc3006ffbabf78cdf406905bfbb
Pierre Leroux <pierre@leroux.tech>
2020-09-03 16:30:06 +0200

    up

--- src/main/redux/sagas/api/publication/import/importFromLink.ts
+++ src/main/redux/sagas/api/publication/import/importFromLink.ts
@@ -6,6 +6,7 @@
 // ==LICENSE-END==
 
 import * as debug_ from "debug";
+import fetch from "node-fetch";
 import { callTyped } from "readium-desktop/common/redux/sagas/typed-saga";
 import { IOpdsLinkView, IOpdsPublicationView } from "readium-desktop/common/views/opds";
 import { PublicationDocument } from "readium-desktop/main/db/document/publication";
@@ -19,60 +20,96 @@ import { importFromFsService } from "./importFromFs";
 // Logger
 const debug = debug_("readium-desktop:main#saga/api/publication/importFromLinkService");
 
+function* importLinkFromPath(
+    downloadPath: string,
+    link: IOpdsLinkView,
+    pub?: IOpdsPublicationView,
+) {
+
+    let returnPublicationDocument: PublicationDocument;
+    // Import downloaded publication in catalog
+    const lcpHashedPassphrase = link?.properties?.lcpHashedPassphrase;
+    let publicationDocument = yield* importFromFsService(downloadPath, lcpHashedPassphrase);
+
+    if (publicationDocument) {
+        const tags = pub?.tags?.map((v) => v.name);
+
+        // Merge with the original publication
+        publicationDocument = Object.assign(
+            {},
+            publicationDocument,
+            {
+                resources: {
+                    r2PublicationBase64: publicationDocument.resources.r2PublicationBase64,
+                    r2LCPBase64: publicationDocument.resources.r2LCPBase64,
+                    r2LSDBase64: publicationDocument.resources.r2LSDBase64,
+                    r2OpdsPublicationBase64: pub?.r2OpdsPublicationBase64 || "",
+                },
+                tags,
+            },
+        );
+
+        const publicationRepository = diMainGet("publication-repository");
+        returnPublicationDocument = yield* callTyped(() => publicationRepository.save(publicationDocument));
+    }
+
+    return returnPublicationDocument;
+}
+
 export function* importFromLinkService(
     link: IOpdsLinkView,
     pub?: IOpdsPublicationView,
 ): SagaGenerator<PublicationDocument | undefined> {
 
-    let returnPublicationDocument: PublicationDocument;
-
-    if (!link?.url) {
-        debug("Unable to get an acquisition url from opds publication", link);
+    let url: URL;
+    try {
+        url = new URL(link?.url);
+    } catch (e) {
+        debug("bad url", link, e);
         throw new Error("Unable to get acquisition url from opds publication");
     }
 
+    if (!link.type) {
+        const response = yield* callTyped(() => fetch(url));
+        const contentType = response?.headers?.get("Content-Type");
+        if (contentType) {
+            link.type = contentType;
+        } else {
+            link.type = "";
+        }
+    }
+    const contentTypeArray = link.type.replace(/\s/g, "").split(";");
+
     const title = link.title || link.url;
-    const isLcpFile = link.type === ContentType.Lcp;
-    const isEpubFile = link.type === ContentType.Epub;
-    const isAudioBookPacked = link.type === ContentType.AudioBookPacked;
-    const isAudioBookPackedLcp = link.type === ContentType.AudioBookPackedLcp;
+    const isLcpFile = contentTypeArray.includes(ContentType.Lcp);
+    const isEpubFile = contentTypeArray.includes(ContentType.Epub);
+    const isAudioBookPacked = contentTypeArray.includes(ContentType.AudioBookPacked);
+    const isAudioBookPackedLcp = contentTypeArray.includes(ContentType.AudioBookPackedLcp);
+    const isHtml = contentTypeArray.includes(ContentType.Html);
+    const isJson = contentTypeArray.includes(ContentType.Json);
+
     if (!isLcpFile && !isEpubFile && !isAudioBookPacked && !isAudioBookPackedLcp) {
-        // throw new Error(`OPDS download link is not EPUB or AudioBook! ${link.url} ${link.type}`);
         debug(`OPDS download link is not EPUB or AudioBook! ${link.url} ${link.type}`);
     }
 
-    debug("Start the download", link);
+    if (isHtml || isJson) {
+        debug("the link need to be packaged");
 
-    const [downloadPath] = yield* callTyped(downloader, [link.url], title);
+        const packagePath = yield* callTyped(packager, url);
 
-    if (downloadPath) {
+        return yield* callTyped(importLinkFromPath, packagePath, { url: url.toString()}, pub);
 
-        // Import downloaded publication in catalog
-        const lcpHashedPassphrase = link?.properties?.lcpHashedPassphrase;
-        let publicationDocument = yield* importFromFsService(downloadPath, lcpHashedPassphrase);
+    } else {
+        debug("Start the download", link);
 
-        if (publicationDocument) {
-            const tags = pub?.tags?.map((v) => v.name);
+        const [downloadPath] = yield* callTyped(downloader, [link.url], title);
+        if (downloadPath) {
 
-            // Merge with the original publication
-            publicationDocument = Object.assign(
-                {},
-                publicationDocument,
-                {
-                    resources: {
-                        r2PublicationBase64: publicationDocument.resources.r2PublicationBase64,
-                        r2LCPBase64: publicationDocument.resources.r2LCPBase64,
-                        r2LSDBase64: publicationDocument.resources.r2LSDBase64,
-                        r2OpdsPublicationBase64: pub?.r2OpdsPublicationBase64 || "",
-                    },
-                    tags,
-                },
-            );
-
-            const publicationRepository = diMainGet("publication-repository");
-            returnPublicationDocument = yield* callTyped(() => publicationRepository.save(publicationDocument));
+            return yield* callTyped(importLinkFromPath, downloadPath, link, pub);
         }
+
     }
 
-    return returnPublicationDocument;
+    debug("error to import", url.toString());
+    return undefined;
 }
