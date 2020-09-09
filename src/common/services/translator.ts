15275c990e05ab48d38fd0a9c593d7013bb6cc83
Pedro Milliet <pedro.milliet@gmail.com>
2020-09-09 12:30:28 -0300

    feat(l10n): Portuguese UI translation (PR #1141)

--- src/common/services/translator.ts
+++ src/common/services/translator.ts
@@ -13,6 +13,8 @@ import * as esCatalog from "readium-desktop/resources/locales/es.json";
 import * as frCatalog from "readium-desktop/resources/locales/fr.json";
 import * as jaCatalog from "readium-desktop/resources/locales/ja.json";
 import * as nlCatalog from "readium-desktop/resources/locales/nl.json";
+import * as ptBrCatalog from "readium-desktop/resources/locales/pt-br.json";
+import * as ptPtCatalog from "readium-desktop/resources/locales/pt-pt.json";
 
 import * as deLang from "readium-desktop/resources/locale-names/deLang.json";
 import * as enLang from "readium-desktop/resources/locale-names/enLang.json";
@@ -20,6 +22,8 @@ import * as esLang from "readium-desktop/resources/locale-names/esLang.json";
 import * as frLang from "readium-desktop/resources/locale-names/frLang.json";
 import * as jaLang from "readium-desktop/resources/locale-names/jaLang.json";
 import * as nlLang from "readium-desktop/resources/locale-names/nlLang.json";
+import * as ptBrLang from "readium-desktop/resources/locale-names/ptBrLang.json";
+import * as ptPtLang from "readium-desktop/resources/locale-names/ptPtLang.json";
 import { TFunction } from "readium-desktop/typings/en.translation";
 
 // -----------------------------------------------------------
@@ -77,23 +81,29 @@ if (i18next.createInstance) {
 i18nextInstance.init({
     debug: false,
     resources: {
-        en: {
+        "en": {
             translation: enCatalog,
         },
-        fr: {
+        "fr": {
             translation: frCatalog,
         },
-        de: {
+        "de": {
             translation: deCatalog,
         },
-        es: {
+        "es": {
             translation: esCatalog,
         },
-        nl: {
+        "nl": {
             translation: nlCatalog,
         },
-        ja: {
+        "ja": {
             translation: jaCatalog,
+        },
+        "pt-br": {
+            translation: ptBrCatalog,
+        },
+        "pt-pt": {
+            translation: ptPtCatalog,
         },
     },
     // lng: undefined,
@@ -122,6 +132,8 @@ i18nextInstance.addResourceBundle("de", "translation", deLang, true);
 i18nextInstance.addResourceBundle("es", "translation", esLang, true);
 i18nextInstance.addResourceBundle("nl", "translation", nlLang, true);
 i18nextInstance.addResourceBundle("ja", "translation", jaLang, true);
+i18nextInstance.addResourceBundle("pt-br", "translation", ptBrLang, true);
+i18nextInstance.addResourceBundle("pt-pt", "translation", ptPtLang, true);
 
 const i18nextInstanceEN = i18nextInstance.cloneInstance();
 i18nextInstanceEN.changeLanguage("en").then((_t) => {
@@ -134,12 +146,14 @@ i18nextInstanceEN.changeLanguage("en").then((_t) => {
 // src/utils/object-keys-values.ts
 // to benefit from compile-type TypeScript typesafe key enum
 export const AvailableLanguages = {
-    en: "English",
-    fr: "Français",
-    de: "Deutsch",
-    es: "Español",
-    nl: "Dutch",
-    ja: "日本語",
+    "en": "English",
+    "fr": "Français",
+    "de": "Deutsch",
+    "es": "Español",
+    "nl": "Dutch",
+    "ja": "日本語",
+    "pt-br": "Português Brasileiro",
+    "pt-pt": "Português",
 };
 
 interface LocalizedContent {
