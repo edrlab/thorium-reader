4e32e9abe3eceba31ea6f6babcc64b30cecab72a
Daniel Weck <daniel.weck@gmail.com>
2020-11-16 05:03:49 +0000

    DAISY support

--- src/main/redux/sagas/api/publication/import/importPublicationFromFs.ts
+++ src/main/redux/sagas/api/publication/import/importPublicationFromFs.ts
@@ -6,6 +6,7 @@
 // ==LICENSE-END==
 
 import * as debug_ from "debug";
+import { nanoid } from "nanoid";
 import * as path from "path";
 import { acceptedExtensionObject } from "readium-desktop/common/extension";
 import { RandomCustomCovers } from "readium-desktop/common/models/custom-cover";
@@ -15,12 +16,15 @@ import {
     PublicationDocument, PublicationDocumentWithoutTimestampable,
 } from "readium-desktop/main/db/document/publication";
 import { diMainGet } from "readium-desktop/main/di";
+import { createTempDir } from "readium-desktop/main/fs/path";
 import { extractFileFromZipToBuffer } from "readium-desktop/main/zip/extract";
 import { v4 as uuidv4 } from "uuid";
 
 import { LCP } from "@r2-lcp-js/parser/epub/lcp";
 import { TaJsonDeserialize, TaJsonSerialize } from "@r2-lcp-js/serializable";
 import { Publication as R2Publication } from "@r2-shared-js/models/publication";
+import { DaisyParsePromise } from "@r2-shared-js/parser/daisy";
+import { convertDaisyToReadiumWebPub } from "@r2-shared-js/parser/daisy-convert-to-epub";
 import { EpubParsePromise } from "@r2-shared-js/parser/epub";
 
 // import { CbzParsePromise } from "@r2-shared-js/parser/cbz";
@@ -58,6 +62,26 @@ export async function importPublicationFromFS(
             r2Publication.freeDestroy();
 
             break;
+
+        case acceptedExtensionObject.daisy:
+
+            debug("daisy extension", ext);
+
+            r2Publication = await DaisyParsePromise(filePath);
+
+            const pathFile = await createTempDir(nanoid(8), "misc");
+            const packagePath = await convertDaisyToReadiumWebPub(pathFile, r2Publication);
+
+            // after PublicationParsePromise, cleanup zip handler
+            // (no need to fetch ZIP data beyond this point)
+            r2Publication.freeDestroy();
+
+            if (packagePath) {
+                return await importPublicationFromFS(packagePath);
+            }
+
+            throw new Error("convertDaisyToReadiumWebPub failed?!");
+            // break;
 
         // case acceptedExtensionObject.cbz:
             // r2Publication = await CbzParsePromise(filePath);
