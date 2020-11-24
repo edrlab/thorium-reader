a07d3103c8d7cabc53bc5957c1a7200eb1ac3a34
Pierre Leroux <pierre@leroux.tech>
2020-11-24 17:52:59 +0100

    new store with pipe and unpipe promisable

--- src/renderer/reader/pdf/webview/store.ts
+++ src/renderer/reader/pdf/webview/store.ts
@@ -1,24 +1,54 @@
 
-export interface IStore<T> {
+type TFn<Key extends keyof Second = any, First = any, Second = any, Ret = any> = (a: First) => (b: Second[Key]) => Ret;
+
+export interface IStore<T, T2> {
     getState: () => T;
-    setState: (a: Partial<T>) => T;
-    subscribe: (key: keyof T, fn: (...a: any[]) => any) => void;
-    unsubscribe: (fn: (...a: any[]) => any) => void;
+    setState: (a: Partial<T>) => void;
+    subscribe: <TK extends keyof T>(key: TK, fn: TFn<TK, T2, T>) => void;
+    unsubscribe: (fn: TFn) => void;
+    pipe: <TK extends keyof T>(key: TK, fn: TFn<TK, T2, T, Promise<T[TK]> | T[TK]>) => void;
+    unpipe: (fn: TFn) => void;
 }
 
 export const storeInit = <TStateValue = {}, TCallbackInjection extends {} = {}>(
     state: TStateValue,
     di: TCallbackInjection = {} as TCallbackInjection,
-    callbackArray: {[key: string]: Set<(...a: any[]) => any>} = {},
-): IStore<TStateValue> => ({
+    callbackArray: { [key: string]: Set<TFn> } = {},
+    pipeArray: { [key: string]: Set<TFn> } = {},
+): IStore<TStateValue, TCallbackInjection> => ({
     getState: () => state,
-    setState: (newState: Partial<TStateValue>) => (
-        state = { ...state, ...newState },
-        Object.keys(newState).forEach((key) => callbackArray[key]?.forEach((fn) => fn ? fn(di) : undefined)),
-        state
-    ),
-    subscribe: (key: keyof TStateValue, fn: (di: TCallbackInjection) => any) =>
-        (callbackArray[key as string] =
-            (callbackArray[key as string] || new Set<(a: any[]) => any>()).add(fn), undefined),
-    unsubscribe: (fn: (...a: any[]) => any) => Object.values(callbackArray).forEach((v) => v?.delete(fn)),
-});
+    setState: (newState) => (
+            newState = Object.entries(newState)
+                .reduce(
+                    (pv, [key, value]) => ({
+                        ...pv,
+                        [key]:
+                            pipeArray[key]
+                                ? Array.from(pipeArray[key])
+                                    .reduce(
+                                        (vP, fn) =>
+                                            vP.then((v) => Promise.resolve(fn(di)(v))),
+                                        Promise.resolve(value),
+                                    )
+                                : Promise.resolve(value),
+                    }),
+                    {},
+                ),
+            Object.entries(newState)
+                .forEach(([key, value]) =>
+                    (value as Promise<unknown>).then((v) => state = { ...state, [key]: v })),
+            Object.entries(newState)
+                .forEach(([key, value]) =>
+                    callbackArray[key]?.forEach((fn) =>
+                        (value as Promise<unknown>).then((v) => fn(di)(v)))),
+            undefined
+        ),
+        subscribe: (key, fn) =>
+            (callbackArray[key as string] =
+                (callbackArray[key as string] || new Set()).add(fn), undefined),
+        unsubscribe: (fn) => Object.values(callbackArray).forEach((v) => v?.delete(fn)),
+        pipe: (key, fn) =>
+            (pipeArray[key as string] =
+                (pipeArray[key as string] || new Set()).add(fn), undefined),
+        unpipe: (fn) => Object.values(pipeArray).forEach((v) => v?.delete(fn)),
+    });
