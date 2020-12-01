672cd1d21db16136a1b8387733ef5289a3348059
Pierre Leroux <pierre@leroux.tech>
2020-12-01 17:24:43 +0100

    ec lint

--- src/main/redux/sagas/auth.ts
+++ src/main/redux/sagas/auth.ts
@@ -602,94 +602,94 @@ const htmlLoginTemplate = (urlToSubmit: string = "", loginLabel = "login", passL
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Sign in</title>
+    <title>Sign in</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <style>
-    html,
-    body {
-      height: 100%;
-    }
+    <!-- Custom styles for this template -->
+    <style>
+        html,
+        body {
+        height: 100%;
+        }
 
-    body {
-      display: -ms-flexbox;
-      display: -webkit-box;
-      display: flex;
-      -ms-flex-align: center;
-      -ms-flex-pack: center;
-      -webkit-box-align: center;
-      align-items: center;
-      -webkit-box-pack: center;
-      justify-content: center;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      background-color: #f5f5f5;
-    }
+        body {
+        display: -ms-flexbox;
+        display: -webkit-box;
+        display: flex;
+        -ms-flex-align: center;
+        -ms-flex-pack: center;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+        }
 
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: 0 auto;
-    }
+        .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+        }
 
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
+        .form-signin .checkbox {
+        font-weight: 400;
+        }
 
-    .form-signin .form-control {
-      position: relative;
-      box-sizing: border-box;
-      height: auto;
-      padding: 10px;
-      font-size: 16px;
-    }
+        .form-signin .form-control {
+        position: relative;
+        box-sizing: border-box;
+        height: auto;
+        padding: 10px;
+        font-size: 16px;
+        }
 
-    .form-signin .form-control:focus {
-      z-index: 2;
-    }
+        .form-signin .form-control:focus {
+        z-index: 2;
+        }
 
-    .form-signin input[type="login"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
+        .form-signin input[type="login"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+        }
 
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-  </style>
-</head>
+        .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        }
+    </style>
+    </head>
 
-<body class="text-center">
-<form class="form-signin" action="${urlToSubmit}" method="post">
-    ${logoUrl ? `<img class="mb-4" src="${logoUrl}" alt="login logo">` : ``}
-    ${title ? `<h4 class="h4 mb-4 font-weight-normal">${title}</h4>` : ``}
-    <!--<img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
-      height="72">!-->
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <label for="inputLogin" class="sr-only">${loginLabel}</label>
-    <input name="login" id="inputUsername" class="form-control" placeholder="${loginLabel}" required autofocus>
-    <!-- <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus> -->
-    <label for="inputPassword" class="sr-only">${passLabel}</label>
-    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="${passLabel}" required>
-    <!--<div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>!-->
-    </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  </form>
+    <body class="text-center">
+    <form class="form-signin" action="${urlToSubmit}" method="post">
+        ${logoUrl ? `<img class="mb-4" src="${logoUrl}" alt="login logo">` : ``}
+        ${title ? `<h4 class="h4 mb-4 font-weight-normal">${title}</h4>` : ``}
+        <!--<img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72"
+        height="72">!-->
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputLogin" class="sr-only">${loginLabel}</label>
+        <input name="login" id="inputUsername" class="form-control" placeholder="${loginLabel}" required autofocus>
+        <!-- <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus> -->
+        <label for="inputPassword" class="sr-only">${passLabel}</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="${passLabel}" required>
+        <!--<div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>!-->
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
 </body>
 
 </html>`;
