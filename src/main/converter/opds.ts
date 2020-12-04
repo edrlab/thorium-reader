c5c019b22fa0ad50e26c2ab0daf9b3e2ef395116
Pierre Leroux <pierre@leroux.tech>
2020-12-04 11:55:02 +0100

    fix(OPDS): filter rel='alternate', improved contentType parsing (PR #1275 Fixes #1148)

--- src/main/converter/opds.ts
+++ src/main/converter/opds.ts
@@ -322,6 +322,7 @@ export class OpdsFeedViewConverter {
         const entrylinkView = fallback(
             this.convertFilterLinkToView(baseUrl, r2OpdsPublication.Links, {
                 type: "type=entry;profile=opds-catalog",
+                rel: "alternate",
             }),
             this.convertFilterLinkToView(baseUrl, r2OpdsPublication.Links, {
                 type: ContentType.Opds2Pub,
