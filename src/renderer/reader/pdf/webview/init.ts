e20bfbbd33b25ea9a6ac192d0eaa84f6d9f07872
Pierre Leroux <pierre@leroux.tech>
2020-12-11 20:14:52 +0100

    scroll zoom works ! for scroll view

--- src/renderer/reader/pdf/webview/init.ts
+++ src/renderer/reader/pdf/webview/init.ts
@@ -84,7 +84,7 @@ export async function pdfReaderInit(
         ...{
             view: "paginated",
             column: "1",
-            scale: "fit",
+            scale: "page-fit",
             lastPageNumber: 1,
             displayPage: () => Promise.resolve(),
             pdfViewer: null,
@@ -150,7 +150,7 @@ export async function pdfReaderInit(
 
     pdfViewer.setDocument(pdfDocument);
 
-    pdfStore.setState({pdfViewer, pdfDistEventBus});
+    pdfStore.setState({ pdfViewer, pdfDistEventBus });
 
     // canva
     const paginatedContainer = document.getElementById("paginatedContainer");
@@ -173,7 +173,7 @@ export async function pdfReaderInit(
         pdfViewer.currentPageNumber = pageNumber;
     };
 
-    const updateDisplayPage = ({store}: IEVState) => {
+    const updateDisplayPage = ({ store }: IEVState) => {
 
         document.body.style.overflow = "auto";
         document.body.style.overflowY = "auto";
@@ -193,7 +193,7 @@ export async function pdfReaderInit(
             displayScrolledPage(lastPageNumber);
         }
     };
-    updateDisplayPage({store: pdfStore, bus});
+    updateDisplayPage({ store: pdfStore, bus });
 
     bus.subscribe("page", goToPageAction);
     bus.subscribe("page-next",
@@ -202,20 +202,20 @@ export async function pdfReaderInit(
         (a) => () => goToPageAction(a)(--a.store.getState().lastPageNumber));
     bus.subscribe("scale",
         (a) => async (scale) => {
-            a.store.setState({scale});
+            a.store.setState({ scale });
             a.bus.dispatch("scale", scale);
             return goToPageAction(a)(a.store.getState().lastPageNumber);
         });
     bus.subscribe("view",
         (a) => async (view) => {
-            a.store.setState({view});
+            a.store.setState({ view });
             updateDisplayPage(a);
             a.bus.dispatch("view", view);
             return goToPageAction(a)(a.store.getState().lastPageNumber);
         });
     bus.subscribe("column",
         (a) => async (column) => {
-            a.store.setState({column});
+            a.store.setState({ column });
             a.bus.dispatch("column", column);
             return goToPageAction(a)(a.store.getState().lastPageNumber);
         });
@@ -233,6 +233,141 @@ export async function pdfReaderInit(
         console.log("resize", document.body.clientWidth, document.body.clientHeight);
         await debouncedResize();
     });
+
+    const DEFAULT_SCALE_DELTA = 1.1;
+    const MIN_SCALE = 0.1;
+    const MAX_SCALE = 10.0;
+
+    // https://github.com/mozilla/pdf.js/blob/00b4f86db301546d21d5dc79c61e2a9a829667a4/web/app.js#L609
+    function zoomIn(ticks: number) {
+
+        let newScale = pdfViewer.currentScale;
+        do {
+            newScale = (newScale * DEFAULT_SCALE_DELTA).toFixed(2);
+            newScale = Math.ceil(newScale * 10) / 10;
+            newScale = Math.min(MAX_SCALE, newScale);
+        } while (--ticks > 0 && newScale < MAX_SCALE);
+        pdfViewer.currentScaleValue = newScale;
+    }
+
+    function zoomOut(ticks: number) {
+        let newScale = pdfViewer.currentScale;
+        do {
+            newScale = (newScale / DEFAULT_SCALE_DELTA).toFixed(2);
+            newScale = Math.floor(newScale * 10) / 10;
+            newScale = Math.max(MIN_SCALE, newScale);
+        } while (--ticks > 0 && newScale > MIN_SCALE);
+        pdfViewer.currentScaleValue = newScale;
+    }
+
+    // function zoomReset() {
+    //     pdfViewer.currentScaleValue = "page-fit";
+    // }
+
+    // https://github.com/mozilla/pdf.js/blob/00b4f86db301546d21d5dc79c61e2a9a829667a4/web/app.js#L2167
+    let _wheelUnusedTicks = 0;
+    function accumulateWheelTicks(ticks: number) {
+        // If the scroll direction changed, reset the accumulated wheel ticks.
+        if (
+            (_wheelUnusedTicks > 0 && ticks < 0) ||
+            (_wheelUnusedTicks < 0 && ticks > 0)
+        ) {
+            _wheelUnusedTicks = 0;
+        }
+        _wheelUnusedTicks += ticks;
+        const wholeTicks =
+            Math.sign(_wheelUnusedTicks) *
+            Math.floor(Math.abs(_wheelUnusedTicks));
+        _wheelUnusedTicks -= wholeTicks;
+        return wholeTicks;
+    }
+
+    const WHEEL_ZOOM_DISABLED_TIMEOUT = 1000; // ms
+    let zoomDisabledTimeout: any = null;
+    function setZoomDisabledTimeout() {
+        if (zoomDisabledTimeout) {
+            clearTimeout(zoomDisabledTimeout);
+        }
+        zoomDisabledTimeout = setTimeout(() => {
+            zoomDisabledTimeout = null;
+        }, WHEEL_ZOOM_DISABLED_TIMEOUT);
+    }
+
+    // not avaialable from pdf_viewer , why ? // ln869
+    // pdfViewerDist.normalizeWheelEventDirection(evt)
+    function normalizeWheelEventDirection(evt: any) {
+        let delta = Math.sqrt(evt.deltaX * evt.deltaX + evt.deltaY * evt.deltaY);
+        const angle = Math.atan2(evt.deltaY, evt.deltaX);
+
+        if (-0.25 * Math.PI < angle && angle < 0.75 * Math.PI) {
+            delta = -delta;
+        }
+
+        return delta;
+    }
+
+    // https://github.com/mozilla/pdf.js/blob/00b4f86db301546d21d5dc79c61e2a9a829667a4/web/app.js#L2832
+    function webViewerWheel(evt: any) {
+        if (
+            evt.ctrlKey || evt.metaKey
+        ) {
+            // Only zoom the pages, not the entire viewer.
+            evt.preventDefault();
+            // NOTE: this check must be placed *after* preventDefault.
+            if (zoomDisabledTimeout || document.visibilityState === "hidden") {
+                return;
+            }
+
+            const previousScale = pdfViewer.currentScale;
+
+            const delta = normalizeWheelEventDirection(evt);
+            let ticks = 0;
+            if (
+                evt.deltaMode === WheelEvent.DOM_DELTA_LINE ||
+                evt.deltaMode === WheelEvent.DOM_DELTA_PAGE
+            ) {
+                // For line-based devices, use one tick per event, because different
+                // OSs have different defaults for the number lines. But we generally
+                // want one "clicky" roll of the wheel (which produces one event) to
+                // adjust the zoom by one step.
+                if (Math.abs(delta) >= 1) {
+                    ticks = Math.sign(delta);
+                } else {
+                    // If we're getting fractional lines (I can't think of a scenario
+                    // this might actually happen), be safe and use the accumulator.
+                    ticks = accumulateWheelTicks(delta);
+                }
+            } else {
+                // pixel-based devices
+                const PIXELS_PER_LINE_SCALE = 30;
+                ticks = accumulateWheelTicks(
+                    delta / PIXELS_PER_LINE_SCALE,
+                );
+            }
+
+            if (ticks < 0) {
+                zoomOut(-ticks);
+            } else if (ticks > 0) {
+                zoomIn(ticks);
+            }
+
+            const currentScale = pdfViewer.currentScale;
+            if (previousScale !== currentScale) {
+                // After scaling the page via zoomIn/zoomOut, the position of the upper-
+                // left corner is restored. When the mouse wheel is used, the position
+                // under the cursor should be restored instead.
+                const scaleCorrectionFactor = currentScale / previousScale - 1;
+                const rect = pdfViewer.container.getBoundingClientRect();
+                const dx = evt.clientX - rect.left;
+                const dy = evt.clientY - rect.top;
+                pdfViewer.container.scrollLeft += dx * scaleCorrectionFactor;
+                pdfViewer.container.scrollTop += dy * scaleCorrectionFactor;
+            }
+        } else {
+            setZoomDisabledTimeout();
+        }
+    }
+    window.addEventListener("wheel", webViewerWheel, { passive: false });
 
     return pdfStore;
 }
