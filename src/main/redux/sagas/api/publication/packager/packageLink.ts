19988d0f45b878a1a3d874c22532dcffa82dc951
Pierre Leroux <pierre@leroux.tech>
2021-01-08 16:18:25 +0100

    fix(c0ddd1923048dec8d030b6f02afa9db0c70a0e14): packager resources path in the zip
    - I've disabled the hash path for the resources and for the readingOrders. The readingOrders didn't follow the same root with the resources. Now all path that not an url is written with the same path inside the zip. The hash was a security for the same path allocate to both resources. It's now enabled for url href with no dedicated file path like audiobooks reading orders

--- src/main/redux/sagas/api/publication/packager/packageLink.ts
+++ src/main/redux/sagas/api/publication/packager/packageLink.ts
@@ -12,6 +12,7 @@ import * as path from "path";
 import { callTyped } from "readium-desktop/common/redux/sagas/typed-saga";
 import { httpGet } from "readium-desktop/main/http";
 import {
+    getUniqueResourcesFromR2Publication,
     w3cPublicationManifestToReadiumPublicationManifest,
 } from "readium-desktop/main/w3c/audiobooks/converter";
 import {
@@ -91,33 +92,20 @@ function* downloadResources(
     title: string,
     baseUrl: string,
 ): SagaGenerator<TResources> {
-    const readingpluslink = [
-        ...(
-            Array.isArray(r2Publication.Links)
-                ? r2Publication.Links
-                : []),
-        ...(
-            Array.isArray(r2Publication.Spine)
-                ? r2Publication.Spine
-                : []),
-    ];
 
-    const resources = [
-        ...(
-            Array.isArray(r2Publication.Resources)
-                ? r2Publication.Resources
-                : []),
-    ];
-
-    const resourcesHrefReadingPlusLink = [...new Set(linksToArray(readingpluslink))];
-    const resourcesHrefResources = [...new Set(linksToArray(resources))];
-
-    const resourcesHref = [...resourcesHrefReadingPlusLink, ...resourcesHrefResources];
-    const resourcesType = [...resourcesHrefReadingPlusLink.map(() => false), ...resourcesHrefResources.map(() => true)];
+    const uResources = getUniqueResourcesFromR2Publication(r2Publication);
+    const resourcesHref = [...new Set(linksToArray(uResources))];
+    const resourcesType = resourcesHref.map((v) => {
+        // tslint:disable-next-line
+        try { new URL(v); return false; } catch {}
+        return true;
+    });
 
     const resourcesHrefResolved = resourcesHref.map((l) => url.resolve(baseUrl, decodeURIComponent(l)));
 
-    const pathArrayFromDownloader = yield* callTyped(downloader, resourcesHrefResolved, title);
+    const pathArrayFromDownloader = baseUrl.startsWith("file://")
+        ? resourcesHrefResolved.map((v) => v.slice("file://".length))
+        : yield* callTyped(downloader, resourcesHrefResolved, title);
     const pathArray = pathArrayFromDownloader.map<[string, boolean]>((v, i) => [v, resourcesType[i]]);
 
     const resourcesHrefMap = pathArray.map<TResource>(
@@ -132,12 +120,6 @@ function* downloadResources(
             if (isResourcesType) {
                 zipPath = path.normalize(resourcesHref[idx]);
 
-                // specific fixes for exploded epub format
-                // zipPath = zipPath.startsWith("OEBPS/")
-                //     ? zipPath.slice(6)
-                //     : zipPath.startsWith("/OEBPS/")
-                //         ? zipPath.slice(7)
-                //         : zipPath;
             } else {
                 const hash = crypto.createHash("sha1").update(resourcesHref[idx]).digest("hex");
                 zipPath = hash + "/" + path.basename(resourcesHrefResolved[idx]);
