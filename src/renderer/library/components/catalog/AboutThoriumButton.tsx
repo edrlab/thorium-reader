aa484815990ac1938ae898e9a267ac71a5f35f0f
Pierre Leroux <p.leroux@gmx.com>
2021-01-19 10:55:55 +0100

    fix(about): disable about modal for nnecessary "about Thorium" publication requests (fixes #1325)

--- src/renderer/library/components/catalog/AboutThoriumButton.tsx
+++ src/renderer/library/components/catalog/AboutThoriumButton.tsx
@@ -5,16 +5,28 @@
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 // ==LICENSE-END==
 
+import { existsSync, promises } from "fs";
+import * as path from "path";
 import * as React from "react";
 import { connect } from "react-redux";
-import { DialogTypeName } from "readium-desktop/common/models/dialog";
-import { dialogActions } from "readium-desktop/common/redux/actions";
-import { _APP_NAME, _APP_VERSION } from "readium-desktop/preprocessor-directives";
+import { ABOUT_BOOK_TITLE_PREFIX } from "readium-desktop/common/constant";
+import { readerActions } from "readium-desktop/common/redux/actions";
+import { PublicationView } from "readium-desktop/common/views/publication";
+import { _APP_NAME, _APP_VERSION, _PACKAGING } from "readium-desktop/preprocessor-directives";
 import * as styles from "readium-desktop/renderer/assets/styles/myBooks.css";
 import {
     TranslatorProps, withTranslator,
 } from "readium-desktop/renderer/common/components/hoc/translator";
 import { TDispatch } from "readium-desktop/typings/redux";
+import { findMimeTypeWithExtension, mimeTypes } from "readium-desktop/utils/mimeTypes";
+
+import { TaJsonSerialize } from "@r2-lcp-js/serializable";
+import { Metadata } from "@r2-shared-js/models/metadata";
+import { Publication as R2Publication } from "@r2-shared-js/models/publication";
+import { Link } from "@r2-shared-js/models/publication-link";
+
+import { apiAction } from "../../apiAction";
+import { ILibraryRootState } from "../../redux/states";
 
 const capitalizedAppName = _APP_NAME.charAt(0).toUpperCase() + _APP_NAME.substring(1);
 
@@ -26,37 +38,128 @@ interface IBaseProps extends TranslatorProps {
 // ReturnType<typeof mapStateToProps>
 // ReturnType<typeof mapDispatchToProps>
 // tslint:disable-next-line: no-empty-interface
-interface IProps extends IBaseProps, ReturnType<typeof mapDispatchToProps> {
+interface IProps extends IBaseProps, ReturnType<typeof mapDispatchToProps>, ReturnType<typeof mapStateToProps> {
 }
 
 class AboutThoriumButton extends React.Component<IProps, undefined> {
+
+    private manifestView: PublicationView;
 
     constructor(props: IProps) {
         super(props);
     }
 
-    public render(): React.ReactElement<{}> {
+    public render() {
         const { __ } = this.props;
         return (
             <section id={styles.aboutThoriumButton}>
                 <h2>{__("catalog.about.title", { appName: capitalizedAppName })}</h2>
                 <p>{`v${_APP_VERSION}`}</p>
-                <button onClick={this.props.displayPublicationInfo}>
+                <button onClick={this.about}>
                     {__("catalog.about.button")}
                 </button>
             </section>
         );
     }
+
+    private about = async () => {
+        const { locale } = this.props;
+        const infoFolderRelativePath = "assets/md/information/"; // final / is important
+        const imagesFolder = "images";
+
+        let aboutLocale = locale;
+
+        const setTitle = (l: string) => `${ABOUT_BOOK_TITLE_PREFIX}${l}`;
+
+        try {
+
+            let title = setTitle(aboutLocale);
+
+            // TODO: search by title is not reliable
+            let [pubView] = await apiAction("publication/search", title);
+            if (pubView) {
+                console.log("pubView already exist no need to generate a new one");
+                console.log(pubView);
+
+                this.manifestView = pubView;
+                return;
+            }
+
+            let folderPath = path.join((global as any).__dirname, infoFolderRelativePath);
+            if (_PACKAGING === "0") {
+                folderPath = path.join(process.cwd(), "dist", infoFolderRelativePath);
+            }
+
+            let htmlFile = `${aboutLocale}.xhtml`;
+            {
+                const filePath = path.join(folderPath, `${aboutLocale}.xhtml`);
+                if (!existsSync(filePath)) {
+                    aboutLocale = "en";
+                }
+                htmlFile = `${aboutLocale}.xhtml`;
+                title = setTitle(aboutLocale);
+            }
+
+            [pubView] = await apiAction("publication/search", title);
+            if (pubView) {
+
+                console.log("pubView already exist no need to generate a new one");
+                console.log(pubView);
+
+                this.manifestView = pubView;
+                return;
+            }
+
+            const publication = new R2Publication();
+            publication.Context = ["https://readium.org/webpub-manifest/context.jsonld"];
+            publication.Metadata = new Metadata();
+            publication.Metadata.Title = title;
+
+            const link = new Link();
+            link.Href = htmlFile;
+            link.TypeLink = mimeTypes.xhtml;
+            link.Title = aboutLocale;
+            publication.Spine = [link];
+
+            const imgPath = path.join(folderPath, imagesFolder);
+            const imgArray = await promises.readdir(imgPath);
+            publication.Resources = imgArray.map((i) => {
+                const l = new Link();
+                l.Href = path.join(imagesFolder, i);
+                l.TypeLink = findMimeTypeWithExtension(path.extname(l.Href));
+
+                return l;
+            });
+
+            const publicationSerialize = TaJsonSerialize(publication);
+            const publicationStr = JSON.stringify(publicationSerialize);
+
+            this.manifestView = await apiAction("publication/importFromString", publicationStr, "file://" + folderPath);
+
+        } catch (e) {
+            console.log("error to import about", aboutLocale, e);
+
+        } finally {
+            if (this.manifestView) {
+                this.props.openReader(this.manifestView);
+            }
+        }
+    }
 }
+
+const mapStateToProps = (state: ILibraryRootState, _props: IBaseProps) => {
+
+    return {
+        locale: state.i18n.locale,
+    };
+};
 
 const mapDispatchToProps = (dispatch: TDispatch, _props: IBaseProps) => {
     return {
-        displayPublicationInfo: () => {
-            dispatch(dialogActions.openRequest.build(DialogTypeName.AboutThorium,
-                {},
-            ));
+        openReader: (publicationView: PublicationView) => {
+            dispatch(readerActions.openRequest.build(publicationView.identifier));
         },
     };
 };
 
-export default connect(undefined, mapDispatchToProps)(withTranslator(AboutThoriumButton));
+export default connect(mapStateToProps, mapDispatchToProps)(withTranslator(AboutThoriumButton));
