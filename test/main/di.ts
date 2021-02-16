26d649a98f9fa6ff3fa0b9d619913e4b108d80e2
Daniel Weck <daniel.weck@gmail.com>
2021-02-16 08:16:51 +0000

    fixed unit tests

--- test/main/di.ts
+++ test/main/di.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import { Container } from "inversify";
 import { Translator } from "../../src/common/services/translator";
 import { diSymbolTable } from "../../src/main/diSymbolTable";
@@ -8,6 +10,13 @@ interface IGet {
     (s: keyof typeof diSymbolTable): any;
 }
 const diGet: IGet = (symbol: keyof typeof diSymbolTable) => container.get<any>(diSymbolTable[symbol]);
+
+const dbBackupfilePath = path.join(
+    "dist",
+    "db-backup-test",
+);
+
 export {
     diGet as diMainGet,
+    dbBackupfilePath,
 };
