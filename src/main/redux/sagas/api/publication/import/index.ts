dae40ea20f98dbf25d4d3febce2208a22e38b1d5
Pierre Leroux <p.leroux@gmx.com>
2021-02-16 17:01:58 +0100

    html and first implementation
    need refactoring from opds authentication
    
    pick some code from auth window codebase

--- src/main/redux/sagas/api/publication/import/index.ts
+++ src/main/redux/sagas/api/publication/import/index.ts
@@ -6,13 +6,16 @@
 // ==LICENSE-END==
 
 import * as debug_ from "debug";
+import { BrowserWindow, globalShortcut } from "electron";
+import { join } from "ramda";
 import { ToastType } from "readium-desktop/common/models/toast";
 import { toastActions } from "readium-desktop/common/redux/actions";
-import { allTyped, callTyped } from "readium-desktop/common/redux/sagas/typed-saga";
+import { allTyped, callTyped, forkTyped, takeTyped } from "readium-desktop/common/redux/sagas/typed-saga";
 import { IOpdsLinkView, IOpdsPublicationView } from "readium-desktop/common/views/opds";
 import { PublicationView } from "readium-desktop/common/views/publication";
 import { diMainGet } from "readium-desktop/main/di";
-import { put } from "redux-saga/effects";
+import { tryCatchSync } from "readium-desktop/utils/tryCatch";
+import { call, cancel, delay, put, race } from "redux-saga/effects";
 import { SagaGenerator } from "typed-redux-saga";
 
 import { importFromFsService } from "./importFromFs";
@@ -175,4 +178,162 @@ export function* importFromFs(
     const ret = pubView.filter((v) => v);
 
     return ret;
+}
+
+const importFromFormHtml = (submitUrl = "") => `
+<!DOCTYPE html>
+    <html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Import publication</title>
+		<style type="text/css">
+		</style>
+		<script type="text/javascript">
+		</script>
+	</head>
+	<body>
+        <form action="${submitUrl}" method="post">
+            <fieldset>
+                <legend>From URL</legend>
+	            <label for="url">enter URL : </label>
+	            <input type="url" name="url" id="url"/>
+            </fieldset>
+	        <fieldset>
+                <legend>From JSON MANIFEST</legend>
+	            <label for="manifestjson">JSON manifest : </label>
+		        <textarea type="text" name="manifestjson" id="manifestjson"></textarea>
+            </fieldset>
+	        <fieldset>
+                <legend>From RAW INFORMATION</legend>
+                <fieldset>
+	                <legend>Metadata</legend>
+			        <label for="title">title : </label><input type="text" name="title" id="title">
+	                <br>
+	                <label for="desc">description : </label><input type="text" name="desc" id="desc">
+	                <br>
+	                <label for="author">author : </label><input type="text" name="author" id="author">
+                </fieldset>
+		        <fieldset>
+	                <legend>Cover</legend>
+			        <label for="cover">cover URL : </label><input type="text" name="cover" id="cover">
+		        </fieldset>
+		        <fieldset>
+	                <legend>Reading Order</legend>
+			        <label for="reading"> URLs (split with \n) : </label>
+                    <textarea type="text" name="reading" id="reading"></textarea>
+		        </fieldset>
+            </fieldset>
+	        <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
+`;
+
+function createWinForm(url: string): BrowserWindow | undefined {
+
+    const win = new BrowserWindow(
+        {
+            width: 800,
+            height: 600,
+        });
+
+    const handler = () => win.close();
+    globalShortcut.register("esc", handler);
+    win.on("close", () => {
+        globalShortcut.unregister("esc");
+    });
+
+    win.once("ready-to-show", () => {
+        win.show();
+    });
+
+    win.loadURL(url);
+
+    return win;
+}
+
+export function* importFromForm(): SagaGenerator<void> {
+
+    // launch new window form with html
+
+    const channel: ReturnType<typeof getImportFromFormRequestCustomProtocolEventChannel> = getImportFromFormRequestCustomProtocolEventChannel();
+
+    const task = yield* forkTyped(function*() {
+
+        const parsedRequest = yield* takeTyped(channel);
+        return {
+            request: parseRequestFromCustomProtocol(parsedRequest.request),
+            callback: parsedRequest.callback,
+        };
+    });
+
+    const browserUrl = `data:text/html;charset=utf-8,${importFromFormHtml("importfromform://result")}`;
+
+    const win =
+        tryCatchSync(
+            () => createWinForm(browserUrl),
+            "",
+        );
+    if (!win) {
+        debug("modal win undefined");
+
+        yield cancel(task);
+        return;
+    }
+
+    try {
+
+        yield race({
+            a: delay(60000),
+            b: join(task),
+            c: call(
+                async () =>
+                    new Promise<void>((resolve) => win.on("close", () => resolve())),
+            ),
+        });
+
+        if (task.isRunning()) {
+            debug("no authentication credentials received");
+            debug("perhaps timeout or closing authentication window occured");
+
+            return;
+
+        } else {
+            const { request: opdsCustomProtocolRequestParsed, callback } = task.result();
+            if (opdsCustomProtocolRequestParsed) {
+
+                const [, err] = yield* callTyped(opdsSetAuthCredentials,
+                    opdsCustomProtocolRequestParsed,
+                    authCredentials,
+                    authParsed.authenticationType,
+                );
+
+                callback({
+                    url: undefined,
+                });
+
+                if (err instanceof Error) {
+                    debug(err.message);
+
+                    return;
+
+                } else {
+                    yield put(historyActions.refresh.build());
+                }
+            }
+        }
+
+    } finally {
+
+        if (win) {
+            win.close();
+        }
+        if (task.isRunning()) {
+            yield cancel(task);
+        }
+    }
+
+
+
+
 }
