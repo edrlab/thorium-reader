17ca91f423ad3f8c57a0a8a1777880709ab4eba8
Pierre Leroux <p.leroux@gmx.com>
2021-03-05 11:22:46 +0100

    remove reader api bookmark

--- src/common/api/interface/readerApi.interface.ts
+++ src/common/api/interface/readerApi.interface.ts
@@ -6,28 +6,13 @@
 // ==LICENSE-END==
 
 import { ReaderMode } from "readium-desktop/common/models/reader";
-import { LocatorView } from "readium-desktop/common/views/locator";
 
 import { IEventPayload_R2_EVENT_CLIPBOARD_COPY } from "@r2-navigator-js/electron/common/events";
 import { LocatorExtended } from "@r2-navigator-js/electron/renderer";
-import { Locator as R2Locator } from "@r2-shared-js/models/locator";
 
 export interface IReaderApi {
     // setLastReadingLocation: (publicationIdentifier: string, locator: R2Locator) => Promise<LocatorView>;
     getLastReadingLocation: (publicationIdentifier: string) => Promise<LocatorExtended | undefined>;
-    findBookmarks: (publicationIdentifier: string) => Promise<LocatorView[]>;
-    updateBookmark: (
-        identifier: string,
-        publicationIdentifier: string,
-        locator: R2Locator,
-        name?: string,
-    ) => Promise<void>;
-    addBookmark: (
-        publicationIdentifier: string,
-        locator: R2Locator,
-        name?: string,
-    ) => Promise<void>;
-    deleteBookmark: (identifier: string) => Promise<void>;
     clipboardCopy: (
         publicationIdentifier: string,
         clipboardData: IEventPayload_R2_EVENT_CLIPBOARD_COPY) => Promise<boolean>;
@@ -37,10 +22,6 @@ export interface IReaderApi {
 export interface IReaderModuleApi {
     // "reader/setLastReadingLocation": IReaderApi["setLastReadingLocation"];
     "reader/getLastReadingLocation": IReaderApi["getLastReadingLocation"];
-    "reader/findBookmarks": IReaderApi["findBookmarks"];
-    "reader/updateBookmark": IReaderApi["updateBookmark"];
-    "reader/addBookmark": IReaderApi["addBookmark"];
-    "reader/deleteBookmark": IReaderApi["deleteBookmark"];
     "reader/clipboardCopy": IReaderApi["clipboardCopy"];
     "reader/getMode": IReaderApi["getMode"];
 }
