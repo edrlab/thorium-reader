99107df488f82ad986a16ca30de385fd5006a8ed
Pierre Leroux <p.leroux@gmx.com>
2021-03-09 21:57:51 +0100

    up

--- src/main/network/fetch.ts
+++ src/main/network/fetch.ts
@@ -9,12 +9,23 @@ import * as nodeFetchCookie from "fetch-cookie";
 import nodeFetch from "node-fetch";
 import { tryCatch } from "readium-desktop/utils/tryCatch";
 import * as tougth from "tough-cookie";
+import * as path from "path";
+import { promises as fsp } from "fs";
+import { app } from "electron";
 import { diMainGet } from "../di";
+import { ok } from "assert";
 
 let fetchLocal: typeof nodeFetch;
 let cookieJar: tougth.CookieJar;
 
 const CONFIGREPOSITORY_COOKIEJAR = "CONFIGREPOSITORY_COOKIEJAR";
+
+const userDataPath = app.getPath("userData");
+const DEFAULTS_FILENAME = "cookiejar.json";
+const defaultsFilePath = path.join(
+    userDataPath,
+    DEFAULTS_FILENAME,
+);
 
 export const cleanCookieJar = async () => {
 
@@ -26,14 +37,14 @@ export const cleanCookieJar = async () => {
 // src/main/redux/sagas/app.ts
 export const fetchCookieJarPersistence = async () => {
 
-    if (cookieJar) {
+    // const configRepo = diMainGet("config-repository");
+    // await configRepo.save({
+    //     identifier: CONFIGREPOSITORY_COOKIEJAR,
+    //     value: cookieJar.serializeSync(),
+    // });
 
-        const configRepo = diMainGet("config-repository");
-        await configRepo.save({
-            identifier: CONFIGREPOSITORY_COOKIEJAR,
-            value: cookieJar.serializeSync(),
-        });
-    }
+    const str = JSON.stringify(cookieJar.serializeSync());
+    return fsp.writeFile(defaultsFilePath, str, { encoding: "utf8" });
 };
 
 const fetchFactory = async () => {
@@ -41,10 +52,13 @@ const fetchFactory = async () => {
     await tryCatch(async () => {
 
         const configRepo = diMainGet("config-repository");
-        const data = await configRepo.get(CONFIGREPOSITORY_COOKIEJAR);
-        if (data?.value) {
-            cookieJar = tougth.CookieJar.deserializeSync(data.value);
+
+        let data = await tryCatch(() => fsp.readFile(defaultsFilePath, { encoding: "utf8" }), "");
+        if (!data) {
+            data = (await configRepo.get(CONFIGREPOSITORY_COOKIEJAR))?.value;
         }
+        ok(data, "NO COOKIE JAR FOUND ON FS");
+        cookieJar = tougth.CookieJar.deserializeSync(data);
 
     }, "src/main/network/fetch");
 
