0c40d4d9c15f065398eba38283eec62fad5845c2
Daniel Weck <daniel.weck@gmail.com>
2021-11-23 22:05:24 +0000

    chore(dev): added regex replace to nuke integrity check on Divina

--- scripts/package-lock-patch.js
+++ scripts/package-lock-patch.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 let pkg = fs.readFileSync('package-lock.json', { encoding: 'utf8' });
 pkg = pkg.replace(/"git\+ssh:\/\/git@github\.com\//g, '"git+https://github.com/');
+pkg = pkg.replace(/(\/readium\/divina-player-js\.git[^"]+",)[\r\n\s]+"integrity": "[^"]+",$/gm, "$1");
 fs.writeFileSync('package-lock.json', pkg, { encoding: 'utf8' });
