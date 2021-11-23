34458d340855813c7baf2a95764eba1d4e0ae4b5
Daniel Weck <daniel.weck@gmail.com>
2021-11-23 20:48:29 +0000

    chore(dev): GitHub Actions Windows CI fix attempt, this time via package-lock.json patching

---
+++ scripts/package-lock-patch.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+let pkg = fs.readFileSync("package-lock.json", { encoding: 'utf8' });
+pkg = pkg.replace(/"git+ssh:\/\/git@github.com\//g, "\"https://github.com/");
+fs.writeFileSync("package-lock.json", pkg, { encoding: 'utf8' });
