b2e14729ece74ee299de97b8f501c06892d90f49
Daniel Weck <daniel.weck@gmail.com>
2021-11-23 21:03:35 +0000

    chore(dev): Windows CI package-lock.json patch update (another fix attempt)

--- scripts/package-lock-patch.js
+++ scripts/package-lock-patch.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-let pkg = fs.readFileSync("package-lock.json", { encoding: 'utf8' });
-pkg = pkg.replace(/"git+ssh:\/\/git@github.com\//g, "\"https://github.com/");
-fs.writeFileSync("package-lock.json", pkg, { encoding: 'utf8' });
+let pkg = fs.readFileSync('package-lock.json', { encoding: 'utf8' });
+pkg = pkg.replace(/"git\+ssh:\/\/git@github\.com\//g, '"git+https://github.com/');
+fs.writeFileSync('package-lock.json', pkg, { encoding: 'utf8' });
