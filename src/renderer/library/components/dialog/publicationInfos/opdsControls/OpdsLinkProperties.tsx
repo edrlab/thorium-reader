09ae89c0585b91556cc8ca77fbbcb3f288c6cfd5
Pierre Leroux <panacpp@gmail.com>
2022-02-04 12:15:55 +0100

    fix(OPDS): presentation of borrow links and other publication acquisition details (PR #1627 Fixes #1563 )

--- src/renderer/library/components/dialog/publicationInfos/opdsControls/OpdsLinkProperties.tsx
+++ src/renderer/library/components/dialog/publicationInfos/opdsControls/OpdsLinkProperties.tsx
@@ -14,6 +14,7 @@ import {
 } from "readium-desktop/renderer/common/components/hoc/translator";
 
 import { OPDSAvailabilityEnum } from "@r2-opds-js/opds/opds2/opds2-availability";
+import { findMimeTypeWithExtension, ADOBE_ADEPT_XML } from "readium-desktop/utils/mimeTypes";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IBaseProps extends TranslatorProps {
@@ -53,6 +54,10 @@ class OpdsLinkProperties extends React.Component<IProps, undefined> {
 
         return (
             <>
+                {
+                    properties.indirectAcquisitionType === findMimeTypeWithExtension(ADOBE_ADEPT_XML) ?
+                    <><span title={properties.indirectAcquisitionType.replace(/application\//, "")} style={{textDecoration: "line-through"}}>(Adobe Adept)</span><br /><br /></> : <></>
+                }
                 {
                     metadataLineComponent(__("catalog.opds.info.numberOfItems"), properties.numberOfItems)
                 }
