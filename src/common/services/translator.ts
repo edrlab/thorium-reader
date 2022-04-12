cb8dfcf548ef2e0f6f525102256d54cfde01d010
Daniel Weck <daniel.weck@gmail.com>
2022-04-12 18:49:35 +0200

    added missing CA lang

--- src/common/services/translator.ts
+++ src/common/services/translator.ts
@@ -23,6 +23,7 @@ import ruCatalog from "readium-desktop/resources/locales/ru.json";
 import zhCnCatalog from "readium-desktop/resources/locales/zh-cn.json";
 import koCatalog from "readium-desktop/resources/locales/ko.json";
 import svCatalog from "readium-desktop/resources/locales/sv.json";
+import caCatalog from "readium-desktop/resources/locales/ca.json";
 
 import { TFunction } from "readium-desktop/typings/en.translation";
 
@@ -98,6 +99,9 @@ i18nextInstance.init({
         },
         "sv": {
             translation: svCatalog,
+        },
+        "ca": {
+            translation: caCatalog,
         },
     },
     // lng: undefined,
