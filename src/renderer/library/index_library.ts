c76d043dce23154497c5ad8f6ce2902e66a4b6c0
Pierre Leroux <panacpp@gmail.com>
2023-09-18 16:38:34 +0200

    lint

--- src/renderer/library/index_library.ts
+++ src/renderer/library/index_library.ts
@@ -45,7 +45,7 @@ if (IS_DEV) {
     // devTron = require("devtron");
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    axe = require('@axe-core/react');
+    axe = require("@axe-core/react");
 }
 
 initGlobalConverters_OPDS();
--- src/renderer/reader/index_reader.ts
+++ src/renderer/reader/index_reader.ts
@@ -29,7 +29,7 @@ if (IS_DEV) {
     // devTron = require("devtron");
 
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    axe = require('@axe-core/react');
+    axe = require("@axe-core/react");
 }
 
 initGlobalConverters_OPDS();
