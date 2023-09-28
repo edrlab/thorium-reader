67dde71dab7743bf6013bd8bd762ad1e2dd7d23a
Pierre Leroux <panacpp@gmail.com>
2023-09-29 00:45:56 +0200

    style

---
+++ src/renderer/assets/styles/components/alert.modals.css.d.ts
@@ -0,0 +1,16 @@
+declare const styles: {
+  readonly "AlertDialogOverlay": string;
+  readonly "overlayShow": string;
+  readonly "AlertDialogContent": string;
+  readonly "contentShow": string;
+  readonly "AlertDialogTitle": string;
+  readonly "AlertDialogDescription": string;
+  readonly "Button": string;
+  readonly "violet": string;
+  readonly "red": string;
+  readonly "mauve": string;
+  readonly "light": string;
+  readonly "light-theme": string;
+};
+export = styles;
+
