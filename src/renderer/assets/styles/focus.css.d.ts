afa269fa7f68287b21c7d639014447ce2aeb5379
Arthur Le Meur <arthur@edrlab.org>
2023-11-13 17:02:29 +0100

    add scss typed cli in style-typed cmd

--- src/renderer/assets/styles/focus.css.d.ts
+++ src/renderer/assets/styles/focus.css.d.ts
@@ -1,10 +1,10 @@
 declare const styles: {
-  readonly "R2_CSS_CLASS__KEYBOARD_INTERACT": string;
   readonly "R2_CSS_CLASS__FORCE_NO_FOCUS_OUTLINE": string;
+  readonly "R2_CSS_CLASS__FORCE_NO_TARGET_OUTLINE": string;
+  readonly "R2_CSS_CLASS__KEYBOARD_INTERACT": string;
+  readonly "___DEBUG___FOCUS_CSS": string;
   readonly "body_element": string;
   readonly "readium2ElectronAnimation_TARGET": string;
-  readonly "R2_CSS_CLASS__FORCE_NO_TARGET_OUTLINE": string;
-  readonly "___DEBUG___FOCUS_CSS": string;
 };
 export = styles;
 
