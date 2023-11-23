394d32ed4ba9edbf661166ede195252803b53640
Arthur Le Meur <arthur@edrlab.org>
2023-11-23 15:50:48 +0100

    fix svg + main position for radix select

--- src/renderer/common/components/settings/LanguageSettings.tsx
+++ src/renderer/common/components/settings/LanguageSettings.tsx
@@ -43,10 +43,10 @@ interface IBaseProps extends TranslatorProps {
 const SelectItem = React.forwardRef(({ children, ...props }: any, forwardedRef) => {
     return (
         <Select.Item {...props} id="itemInner" ref={forwardedRef}
-        className={stylesSettings.select_item}>
+            className={stylesSettings.select_item}>
             <Select.ItemText>{children}</Select.ItemText>
             <Select.ItemIndicator className={stylesSettings.select_icon}>
-                <SVG svg={DoneIcon} ariaHidden/>
+                <SVG svg={DoneIcon} ariaHidden />
             </Select.ItemIndicator>
         </Select.Item>
     );
@@ -82,32 +82,32 @@ interface AvailableLanguages {
 
 
 const LanguageSettings = (props: any) => {
-    const [ __ ] = useTranslator();
+    const [__] = useTranslator();
     const currentLanguage: keyof AvailableLanguages = props.locale;
     return (
-            <section className={stylesSettings.settings_tab_container}>
-                <div className={stylesGlobal.heading}>
-                    <h4>{__("settings.language.languageChoice")}</h4>
-                </div>
-                <Select.Root onValueChange={(lang) => props.setLocale(lang)}>
-                    <Select.Trigger  className={stylesSettings.select_trigger}>
-                        <div>
-                            <Select.Icon className={stylesSettings.select_icon}><SVG ariaHidden={true} svg={LanguageIcon} /></Select.Icon>
-                            <Select.Value placeholder={AvailableLanguages[currentLanguage]} />
-                        </div>
-                        <Select.Icon className={stylesSettings.select_icon}><SVG ariaHidden={true} svg={ChevronDown} /></Select.Icon>
-                    </Select.Trigger>
-                    <Select.Portal>
-                        <Select.Content className={stylesSettings.select_content} position="popper" sideOffset={10} sticky="always">
-                            <Select.Viewport role="select">
-                            { ObjectKeys(AvailableLanguages).map((lang, i) =>
-                                <SelectItem value={lang} key={i}>{ AvailableLanguages[lang] }</SelectItem>,
+        <section className={stylesSettings.settings_tab_container}>
+            <div className={stylesGlobal.heading}>
+                <h4>{__("settings.language.languageChoice")}</h4>
+            </div>
+            <Select.Root onValueChange={(lang) => props.setLocale(lang)}>
+                <Select.Trigger className={stylesSettings.select_trigger}>
+                    <div>
+                        <Select.Icon className={stylesSettings.select_icon}><SVG ariaHidden={true} svg={LanguageIcon} /></Select.Icon>
+                        <Select.Value placeholder={AvailableLanguages[currentLanguage]} />
+                    </div>
+                    <Select.Icon className={stylesSettings.select_icon}><SVG ariaHidden={true} svg={ChevronDown} /></Select.Icon>
+                </Select.Trigger>
+                <Select.Portal>
+                    <Select.Content className={stylesSettings.select_content} position="popper" sideOffset={10} sticky="always">
+                        <Select.Viewport role="select">
+                            {ObjectKeys(AvailableLanguages).map((lang, i) =>
+                                <SelectItem value={lang} key={i}>{AvailableLanguages[lang]}</SelectItem>,
                             )}
-                            </Select.Viewport>
-                        </Select.Content>
-                        </Select.Portal>
-                </Select.Root>
-            </section>
+                        </Select.Viewport>
+                    </Select.Content>
+                </Select.Portal>
+            </Select.Root>
+        </section>
     );
 };
 
