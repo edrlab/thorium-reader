5d2aa4df96e58e2ca8cae35bf66cdce16513bf9a
Pierre Leroux <panacpp@gmail.com>
2023-11-23 12:45:07 +0100

    chore(sass): move typed-scss to devDep in package.json and run typed-css in style files
    clone https://github.com/panaC/typed-scss-modules to my personal github
    repos to update the module to node-sass version 9.0.0 (PR
    https://github.com/skovy/typed-scss-modules/pull/214)

--- src/renderer/assets/styles/components/blocks.css.d.ts
+++ src/renderer/assets/styles/components/blocks.css.d.ts
@@ -1,14 +1,14 @@
 declare const styles: {
-  readonly "block_line": string;
-  readonly "block_line_inactive": string;
-  readonly "description_see_more": string;
-  readonly "block_line_edit": string;
+  readonly "___DEBUG___COMPONENTS_BLOCKS_CSS": string;
+  readonly "block_dashed": string;
   readonly "block_full": string;
-  readonly "block_full_wrapper": string;
   readonly "block_full_close": string;
   readonly "block_full_update": string;
-  readonly "block_dashed": string;
-  readonly "___DEBUG___COMPONENTS_BLOCKS_CSS": string;
+  readonly "block_full_wrapper": string;
+  readonly "block_line": string;
+  readonly "block_line_edit": string;
+  readonly "block_line_inactive": string;
+  readonly "description_see_more": string;
 };
 export = styles;
 
