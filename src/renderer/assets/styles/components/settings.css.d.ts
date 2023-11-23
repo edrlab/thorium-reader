c1dcb86fe0e53097249f5aeb38df4ecf6011f637
Pierre Leroux <panacpp@gmail.com>
2023-11-23 09:51:48 +0100

    up style-typed

--- src/renderer/assets/styles/components/settings.css.d.ts
+++ src/renderer/assets/styles/components/settings.css.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
-  readonly "settings_container": string;
-  readonly "settings_tabslist": string;
-  readonly "settings_tab_container": string;
-  readonly "select_language_trigger": string;
+  readonly "select_language_content": string;
   readonly "select_language_icon": string;
   readonly "select_language_item": string;
-  readonly "select_language_content": string;
+  readonly "select_language_trigger": string;
+  readonly "settings_container": string;
+  readonly "settings_tab_container": string;
+  readonly "settings_tabslist": string;
 };
 export = styles;
 
--- src/renderer/assets/styles/components/themes.css.d.ts
+++ src/renderer/assets/styles/components/themes.css.d.ts
@@ -1,9 +1,9 @@
 declare const styles: {
-  readonly "themes_container": string;
-  readonly "theme_preview_circle": string;
+  readonly "auto_theme": string;
   readonly "dark_theme": string;
   readonly "light_theme": string;
-  readonly "auto_theme": string;
+  readonly "theme_preview_circle": string;
+  readonly "themes_container": string;
 };
 export = styles;
 
