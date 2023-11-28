e100169d14c126efd0f35b2e0222b6f8648ef700
Arthur Le Meur <arthur@edrlab.org>
2023-11-28 14:48:04 +0100

    upgrade display layout component

--- src/renderer/common/components/settings/Reading/ReadingDisplayLayout.tsx
+++ src/renderer/common/components/settings/Reading/ReadingDisplayLayout.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { useTranslator } from "readium-desktop/renderer/common/hooks/useTranslator";
 import * as stylesSettings from "readium-desktop/renderer/assets/styles/components/settings.scss";
-
-
-import SVG from "readium-desktop/renderer/common/components/SVG";
 import * as ScrollableIcon from "readium-desktop/renderer/assets/icons/Scrollable-icon.svg";
 import * as PaginatedIcon from "readium-desktop/renderer/assets/icons/paginated-icon.svg";
 import { useSaveConfig } from "./useSaveConfig";
 import { useSelector } from "readium-desktop/renderer/common/hooks/useSelector";
 import { ICommonRootState } from "readium-desktop/common/redux/states/commonRootState";
+import { SettingsRadioBtnTemplate, TOptions } from "./SettingsRadioBtnTemplate";
 
 
 const ReadingDisplayLayout = () => {
@@ -16,15 +14,31 @@ const ReadingDisplayLayout = () => {
     const saveConfigDebounced = useSaveConfig();
     const layout = useSelector((s: ICommonRootState) => s.reader.defaultConfig.paged);
 
-    React.useEffect(() => {
-        const page_option = document.getElementById("page_option") as HTMLInputElement;
-        const scroll_option = document.getElementById("scroll_option") as HTMLInputElement;
-        if (layout) {
-            page_option.checked = true;
-        } else {
-            scroll_option.checked = true;
+
+    const options: TOptions = [
+        {
+            id: "scroll_option",
+            onChange: () => saveConfigDebounced({ paged: false }),
+            svg: ScrollableIcon,
+            description: `${__("reader.settings.scrolled")}`,
+            checked: (!layout),
+            disabled: false,
+            name: "pagination",
+        },
+        {
+            id: "page_option",
+            onChange: () => saveConfigDebounced({ paged: true }),
+            svg: PaginatedIcon,
+            description: `${__("reader.settings.paginated")}`,
+            checked: (layout),
+            disabled: false,
+            name: "pagination",
         }
-    }, []);
+    ]
+
+    const List = () => {
+        return options.map((o) => <SettingsRadioBtnTemplate key={o.id} {...o} />);
+    };
 
 
     return (
@@ -33,16 +47,7 @@ const ReadingDisplayLayout = () => {
                 <h4>{__("reader.settings.disposition.title")}</h4>
             </div>
             <div className={stylesSettings.display_options}>
-                <input id="scroll_option" type="radio" name="disposition" onClick={() => saveConfigDebounced({ paged: false })} />
-                <label className={stylesSettings.display_options_item} htmlFor="scroll_option">
-                    <SVG ariaHidden svg={ScrollableIcon} />
-                    <p>{__("reader.settings.scrolled")}</p>
-                </label>
-                <input id="page_option" type="radio" name="disposition" onClick={() => saveConfigDebounced({ paged: true })} />
-                <label className={stylesSettings.display_options_item} htmlFor="page_option">
-                    <SVG ariaHidden svg={PaginatedIcon} />
-                    <p>{__("reader.settings.paginated")}</p>
-                </label>
+                <List />
             </div>
         </section>
     );
