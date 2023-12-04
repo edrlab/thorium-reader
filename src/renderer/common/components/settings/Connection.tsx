78dbcfd03298bc3f06e36f0fc206f2e57b909ba5
Pierre Leroux <panacpp@gmail.com>
2023-12-04 15:45:46 +0100

    fix:  saveSession and wipeAuth

--- src/renderer/common/components/settings/Connection.tsx
+++ src/renderer/common/components/settings/Connection.tsx
@@ -8,9 +8,51 @@
 import * as React from "react";
 import { useTranslator } from "../../hooks/useTranslator";
 import * as stylesSettings from "readium-desktop/renderer/assets/styles/components/settings.scss";
-import { Session } from "./SessionSettings";
-import { Auth } from "./AuthSettings";
+import * as BinIcon from "readium-desktop/renderer/assets/icons/bin-icon.svg";
+import { useDispatch } from "../../hooks/useDispatch";
+import { authActions, sessionActions } from "readium-desktop/common/redux/actions";
+import SVG from "../SVG";
+import { useSelector } from "../../hooks/useSelector";
+import { IRendererCommonRootState } from "readium-desktop/common/redux/states/rendererCommonRootState";
 
+export const Auth = () => {
+    const [__] = useTranslator();
+    const dispatch = useDispatch();
+
+    return (
+        <button
+            className={stylesSettings.btn_primary}
+            onClick={() => dispatch(authActions.wipeData.build())}>
+            <SVG ariaHidden svg={BinIcon} />
+            {__("settings.auth.wipeData")}
+        </button>
+    );
+};
+
+export const Session = () => {
+    const [__] = useTranslator();
+    const dispatch = useDispatch();
+    const sessionState = useSelector((state: IRendererCommonRootState) => state.session.state);
+
+    return (
+        <>
+            <form>
+                <input
+                    id={"session-true"}
+                    type="checkbox"
+                    lang={__("settings.session.yes")}
+                    name="language"
+                    defaultChecked={sessionState}
+                    onChange={(e) => dispatch(sessionActions.enable.build(e.target.checked))}
+                />
+                <label htmlFor={"session-true"}>{__("settings.session.title")}</label>
+            </form>
+            <div className={stylesSettings.session_text}>
+                <p>{__("settings.session.description")}</p>
+            </div>
+        </>
+    );
+};
 
 const ConnectionSettings: React.FC<{}> = () => {
     const [__] = useTranslator();
