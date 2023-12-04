91eeaacafe3c826bc395f28eefff610b56db6a08
Pierre Leroux <panacpp@gmail.com>
2023-12-04 12:07:29 +0100

    catalogmenu to fc

--- src/renderer/library/components/publication/menu/CatalogMenu.tsx
+++ src/renderer/library/components/publication/menu/CatalogMenu.tsx
@@ -6,95 +6,42 @@
 // ==LICENSE-END==
 
 import * as React from "react";
-// import { connect } from "react-redux";
-// import { DialogTypeName } from "readium-desktop/common/models/dialog";
-// import * as dialogActions from "readium-desktop/common/redux/actions/dialog";
 import { PublicationView } from "readium-desktop/common/views/publication";
-import {
-    TranslatorProps, withTranslator,
-} from "readium-desktop/renderer/common/components/hoc/translator";
-// import { TDispatch } from "readium-desktop/typings/redux";
-
 import PublicationExportButton from "./PublicationExportButton";
 import DeletePublicationConfirm from "../../dialog/DeletePublicationConfirm";
 import { PublicationInfoLibWithRadix, PublicationInfoLibWithRadixContent, PublicationInfoLibWithRadixTrigger } from "../../dialog/publicationInfos/PublicationInfo";
+import { useTranslator } from "readium-desktop/renderer/common/hooks/useTranslator";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IBaseProps extends TranslatorProps {
-    publicationView: PublicationView;
-}
-// IProps may typically extend:
-// RouteComponentProps
-// ReturnType<typeof mapStateToProps>
-// ReturnType<typeof mapDispatchToProps>
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IProps extends IBaseProps {//, ReturnType<typeof mapDispatchToProps> {
-}
+const CatalogMenu: React.FC<{publicationView: PublicationView}> = (props) => {
+    const [__] = useTranslator();
 
-interface IState {
-    menuOpen: boolean;
-}
+    return (
+        <>
+            <PublicationInfoLibWithRadix
+                publicationView={props.publicationView}
+            >
+                <PublicationInfoLibWithRadixTrigger asChild>
+                    <button
+                    >
+                        {__("catalog.bookInfo")}
+                    </button>
+                </PublicationInfoLibWithRadixTrigger>
+                <PublicationInfoLibWithRadixContent />
+            </PublicationInfoLibWithRadix>
+            <DeletePublicationConfirm
+                trigger={(
+                    <button
+                    >
+                        {__("catalog.delete")}
+                    </button>
+                )}
+                publicationView={props.publicationView}
+            />
+            <PublicationExportButton
+                publicationView={props.publicationView}
+            />
+        </>
+    );
+};
 
-export class CatalogMenu extends React.Component<IProps, IState> {
-
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-            menuOpen: false,
-        };
-
-        // this.deletePublication = this.deletePublication.bind(this);
-        // this.displayPublicationInfo = this.displayPublicationInfo.bind(this);
-    }
-
-    public render(): React.ReactElement<{}> {
-        const { __ } = this.props;
-        return (
-            <>
-                <PublicationInfoLibWithRadix
-                    publicationView={this.props.publicationView}
-                >
-                    <PublicationInfoLibWithRadixTrigger asChild>
-                        <button
-                        >
-                            {__("catalog.bookInfo")}
-                        </button>
-                    </PublicationInfoLibWithRadixTrigger>
-                    <PublicationInfoLibWithRadixContent />
-                </PublicationInfoLibWithRadix>
-                <DeletePublicationConfirm
-                    trigger={(
-                        <button
-                        >
-                            {__("catalog.delete")}
-                        </button>
-                    )}
-                    publicationView={this.props.publicationView}
-                />
-                <PublicationExportButton
-                    publicationView={this.props.publicationView}
-                />
-            </>
-        );
-    }
-
-    // private displayPublicationInfo() {
-    //     this.props.displayPublicationInfo();
-    // }
-}
-
-// const mapDispatchToProps = (dispatch: TDispatch, props: IBaseProps) => {
-//     return {
-//         displayPublicationInfo: () => {
-//             dispatch(dialogActions.openRequest.build(DialogTypeName.PublicationInfoLib,
-//                 {
-//                     publicationIdentifier: props.publicationView.identifier,
-//                 },
-//             ));
-//         },
-//     };
-// };
-
-// export default connect(undefined, mapDispatchToProps)(withTranslator(CatalogMenu));
-export default withTranslator(CatalogMenu);
+export default (CatalogMenu);
