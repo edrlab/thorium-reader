f13b957289687827cfe538d334cdd887c911a153
Arthur Le Meur <arthur@edrlab.org>
2024-01-05 12:00:11 +0100

    add PopoverContent class

--- src/renderer/assets/styles/components/dropdown.css.d.ts
+++ src/renderer/assets/styles/components/dropdown.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "PopoverArrow": string;
+  readonly "PopoverContent": string;
   readonly "___DEBUG___COMPONENTS_DROPDOWN_CSS": string;
   readonly "dropdown_menu": string;
   readonly "dropdown_publication": string;
