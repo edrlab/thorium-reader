3a23a7ae6fb0971a9540afc966aedc5e5c59d173
Pierre Leroux <panacpp@gmail.com>
2024-01-05 14:19:49 +0100

    clean

--- src/renderer/library/components/dialog/DeleteOpdsFeedConfirm.tsx
+++ src/renderer/library/components/dialog/DeleteOpdsFeedConfirm.tsx
@@ -6,15 +6,7 @@
 // ==LICENSE-END==
 
 import * as React from "react";
-// import { connect } from "react-redux";
 import * as AlertDialog from "@radix-ui/react-alert-dialog";
-// import { DialogType, DialogTypeName } from "readium-desktop/common/models/dialog";
-// import Dialog from "readium-desktop/renderer/common/components/dialog/Dialog";
-// import {
-//     TranslatorProps, withTranslator,
-// } from "readium-desktop/renderer/common/components/hoc/translator";
-// import { apiAction } from "readium-desktop/renderer/library/apiAction";
-// import { ILibraryRootState } from "readium-desktop/common/redux/states/renderer/libraryRootState";
 import { IOpdsFeedView } from "readium-desktop/common/views/opds";
 import { useTranslator } from "readium-desktop/renderer/common/hooks/useTranslator";
 import { useApi } from "readium-desktop/renderer/common/hooks/useApi";
@@ -22,59 +14,6 @@ import { useDispatch } from "react-redux";
 import { dialogActions } from "readium-desktop/common/redux/actions";
 import * as stylesAlertModals from "readium-desktop/renderer/assets/styles/components/alert.modals.css";
 import classNames from "classnames";
-
-// // eslint-disable-next-line @typescript-eslint/no-empty-interface
-// interface IBaseProps extends TranslatorProps {
-// }
-// // IProps may typically extend:
-// // RouteComponentProps
-// // ReturnType<typeof mapStateToProps>
-// // ReturnType<typeof mapDispatchToProps>
-// // eslint-disable-next-line @typescript-eslint/no-empty-interface
-// interface IProps extends IBaseProps, ReturnType<typeof mapStateToProps> {
-// }
-
-// class DeleteOpdsFeedConfirm extends React.Component<IProps, undefined> {
-
-//     constructor(props: IProps) {
-//         super(props);
-//     }
-
-//     public render(): React.ReactElement<{}> {
-//         if (!this.props.open || !this.props.feed) {
-//             return (<></>);
-//         }
-
-//         const { __ } = this.props;
-//         return (
-//             <Dialog
-//                 title={__("dialog.deleteFeed")}
-//                 onSubmitButton={this.remove}
-//                 submitButtonDisabled={false}
-//                 submitButtonTitle={this.props.__("dialog.yes")}
-//                 shouldOkRefEnabled={true}
-//                 size={"small"}
-//             >
-//                 <p>
-//                     <span>{this.props.feed.title}</span>
-//                 </p>
-//             </Dialog>
-//         );
-//     }
-
-//     private remove = () => {
-//         apiAction("opds/deleteFeed", this.props.feed.identifier).catch((error) => {
-//             console.error("Error to fetch opds/deleteFeed", error);
-//         });
-//     };
-// }
-
-// const mapStateToProps = (state: ILibraryRootState, _props: IBaseProps) => ({
-//     open: state.dialog.type === DialogTypeName.DeleteOpdsFeedConfirm,
-//     feed: (state.dialog.data as DialogType[DialogTypeName.DeleteOpdsFeedConfirm]).feed,
-// });
-
-// export default connect(mapStateToProps)(withTranslator(DeleteOpdsFeedConfirm));
 
 const DeleteOpdsFeedConfirm = (props: { feed: IOpdsFeedView, trigger: React.ReactNode } & AlertDialog.AlertDialogProps) => {
     const [__] = useTranslator();
@@ -85,16 +24,12 @@ const DeleteOpdsFeedConfirm = (props: { feed: IOpdsFeedView, trigger: React.Reac
         remove(props.feed.identifier);
     }, [remove, props.feed.identifier]);
 
-    const appOverlayElement = React.useMemo(() => document.getElementById("app-overlay"), []);
     return (
         <AlertDialog.Root {...props}>
             <AlertDialog.Trigger asChild>
                 {props.trigger}
             </AlertDialog.Trigger>
-            <AlertDialog.Portal container={appOverlayElement}>
-
-                {/** Overlay Component doesn't work */}
-                {/* <AlertDialog.Overlay className={stylesAlertModals.AlertDialogOverlay}/> */}
+            <AlertDialog.Portal>
                 <div className={stylesAlertModals.AlertDialogOverlay}></div>
                 <AlertDialog.Content className={stylesAlertModals.AlertDialogContent}>
                     <AlertDialog.Title className={stylesAlertModals.AlertDialogTitle}>{__("dialog.deleteFeed")}</AlertDialog.Title>
