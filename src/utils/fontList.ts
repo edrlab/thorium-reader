5df5085b364dd99e784cc22a4ef2491de8ac0c67
Daniel Weck <daniel.weck@gmail.com>
2024-02-21 11:42:18 +0000

    hotfix(lint): double quotes

--- src/utils/fontList.ts
+++ src/utils/fontList.ts
@@ -56,18 +56,22 @@ const FONT_LIST_JA: Font[] =
 [{
     id: "JA",
     label: "日本語 SERIF (H)",
+    // eslint-disable-next-line quotes
     fontFamily: `"ＭＳ Ｐ明朝", "MS PMincho", "Hiragino Mincho Pro", "ヒラギノ明朝 Pro W3", "游明朝", "YuMincho", "ＭＳ 明朝", "MS Mincho", "Hiragino Mincho ProN", serif`,
 }, {
     id: "JA-SANS",
     label: "日本語 SANS (H)",
+    // eslint-disable-next-line quotes
     fontFamily: `"ＭＳ Ｐゴシック", "MS PGothic", "Hiragino Kaku Gothic Pro W3", "ヒラギノ角ゴ Pro W3", "Hiragino Sans GB", "ヒラギノ角ゴシック W3", "游ゴシック", "YuGothic", "ＭＳ ゴシック", "MS Gothic", "Hiragino Sans", sans-serif`,
 }, {
     id: "JA-V",
     label: "日本語 SERIF (V)",
+    // eslint-disable-next-line quotes
     fontFamily: `"ＭＳ 明朝", "MS Mincho", "Hiragino Mincho Pro", "ヒラギノ明朝 Pro W3", "游明朝", "YuMincho", "ＭＳ Ｐ明朝", "MS PMincho", "Hiragino Mincho ProN", serif`,
 }, {
     id: "JA-V-SANS",
     label: "日本語 SANS (V)",
+    // eslint-disable-next-line quotes
     fontFamily: `"ＭＳ ゴシック", "MS Gothic", "Hiragino Kaku Gothic Pro W3", "ヒラギノ角ゴ Pro W3", "Hiragino Sans GB", "ヒラギノ角ゴシック W3", "游ゴシック", "YuGothic", "ＭＳ Ｐゴシック", "MS PGothic", "Hiragino Sans", sans-serif`,
 }];
 
