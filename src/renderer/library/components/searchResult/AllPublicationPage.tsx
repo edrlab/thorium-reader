873cf2bcfc36b0c572865eb9a3b98e5db23b8912
Daniel Weck <daniel.weck@gmail.com>
2024-05-21 12:07:51 +0100

    hyperlink href

--- src/renderer/library/components/searchResult/AllPublicationPage.tsx
+++ src/renderer/library/components/searchResult/AllPublicationPage.tsx
@@ -1168,6 +1168,8 @@ const CellTitle: React.FC<ITableCellProps_Column & ITableCellProps_GenericCell &
                     className={stylesPublication.cell_bookTitle}
                     onClick={() => props.openReader(props.value.publicationViewIdentifier)}
                     onKeyUp={(e) => {
+                        // ALTERNATIVE IMPLEMENTATION:
+                        // href="" ==> automatically sets up ENTER key and keyboard tab, but also requires preventDefault inside onClick (otherwise user can hit the options/alt key to download the href current location!), and introduces hyperlin visited style so CSS must account for this!
                         if (e.key === "Enter") {
                             props.openReader(props.value.publicationViewIdentifier);
                         }
