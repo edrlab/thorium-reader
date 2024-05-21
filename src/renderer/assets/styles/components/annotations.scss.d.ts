41b62ed127ce571b2aa638c9cc07ae0668229b20
Arthur Le Meur <arthur@edrlab.org>
2024-05-21 13:10:19 +0200

    fix responsive in annotations edit style

--- src/renderer/assets/styles/components/annotations.scss.d.ts
+++ src/renderer/assets/styles/components/annotations.scss.d.ts
@@ -27,5 +27,6 @@ export declare const R2_CSS_CLASS__FORCE_NO_FOCUS_OUTLINE: string;
 export declare const R2_CSS_CLASS__FORCE_NO_TARGET_OUTLINE: string;
 export declare const R2_CSS_CLASS__KEYBOARD_INTERACT: string;
 export declare const readium2ElectronAnimation_TARGET: string;
+export declare const settings_tab: string;
 export declare const stylePicker: string;
 export declare const update_form: string;
