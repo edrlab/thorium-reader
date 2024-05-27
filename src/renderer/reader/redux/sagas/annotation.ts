edrlab/thorium-reader

commit 0dfbd187aa802bdb12ac07a44a4a53207b4166bf

fix: annotations skip in Redux Saga for non-EPUB (PDF, Divina, AudioBooks), acts as code guard for navigator global API (notably, margin conf):

src/renderer/reader/redux/sagas/annotation.ts
@@ -200,7 +200,13 @@ function* readerStart() {
         take(winActions.initSuccess.build),
     ]);
 
-    debug("annotation iframe reader viewport is started and ready to annotate, we draws all the annoatation for the first time with 'highlightsDrawMargin' enabled");
+    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+        locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
+        info?.publicationView?.isPDF;
+    if (skip) return;
+
+    debug("annotation iframe reader viewport is started and ready to annotate, we draws all the annotation for the first time with 'highlightsDrawMargin' enabled");
 
     const { annotation_defaultDrawView } = yield* selectTyped((state: IReaderRootState) => state.reader.config);
     if (annotation_defaultDrawView === "margin") {
@@ -223,6 +229,13 @@ function* readerStart() {
 }
 
 function* captureHightlightDrawMargin(action: readerLocalActionSetConfig.TAction) {
+
+    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+        locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
+        info?.publicationView?.isPDF;
+    if (skip) return;
+
     const { annotation_defaultDrawView } = action.payload;
 
     debug(`captureHightlightDrawMargin : readerLocalActionSetConfig CHANGED apply=${annotation_defaultDrawView}`);

src/renderer/reader/redux/sagas/highlight/handler.ts
@@ -73,6 +73,12 @@ function* pop(action: readerLocalActionHighlights.handler.pop.TAction) {
 
 function* hrefChanged(action: readerLocalActionLocatorHrefChanged.TAction) {
 
+    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+        locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
+        info?.publicationView?.isPDF;
+    if (skip) return;
+
     debug(`hrefChanged (unmountHightlight+mountHighlight) -- action.payload: [${JSON.stringify(action.payload, null, 4)}]`);
 
     // yield* callTyped(() => highlightsDrawMargin(_drawMargin)); // should probably be invoked in mounter, not handler

commit 93377b6a19bf55e64b10ecdc9ab490ed579b7845

fix(regression): caused by https://github.com/edrlab/thorium-reader/commit/0dfbd187aa802bdb12ac07a44a4a53207b4166bf anotation highlights draw was skipped due to Divina incorrect filter check (Fixes #2360)

src/renderer/reader/redux/sagas/annotation.ts
@@ -200,11 +200,17 @@ function* readerStart() {
         take(winActions.initSuccess.build),
     ]);
 
-    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
-    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+    // divina,
+    const { info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    // typeof divina !== "undefined" ||
+    const skip = info?.publicationView?.isDivina ||
         locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
         info?.publicationView?.isPDF;
-    if (skip) return;
+    if (skip) {
+        // divina,
+        debug("readerStart SKIP annot", skip, info?.publicationView?.isDivina, locator?.audioPlaybackInfo, info?.publicationView?.isAudio, info?.publicationView?.isPDF);
+        return;
+    }
 
     debug("annotation iframe reader viewport is started and ready to annotate, we draws all the annotation for the first time with 'highlightsDrawMargin' enabled");
 
@@ -230,11 +236,17 @@ function* readerStart() {
 
 function* captureHightlightDrawMargin(action: readerLocalActionSetConfig.TAction) {
 
-    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
-    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+    // divina,
+    const { info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    // typeof divina !== "undefined" ||
+    const skip = info?.publicationView?.isDivina ||
         locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
         info?.publicationView?.isPDF;
-    if (skip) return;
+    if (skip) {
+        // divina,
+        debug("captureHightlightDrawMargin SKIP annot", skip, info?.publicationView?.isDivina, locator?.audioPlaybackInfo, info?.publicationView?.isAudio, info?.publicationView?.isPDF);
+        return;
+    }
 
     const { annotation_defaultDrawView } = action.payload;
 

src/renderer/reader/redux/sagas/highlight/handler.ts
@@ -73,11 +73,17 @@ function* pop(action: readerLocalActionHighlights.handler.pop.TAction) {
 
 function* hrefChanged(action: readerLocalActionLocatorHrefChanged.TAction) {
 
-    const { divina, info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
-    const skip = typeof divina !== "undefined" || info?.publicationView?.isDivina ||
+    // divina,
+    const { info, locator } = yield* selectTyped((state: IReaderRootState) => state.reader);
+    // typeof divina !== "undefined" ||
+    const skip = info?.publicationView?.isDivina ||
         locator?.audioPlaybackInfo || info?.publicationView?.isAudio ||
         info?.publicationView?.isPDF;
-    if (skip) return;
+    if (skip) {
+        // divina,
+        debug("hrefChanged SKIP annot", skip, info?.publicationView?.isDivina, locator?.audioPlaybackInfo, info?.publicationView?.isAudio, info?.publicationView?.isPDF);
+        return;
+    }
 
     debug(`hrefChanged (unmountHightlight+mountHighlight) -- action.payload: [${JSON.stringify(action.payload, null, 4)}]`);
 