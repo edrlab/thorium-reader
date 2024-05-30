477ffe9081fa505f33c17e0d0ce9c07caa0191f7
arthur-lemeur <113124595+arthur-lemeur@users.noreply.github.com>
2024-05-30 14:16:51 +0200

    fix(GUI): bookmarks and annotations labels in reader window header (PR #2414 Fixes #2412)

--- src/renderer/reader/components/ReaderHeader.tsx
+++ src/renderer/reader/components/ReaderHeader.tsx
@@ -929,6 +929,8 @@ export class ReaderHeader extends React.Component<IProps, IState> {
                                 aria-hidden="true"
                                 className={stylesReader.menu_button}
                                 id="bookmarkLabel"
+                                aria-label={__("reader.navigation.bookmarkTitle")}
+                                title={__("reader.navigation.bookmarkTitle")}
                             >
                                 <SVG ariaHidden={true} svg={MarkIcon} className={classNames(stylesReaderHeader.bookmarkIcon, this.props.isOnBookmark ? stylesReaderHeader.active_svg : "")} />
                                 <SVG ariaHidden={true} svg={RemoveBookMarkIcon} className={classNames(stylesReaderHeader.bookmarkRemove, this.props.isOnBookmark ? stylesReaderHeader.active_svg : "")} />
@@ -970,6 +972,8 @@ export class ReaderHeader extends React.Component<IProps, IState> {
                                         aria-hidden="true"
                                         className={stylesReader.menu_button}
                                         id="annotationLabel"
+                                        aria-label={__("reader.navigation.annotationTitle")}
+                                        title={__("reader.navigation.annotationTitle")}
                                     >
                                         <SVG ariaHidden svg={AnnotationsIcon} className={classNames(stylesReaderHeader.annotationsIcon, this.props.isAnnotationModeEnabled ? stylesReaderHeader.active_svg : "")} />
                                     </label>
