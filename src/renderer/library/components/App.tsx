51be6b749625a8d5324228b0ce68f949b9598d68
Daniel Weck <daniel.weck@gmail.com>
2024-09-24 16:16:15 +0100

    fix: lint (sorry)

--- src/renderer/library/components/App.tsx
+++ src/renderer/library/components/App.tsx
@@ -73,7 +73,7 @@ export default class App extends React.Component<{}, undefined> {
                     return {
                         name: file.name,
                         path: absolutePath,
-                    }
+                    };
                 },
             );
 
