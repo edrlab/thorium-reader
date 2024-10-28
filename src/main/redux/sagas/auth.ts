babb90ada9c3d6a86138bb0aab556006a1ab8ad7
arthur-lemeur <113124595+arthur-lemeur@users.noreply.github.com>
2024-10-28 11:21:03 +0100

    fix(opds): opds authentication css (Fixes #2396 PR #2457)

--- src/main/redux/sagas/auth.ts
+++ src/main/redux/sagas/auth.ts
@@ -931,6 +931,7 @@ const htmlLoginTemplate = (
                 display: flex;
                 align-items: center;
                 justify-content: center;
+                margin: 0;
             }
 
             .login {
@@ -948,7 +949,8 @@ const htmlLoginTemplate = (
                 position: relative;
                 gap: 50px;
                 flex-wrap: wrap;
-
+                overflow: hidden;
+    
                 @media only screen and (max-width: 1000px) {
                     flex-direction: column;
                 }
@@ -1027,7 +1029,7 @@ const htmlLoginTemplate = (
             .login form {
                 flex: 2;
                 border-bottom: 1px solid #e5e5e5;
-                width: 100%;
+                width: fit-content;
                 align-items: end;
                 justify-content: center;
                 display: flex;
@@ -1088,18 +1090,15 @@ const htmlLoginTemplate = (
 
             .login .submit {
                 text-align: right;
-                position: absolute;
+                // position: absolute;
                 bottom: 20px;
                 right: 40px;
                 display: flex;
                 align-items: center;
                 gap: 10px;
-
-                @media only screen and (max-width: 1000px) {
-                    position: unset;
-                    width: 100%;
-                    justify-content: end;
-                }
+                justify-content: end;
+                width: 100%;
+                max-width: 400px;
             }
 
             .submit_button {
@@ -1107,14 +1106,14 @@ const htmlLoginTemplate = (
                 display: flex;
                 align-items: center;
                 gap: 5px;
-                padding: 0 5px;
+                padding: 0;
             }
 
             .submit_button label {
                 position: absolute;
                 transform: translate(-50%, -50%);
                 top: 50%;
-                left: 15px;
+                left: 10px;
             }
 
             .submit_button label svg {
