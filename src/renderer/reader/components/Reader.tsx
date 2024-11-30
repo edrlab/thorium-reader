fee5fdae93061d350fb3be7aefe251cd3fe42b63
Daniel Weck <daniel.weck@gmail.com>
2024-11-30 22:06:34 +0000

    fix(TTS, MO): disable popup foonotes so that they can be played inline as intended by content creators, see https://github.com/edrlab/thorium-reader/discussions/2679

--- src/renderer/reader/components/Reader.tsx
+++ src/renderer/reader/components/Reader.tsx
@@ -2671,7 +2671,16 @@ class Reader extends React.Component<IProps, IState> {
 
     private handleTTSPlay() {
         ttsClickEnable(true);
-        ttsPlay(parseFloat(this.props.ttsPlaybackRate), this.props.ttsVoice);
+        let delay = 0;
+        if (!this.props.readerConfig?.noFootnotes) {
+            delay = 100;
+            // console.log("TTS PLAY ==> NO_FOOTNOTES MUST BE TRUE (POPUP DISABLED), SWITCHING...");
+            this.props.setConfig({ noFootnotes: true });
+            // TODO: skippability should be disabled when user explicitly "requests" a skippable item, such as when clicking on a note reference hyperlink, or even on a skippable element itself(?)
+        }
+        setTimeout(() => {
+            ttsPlay(parseFloat(this.props.ttsPlaybackRate), this.props.ttsVoice);
+        }, delay);
     }
     private handleTTSPause() {
         ttsPause();
@@ -2710,7 +2719,16 @@ class Reader extends React.Component<IProps, IState> {
 
     private handleMediaOverlaysPlay() {
         mediaOverlaysClickEnable(true);
-        mediaOverlaysPlay(parseFloat(this.props.mediaOverlaysPlaybackRate));
+        let delay = 0;
+        if (!this.props.readerConfig?.noFootnotes) {
+            delay = 100;
+            // console.log("MO PLAY ==> NO_FOOTNOTES MUST BE TRUE (POPUP DISABLED), SWITCHING...");
+            this.props.setConfig({ noFootnotes: true });
+            // TODO: skippability should be disabled when user explicitly "requests" a skippable item, such as when clicking on a note reference hyperlink, or even on a skippable element itself(?)
+        }
+        setTimeout(() => {
+            mediaOverlaysPlay(parseFloat(this.props.mediaOverlaysPlaybackRate));
+        }, delay);
     }
     private handleMediaOverlaysPause() {
         mediaOverlaysPause();
