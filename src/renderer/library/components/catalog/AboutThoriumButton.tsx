379bda9f698a1b790581c97a6f131898fdcd9385
Daniel Weck <daniel.weck@gmail.com>
2024-12-20 14:58:16 +0000

    removed unnecessary encodeUriComponent (in fact, in principle incorrect due to potentially percent-escaped characters)

--- src/renderer/library/components/catalog/AboutThoriumButton.tsx
+++ src/renderer/library/components/catalog/AboutThoriumButton.tsx
@@ -104,7 +104,7 @@ class AboutThoriumButton extends React.Component<IProps, IState> {
                 }
                 <div className={stylesFooter.footer_about}>
                     <div>
-                    <p>{`v${app_version}`}</p>
+                    <p>{`v${_APP_VERSION}`}</p>
                     <a href="" onClick={async (ev) => {
                                 ev.preventDefault(); // necessary because href="", CSS must also ensure hyperlink visited style
                                 await shell.openExternal(`https://thorium.edrlab.org/?lang=${locale}&v=${app_version}`);
