f9a00d3e7842fd5615f0b8c6b2c9b299cabaf154
Pierre Leroux <p.leroux@gmx.com>
2025-03-12 16:09:06 +0100

    feat: ctrl-shift-b edit and add a bookmark with popover

---
+++ src/renderer/assets/styles/components/bookmarks.scss.d.ts
@@ -0,0 +1,4 @@
+export declare const bookmark_edit_form: string;
+export declare const bookmark_edit_form_textarea: string;
+export declare const bookmark_edit_form_textarea_buttons: string;
+export declare const bookmark_edit_form_textarea_container: string;
