1f01566cc4e97fea501e90b33608d71ba5c8b125
Arthur Le Meur <arthur@edrlab.org>
2025-04-14 10:52:28 +0200

    fix CI

--- src/main/redux/store/memory.ts
+++ src/main/redux/store/memory.ts
@@ -443,6 +443,7 @@ export async function initStore()
                 process.env["MISTRAL_API_KEY"] = aiKey.aiKey;
             } 
         }
+    }
     if (preloadedState?.creator && !preloadedState.creator.urn) {
         preloadedState.creator.urn = `urn:uuid:${preloadedState.creator.id}`;
     }
@@ -471,5 +472,5 @@ export async function initStore()
     sagaMiddleware.run(rootSaga);
 
     return [store, sagaMiddleware];
-    }
 }
+
