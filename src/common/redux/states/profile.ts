a9e35eae397974e3b93d28b57618ea9334db01e6
Arthur Le Meur <arthur@edrlab.org>
2025-07-07 17:06:04 +0200

    up profiles structure and test profile content

--- src/common/redux/states/profile.ts
+++ src/common/redux/states/profile.ts
@@ -5,10 +5,42 @@
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 // ==LICENSE-END==
 
+interface ILink {
+  title: string;
+  href: string;
+  type: string;
+  rel: string;
+  properties?: {
+    showOnHomepage?: boolean;
+    allowDeletion?: boolean;
+    authenticate?: {
+      href: string;
+      type: string;
+    };
+    logo?: {
+      href: string;
+      type: string;
+    };
+  };
+}
+
 export interface IProfile {
     id: number,
     name: string,
-    colors?: string[],
-    logo?: string,
+    links?: ILink[],
+    colors?: {
+        "neutral-light": string;
+        "neutral-dark": string;
+        "primary-light": string;
+        "primary-dark": string;
+        "secondary-light": string;
+        "secondary-dark": string;
+        "border-light": string;
+        "border-dark": string;
+        "background-light": string;
+        "background-dark": string;
+        "appName-light": string;
+        "appName-dark": string;
+    };
     dilicom: boolean,
 }
