c8a8aa9e153447acff11054788d79db6f60ff1ed
Daniel Weck <daniel.weck@gmail.com>
2025-08-01 23:41:26 +0100

    chore(dev): handy shell scripts for ARM64 vs. x64 build/package [skip ci]

---
+++ package-linux_ARM64.sh
@@ -0,0 +1,7 @@
+# rm -rf node_modules/ && rm -f package-lock.json &&\
+rm -rf node_modules/electron &&\
+npm install --foreground-scripts --arch=arm64 --cpu=arm64 &&\
+node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--x64/g, "--arm64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
+npm run package:linux &&\
+node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--arm64/g, "--x64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });'
+# && file release/mac-arm64/Thorium.app/Contents/MacOS/Thorium
