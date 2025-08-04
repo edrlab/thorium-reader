ab0abbf4706140fff61046ac288d748a03e658ce
Pierre Leroux <p.leroux@gmx.com>
2025-08-04 16:48:35 +0200

    up dev_mode + test instruction

--- src/main/zip/create.ts
+++ src/main/zip/create.ts
@@ -189,52 +189,56 @@ export async function createProfilePackageZip(
     return packagePath;
 }
 
-// console.log(require.main);
-// console.log(process.argv[1]);
-if (process.argv[1] === path.resolve(process.cwd(), "./dist/make-package.js")) {
+if (__TH__IS_DEV__) {
+    // console.log(require.main);
+    // console.log(process.argv[1]);
 
-
-    if (process.argv.length !== 5) {
-        console.error(`usage: ${process.argv[0]} ${process.argv[1]} --signed=[false|true] inputDirectory outputDirectory`);
-        process.exit(1);
-    }
-    const signed = process.argv[2] === "--signed=true"; 
-    const inputDir = path.resolve(process.cwd(), process.argv[3]);
-    const outputDir = path.resolve(process.cwd(), process.argv[4]);
-
-    const resourcesMap: Array<[string, string]> = [];
-
-    const toBeVisit = ["./"];
-    while (toBeVisit.length) {
-
-        const dirPath = toBeVisit.shift();
-        const dirAbsolutePath = path.join(inputDir, dirPath);
-        const fileNameArray = readdirSync(dirAbsolutePath);
-        for (const fileName of fileNameArray) {
-            const filePath = path.join(dirPath, fileName);
-            const fileAbsolutePath = path.join(inputDir, filePath);
-            const stat = statSync(fileAbsolutePath);
-            if (stat.isDirectory()) {
-                toBeVisit.push(filePath);
-            } else {
-                if (filePath !== "manifest.json")
-                    resourcesMap.push([fileAbsolutePath, filePath]);
-            }
+    // npx webpack --config webpack.config.customization.js && DEBUG=r2:*,readium-desktop:* node ./dist/make-package.js --signed=true /tmp/test-profile /tmp
+    if (process.argv[1] === path.resolve(process.cwd(), "./dist/make-package.js")) {
+    
+    
+        if (process.argv.length !== 5) {
+            console.error(`usage: ${process.argv[0]} ${process.argv[1]} --signed=[false|true] inputDirectory outputDirectory`);
+            process.exit(1);
         }
-    } // BFS
-
-
-    let manifest: ICustomizationManifest;
-    try {
-        manifest = JSON.parse(readFileSync(path.join(inputDir, "manifest.json"), "utf-8"));
-    } catch {
-        console.error("manifest not found!!!");
-        process.exit(1);
+        const signed = process.argv[2] === "--signed=true"; 
+        const inputDir = path.resolve(process.cwd(), process.argv[3]);
+        const outputDir = path.resolve(process.cwd(), process.argv[4]);
+    
+        const resourcesMap: Array<[string, string]> = [];
+    
+        const toBeVisit = ["./"];
+        while (toBeVisit.length) {
+    
+            const dirPath = toBeVisit.shift();
+            const dirAbsolutePath = path.join(inputDir, dirPath);
+            const fileNameArray = readdirSync(dirAbsolutePath);
+            for (const fileName of fileNameArray) {
+                const filePath = path.join(dirPath, fileName);
+                const fileAbsolutePath = path.join(inputDir, filePath);
+                const stat = statSync(fileAbsolutePath);
+                if (stat.isDirectory()) {
+                    toBeVisit.push(filePath);
+                } else {
+                    if (filePath !== "manifest.json")
+                        resourcesMap.push([fileAbsolutePath, filePath]);
+                }
+            }
+        } // BFS
+    
+    
+        let manifest: ICustomizationManifest;
+        try {
+            manifest = JSON.parse(readFileSync(path.join(inputDir, "manifest.json"), "utf-8"));
+        } catch {
+            console.error("manifest not found!!!");
+            process.exit(1);
+        }
+    
+    
+        createProfilePackageZip(manifest, resourcesMap, outputDir, signed).then((outPath) => {
+            console.log("OUTPUT=", outPath);
+        }).catch((e) => console.error("ERROR!? ", e));
+    
     }
-
-
-    createProfilePackageZip(manifest, resourcesMap, outputDir, signed).then((outPath) => {
-        console.log("OUTPUT=", outPath);
-    }).catch((e) => console.error("ERROR!? ", e));
-
 }
