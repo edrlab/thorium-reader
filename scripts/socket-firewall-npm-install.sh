e86c557a7c4fe58e912a29ad642cca4654034a5a
Daniel Weck <daniel.weck@gmail.com>
2025-10-01 22:33:46 +0100

    chore(dev): added Socket Firewall to check NPM direct and transitive dependencies for security vulnerabilities / supply chain attacks (also minor NPM package update, and Flox/Nix env update)

---
+++ scripts/socket-firewall-npm-install.sh
@@ -0,0 +1,14 @@
+# https://github.com/SocketDev/sfw-free/issues/3
+
+npm cache clear --force &&\
+rm -rf node_modules/ &&\
+cp package.json package-sfw-backup.json &&\
+cp package-lock.json package-lock-sfw-backup.json ;\
+rm -f package-lock.json &&\
+node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/^\s+"electron-builder":.+$/gm, ""); fileStr = fileStr.replace(/^\s+"readium-speech":.+$/gm, ""); fileStr = fileStr.replace(/^\s+"divina-player-js":.+$/gm, ""); fileStr = fileStr.replace(/^\s+"pdf\.js":.+$/gm, `"pdf.js": "latest",`); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
+git diff package.json &&\
+sfw npm install --foreground-scripts ;\
+cp package-lock-sfw-backup.json package-lock.json ;\
+cp package-sfw-backup.json package.json ;\
+rm -rf node_modules/ &&\
+git status
