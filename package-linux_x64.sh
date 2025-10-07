8cf2ee55888c4010bebe5e8566bf92b6f7a221b1
Daniel Weck <daniel.weck@gmail.com>
2025-10-07 12:19:25 +0100

    chore(dev): code-signing disabling in rebuild scripts [skip ci]

--- package-linux_x64.sh
+++ package-linux_x64.sh
@@ -2,6 +2,6 @@
 rm -rf node_modules/electron &&\
 npm install --foreground-scripts --arch=x64 --cpu=x64 &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--arm64/g, "--x64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
-npm run package:linux
+CSC_NAME="" CSC_IDENTITY_AUTO_DISCOVERY="false" npm run package:linux
 # node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--x64/g, "--arm64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });'
 # && file release/mac/Thorium.app/Contents/MacOS/Thorium
--- package-mac-skip-notarize_ARM64.sh
+++ package-mac-skip-notarize_ARM64.sh
@@ -2,6 +2,6 @@
 rm -rf node_modules/electron &&\
 npm install --foreground-scripts --arch=arm64 --cpu=arm64 &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--x64/g, "--arm64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
-npm run package:mac:skip-notarize &&\
+CSC_NAME="" CSC_IDENTITY_AUTO_DISCOVERY="false" npm run package:mac:skip-notarize &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--arm64/g, "--x64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });'
 # && file release/mac-arm64/Thorium.app/Contents/MacOS/Thorium
