0b3b7cf3d464e03c4dbe1e4fdd01c7c47b33257e
Daniel Weck <daniel.weck@gmail.com>
2025-10-13 10:29:24 +0100

    chore(dev): code comments for JSON.stringify() with sorted object keys [skip ci]

--- src/common/utils/json.ts
+++ src/common/utils/json.ts
@@ -30,9 +30,17 @@
 // // sortObject modifies the passed object recursively, which is wy we clone the input JSON
 // export const JsonStringifySortedKeys = (json: any) => JSON.stringify(sortObject(JSON.parse(JSON.stringify(json))));
 // ... whereas the JSON.stringify() replacer intercept values to be stringified and applies a local transform (no actual mutation of the input)
+// Also see: https://github.com/ljharb/json-stable-stringify
 export const JsonStringifySortedKeys = (json: any, indentSpaces = 0): string => {
     return JSON.stringify(json, (_, val: any) => {
+        // https://github.com/lodash/lodash/blob/f299b52f39486275a9e6483b60a410e06520c538/lodash.js#L12076-L12115
+        // https://www.npmjs.com/package/lodash.isplainobject
+        // isPlainObject(val)
+        // https://github.com/jonschlinkert/is-plain-object/blob/0a47f0f6cd10e0d2489beb55a32a8d0ba7b04b25/is-plain-object.js#L8-L32
+        // https://github.com/jquery/jquery/blob/d6c1e2388420d97b5291f9bd45f362bedc0a8bcd/src/core.js#L201-L220
+        // https://github.com/jashkenas/underscore/blob/0d820ef7292cdec28dd8b5d565921b8100ca3397/modules/isObject.js#L2-L5
         if (typeof val === "object" && val !== null && !Array.isArray(val)) {
+
             const keys = Object.keys(val).sort(); // in-place array mutate
             const obj: Record<string, any> = {};
             for (let i = 0; i < keys.length; i++) {
