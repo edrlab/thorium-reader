6c3cb4b966848b9ac016102658ce317616e12569
Pierre Leroux <panacpp@gmail.com>
2025-11-04 18:29:38 +0100

    fix(customization): improve profile packaging command line build chain (PR #3280)

---
+++ src/main/customization/cli.ts
@@ -0,0 +1,127 @@
+
+// import * as debug_ from "debug";
+import * as fs from "fs";
+import * as path from "path";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import { customizationManifestJsonSchema, ICustomizationManifest } from "readium-desktop/common/readium/customization/manifest";
+import { createProfilePackageZip } from "./packager";
+
+// do not import "./provisioning" : build chain too heavy, lots of unused import
+// import { __CUSTOMIZATION_PROFILE_MANIFEST_AJV_ERRORS, isCustomizationProfileManifest } from "./provisioning";
+
+let __CUSTOMIZATION_PROFILE_MANIFEST_AJV_ERRORS = "";
+function isCustomizationProfileManifest(data: any): data is ICustomizationManifest {
+
+    const ajv = new Ajv();
+    addFormats(ajv);
+
+    const valid = ajv.validate(customizationManifestJsonSchema, data);
+
+    __CUSTOMIZATION_PROFILE_MANIFEST_AJV_ERRORS = ajv.errors?.length ? JSON.stringify(ajv.errors, null, 2) : "";
+
+    return valid;
+}
+
+// const debug = debug_("readium-desktop:main#customization/cli");
+
+function main() {
+    // console.log(require.main);
+    // console.log(process.argv[1]);
+
+    if (process.argv.length !== 5) {
+        console.error(`usage: ${process.argv[0]} ${process.argv[1]} --signed=[false|true] inputDirectory outputDirectory`);
+        process.exit(1);
+    }
+    const signed = process.argv[2] === "--signed=true";
+    const inputDir = path.resolve(process.cwd(), process.argv[3]);
+    const outputDir = path.resolve(process.cwd(), process.argv[4]);
+
+    const resourcesMap: Array<[string, string]> = [];
+
+    const toBeVisit = ["./"];
+    while (toBeVisit.length) {
+
+        const dirPath = toBeVisit.shift();
+        const dirAbsolutePath = path.join(inputDir, dirPath);
+        const fileNameArray = fs.readdirSync(dirAbsolutePath);
+        for (const fileName of fileNameArray) {
+            const filePath = path.join(dirPath, fileName);
+            const fileAbsolutePath = path.join(inputDir, filePath);
+            const stat = fs.statSync(fileAbsolutePath);
+            if (stat.isDirectory()) {
+                toBeVisit.push(filePath);
+            } else {
+                if (filePath !== "manifest.json")
+                    resourcesMap.push([fileAbsolutePath, filePath]);
+            }
+        }
+    } // BFS
+
+
+    let manifest: ICustomizationManifest;
+    try {
+        manifest = JSON.parse(fs.readFileSync(path.join(inputDir, "manifest.json"), "utf-8"));
+    } catch {
+        console.error("manifest not found!!!");
+        process.exit(1);
+    }
+
+    if (!isCustomizationProfileManifest(manifest)) {
+
+        console.error(__CUSTOMIZATION_PROFILE_MANIFEST_AJV_ERRORS);
+        process.exit(1);
+    }
+
+    const manifestResources: string[] = [];
+
+    for (const img of manifest.images || []) {
+        const href = img.href;
+        if (href.startsWith("./")) {
+            manifestResources.push(path.join("./", href));
+        }
+    }
+    for (const ln of manifest.links || []) {
+        const href = ln.href;
+        if (href.startsWith("./")) {
+            manifestResources.push(path.join("./", href));
+        }
+    }
+    for (const pub of manifest.publications || []) {
+        for (const ln of (pub as any).images || []) {
+            const href = ln.href;
+            if (href.startsWith("./")) {
+                manifestResources.push(path.join("./", href));
+            }
+        }
+        for (const ln of (pub as any).links || []) {
+            const href = ln.href;
+            if (href.startsWith("./")) {
+                manifestResources.push(path.join("./", href));
+            }
+        }
+    }
+    // TODO: Do you need to block external request (http) to ressources, local only !?
+
+    const resourcesFiltered = resourcesMap.filter(([, filePath]) => manifestResources.includes(filePath));
+
+    console.log("ressourcesMapFromDirectory:");
+    console.log(resourcesMap);
+
+    console.log("manifestRessources:");
+    console.log(manifestResources);
+
+    console.log("resourcesFiltered:");
+    console.log(resourcesFiltered);
+
+    createProfilePackageZip(manifest, resourcesFiltered, outputDir, signed, true).then((outPath) => {
+        console.log("OUTPUT=", outPath);
+    }).catch((e) => console.error("ERROR!? ", e));
+
+        // await createZip(packagePath, resourcesMapFs, [[manifestBuffer, "manifest.json"]]);
+
+}
+
+if (require.main === module) {
+  main();
+}
---
+++ src/main/customization/packager.ts
@@ -0,0 +1,132 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END=
+
+import * as debug_ from "debug";
+import * as fs from "fs";
+import * as path from "path";
+
+import { extractCrc32OnZip } from "../tools/crc";
+import { createSign, createVerify } from "crypto";
+
+import { _CUSTOMIZATION_PROFILE_PRIVATE_KEY, _CUSTOMIZATION_PROFILE_PUB_KEY } from "readium-desktop/preprocessor-directives";
+import { ICustomizationManifest } from "readium-desktop/common/readium/customization/manifest";
+import { sanitizeForFilename } from "readium-desktop/common/safe-filename";
+import { injectBufferInZip } from "../tools/zipInjector";
+import { EXT_THORIUM } from "readium-desktop/common/extension";
+import { createZip, TResourcesFSCreateZip } from "../zip/create";
+
+// Logger
+const debug = debug_("readium-desktop:main#customization/packager");
+
+const signManifest = (manifest: ICustomizationManifest) => {
+
+    const manifestStringified = JSON.stringify(manifest);
+
+    const sign = createSign("SHA256");
+    sign.update(manifestStringified);
+    sign.end();
+    const signature = sign.sign(_CUSTOMIZATION_PROFILE_PRIVATE_KEY, "hex");
+
+    return {
+        key: _CUSTOMIZATION_PROFILE_PUB_KEY, // PUBLIC Not PRIVATE !?!
+        value: signature,
+        algorithm: "https://www.w3.org/2008/xmlsec/namespaces.html#ECKeyValue", // see scripts/profile-generate-key-pair.js
+    };
+};
+const verifyManifest = (manifest: ICustomizationManifest) => {
+
+    const signatureValue = manifest.signature.value;
+    const signatureKey = manifest.signature.key;
+    const stringifiedManifest = JSON.stringify({...manifest, signature: undefined});
+
+    const verify = createVerify("SHA256");
+    verify.write(stringifiedManifest);
+    verify.end();
+    const verified = verify.verify(signatureKey, signatureValue, "hex");
+    return verified;
+};
+export async function createProfilePackageZip(
+    manifest: ICustomizationManifest,
+    resourcesMapFs: TResourcesFSCreateZip,
+    outputProfilePath: string,
+    signed = false,
+    testSignature = false,
+) {
+    const packagePath = path.resolve(outputProfilePath,
+        sanitizeForFilename((manifest.title["en"] || "") + "_" + (manifest.version || "") + EXT_THORIUM),
+    );
+    // const packagePathTMP = packagePath + ".tmp";
+
+    debug("Ouput path =", packagePath);
+
+    if (resourcesMapFs.length) {
+        await createZip(packagePath, resourcesMapFs, []);
+        const contentHash = await extractCrc32OnZip(packagePath, "profile");
+        debug("ZIP CRC = ", contentHash);
+        manifest.contentHash = contentHash;
+        manifest.signature = undefined;
+
+        if (signed) {
+            const signature = signManifest(manifest);
+            debug("Manifest signed", JSON.stringify(signature));
+            manifest.signature = signature;
+        }
+        const manifestBuffer = Buffer.from(JSON.stringify(manifest, null, 4));
+        const packagePathTMP = packagePath + ".tmpzip";
+        await new Promise<void>((resolve, reject) => {
+            injectBufferInZip(
+                packagePath,
+                packagePathTMP,
+                manifestBuffer,
+                "manifest.json",
+                (e: any) => {
+                    debug("injectManifestToZip - injectBufferInZip ERROR!");
+                    debug(e);
+                    reject(e);
+                },
+                () => {
+                    resolve();
+                });
+        });
+
+        fs.unlinkSync(packagePath);
+        await new Promise<void>((resolve, _reject) => {
+            setTimeout(() => {
+                resolve();
+            }, 200); // to avoid issues with some filesystems (allow extra completion time)
+        });
+        fs.renameSync(packagePathTMP, packagePath);
+        await new Promise<void>((resolve, _reject) => {
+            setTimeout(() => {
+                resolve();
+            }, 200); // to avoid issues with some filesystems (allow extra completion time)
+        });
+    } else {
+        manifest.contentHash = "";
+        manifest.signature = undefined;
+
+        if (signed) {
+            const signature = signManifest(manifest);
+            debug("Manifest signed", JSON.stringify(signature));
+            manifest.signature = signature;
+        }
+        const manifestBuffer = Buffer.from(JSON.stringify(manifest, null, 4));
+        await createZip(packagePath, resourcesMapFs, [[manifestBuffer, "manifest.json"]]);
+    }
+
+
+    if (signed && testSignature) {
+        if (verifyManifest(manifest)) {
+            debug("PASS!: Manifest Signed And Verifed");
+        } else {
+            throw new Error("KO ERRRO!: Manifest not verified after signature");
+        }
+    }
+
+
+    return packagePath;
+}
