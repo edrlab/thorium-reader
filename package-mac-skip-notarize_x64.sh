472d2f7ef83f8d69d27ffc660c1658c016c17d53
Daniel Weck <daniel.weck@gmail.com>
2025-12-09 21:07:14 +0100

    chore(dev): NPM install ignore-scripts, with explicit Electron postinstall call

--- package-mac-skip-notarize_x64.sh
+++ package-mac-skip-notarize_x64.sh
@@ -1,6 +1,6 @@
 # rm -rf node_modules/ && rm -f package-lock.json &&\
 rm -rf node_modules/electron &&\
-npm install --foreground-scripts --arch=x64 --cpu=x64 &&\
+npm install --ignore-scripts --foreground-scripts --arch=x64 --cpu=x64 && cd node_modules/electron && npm run postinstall && cd - &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--arm64/g, "--x64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
 GITHUB_TOKEN_RELEASE_PUBLISH="xxx" CSC_NAME="" CSC_IDENTITY_AUTO_DISCOVERY="false" npm run package:mac:skip-notarize
 # node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--x64/g, "--arm64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });'
