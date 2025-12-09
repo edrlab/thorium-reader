3a31b616535336495c557425f9d9cce9f40f6bef
Daniel Weck <daniel.weck@gmail.com>
2025-12-09 23:26:17 +0100

    chore(dev): NPM postinstall explicit arch [skip ci]

--- package-win_ARM64.sh
+++ package-win_ARM64.sh
@@ -1,6 +1,6 @@
 # rm -rf node_modules/ && rm -f package-lock.json &&\
 rm -rf node_modules/electron &&\
-npm install --ignore-scripts --foreground-scripts --arch=arm64 --cpu=arm64 && cd node_modules/electron && npm run postinstall && cd - &&\
+npm install --ignore-scripts --foreground-scripts --arch=arm64 --cpu=arm64 && cd node_modules/electron && npm run postinstall --arch=arm64 --cpu=arm64 && cd - &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--x64/g, "--arm64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });' &&\
 GITHUB_TOKEN_RELEASE_PUBLISH="xxx" CSC_NAME="" CSC_IDENTITY_AUTO_DISCOVERY="false" npm run package:win &&\
 node -e 'const path = require("path"); const fs = require("fs"); const filePath = path.join(process.cwd(), "package.json"); let fileStr = fs.readFileSync(filePath, { encoding: "utf8" }); fileStr = fileStr.replace(/--arm64/g, "--x64"); fs.writeFileSync(filePath, fileStr, { encoding: "utf8" });'
