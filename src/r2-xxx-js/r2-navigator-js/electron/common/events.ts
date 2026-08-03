edrlab/thorium-reader

commit 357c9223508c2660ff0c9ffc68b0825aa3df7d41

fix: fixed-layout EPUB documents were blank when hyperlinking into already-loaded webview

src/r2-xxx-js/r2-navigator-js/electron/renderer/dom.ts
@@ -46,7 +46,7 @@ import {
 } from "./readaloud";
 import { adjustReadiumCssJsonMessageForFixedLayout, isFixedLayout, obtainReadiumCss } from "./readium-css";
 import { soundtrackHandleIpcMessage } from "./soundtrack";
-import { ReadiumElectronBrowserWindow, IReadiumElectronWebview } from "./webview/state";
+import { ReadiumElectronBrowserWindow, IReadiumElectronWebview, IReadiumElectronWebviewState } from "./webview/state";
 import { HighlightDrawTypeBackground } from "../common/highlight";
 
 const ELEMENT_ID_SLIDING_VIEWPORT = "r2_navigator_sliding_viewport";
@@ -243,9 +243,10 @@ export function fixedLayoutZoomPercent(zoomPercent: number) {
             debug("fixedLayoutZoomPercent ... setWebViewStyle");
 
             // win.READIUM2.domSlidingViewport
+            debug("DOM OPACITY ZERO 1 (fixedLayoutZoomPercent): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
             win.READIUM2.domRootElement.style.opacity = "0";
             win.READIUM2.opacityMaskCounter++;
-            setWebViewStyle(activeWebView, wvSlot);
+            setWebViewStyle(activeWebView, wvSlot, undefined);
 
             _fixedLayoutZoomPercentTimers[activeWebView.id] = win.setTimeout(() => {
                 try {
@@ -255,6 +256,7 @@ export function fixedLayoutZoomPercent(zoomPercent: number) {
                     if (activeWebView.READIUM2?.DOMisReady) {
                         activeWebView.send("R2_EVENT_WINDOW_RESIZE", zoomPercent).then((_v) => { /* noop */ }).catch((_err) => { /* debug(err); */ });
                     } else {
+                        debug("DOM OPACITY ONE 1 (fixedLayoutZoomPercent timeout not ready): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                         if (win.READIUM2.opacityMaskCounter) {
                             win.READIUM2.opacityMaskCounter--;
                         }
@@ -290,9 +292,11 @@ export function setImageClickHandler(cb: (payload: IEventPayload_R2_EVENT_IMAGE_
     _imageClickHandler = cb;
 };
 
-function createWebViewInternal(preloadScriptPath: string): IReadiumElectronWebview {
+function createWebViewInternal(READIUM2: IReadiumElectronWebviewState, preloadScriptPath: string): IReadiumElectronWebview {
 
     const wv = document.createElement("webview");
+    (wv as IReadiumElectronWebview).READIUM2 = READIUM2;
+
     // https://github.com/electron/electron/blob/main/docs/tutorial/security.md
     //
     // https://www.electronjs.org/docs/latest/tutorial/sandbox
@@ -312,7 +316,7 @@ function createWebViewInternal(preloadScriptPath: string): IReadiumElectronWebvi
         wv.setAttribute("httpreferrer", publicationURL_);
     }
     debug("createWebViewInternal ... setWebViewStyle");
-    setWebViewStyle(wv as IReadiumElectronWebview, WebViewSlotEnum.center);
+    setWebViewStyle(wv as IReadiumElectronWebview, WebViewSlotEnum.center, null);
     wv.setAttribute("preload", preloadScriptPath); // "file://"
 
     // if (ENABLE_WEBVIEW_RESIZE) {
@@ -387,8 +391,10 @@ function createWebViewInternal(preloadScriptPath: string): IReadiumElectronWebvi
             if (payload.fxl) {
                 setWebViewStyle(webview, WebViewSlotEnum.center, payload.fxl);
             } else {
-                setWebViewStyle(webview, WebViewSlotEnum.center, null);
+                setWebViewStyle(webview, WebViewSlotEnum.center, undefined);
             }
+
+            debug("DOM OPACITY ONE 2 (IPC R2_EVENT_FXL_CONFIGURE): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
             if (!win.READIUM2.opacityMaskCounter || --win.READIUM2.opacityMaskCounter <= 0) {
                 win.READIUM2.domRootElement.style.opacity = "1";
             }
@@ -540,26 +546,28 @@ function createWebView(second?: boolean) {
         if (_webview2) {
             destroyWebView(true);
         }
-        _webview2 = createWebViewInternal(preloadScriptPath);
-        _webview2.READIUM2 = {
+        const READIUM2: IReadiumElectronWebviewState = {
             id: 2,
             link: undefined,
             readiumCss: undefined,
             highlights: undefined,
         };
+        _webview2 = createWebViewInternal(READIUM2, preloadScriptPath);
+
         _webview2.setAttribute("id", "r2_webview2");
         domSlidingViewport.appendChild(_webview2 as Node);
     } else {
         if (_webview1) {
             destroyWebView(false);
         }
-        _webview1 = createWebViewInternal(preloadScriptPath);
-        _webview1.READIUM2 = {
+        const READIUM2: IReadiumElectronWebviewState = {
             id: 1,
             link: undefined,
             readiumCss: undefined,
             highlights: undefined,
         };
+        _webview1 = createWebViewInternal(READIUM2, preloadScriptPath);
+
         _webview1.setAttribute("id", "r2_webview1");
         domSlidingViewport.appendChild(_webview1 as Node);
     }
@@ -812,9 +820,10 @@ export function installNavigatorDOM(
                 const wvSlot = activeWebView.getAttribute("data-wv-slot") as WebViewSlotEnum;
                 if (wvSlot) {
                     debug("Window resize (TOP), IMMEDIATE ... setWebViewStyle");
+                    debug("DOM OPACITY ZERO 2 (Resize Observer): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                     win.READIUM2.domRootElement.style.opacity = "0";
                     win.READIUM2.opacityMaskCounter++;
-                    setWebViewStyle(activeWebView, wvSlot);
+                    setWebViewStyle(activeWebView, wvSlot, undefined);
                 }
             }
         }
@@ -837,6 +846,7 @@ export function installNavigatorDOM(
                         if (activeWebView.READIUM2?.DOMisReady) {
                             activeWebView.send("R2_EVENT_WINDOW_RESIZE", win.READIUM2.fixedLayoutZoomPercent).then((_v) => { /* noop */ }).catch((_err) => { /* debug(err); */ });
                         } else {
+                            debug("DOM OPACITY ONE 3 (Resize Observer timeout not ready): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                             if (win.READIUM2.opacityMaskCounter) {
                                 win.READIUM2.opacityMaskCounter--;
                             }

src/r2-xxx-js/r2-navigator-js/electron/renderer/location.ts
@@ -85,16 +85,37 @@ const webviewStyleRight_ = "opacity: 1; " + webviewStyleCommon +
 const webviewStyleCenter_ = "opacity: 1; " + webviewStyleCommon +
     "left: 0; top: calc(0 - var(--R2_FXL_Y_SHIFT));";
 
-export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlotEnum, fxl?: IwidthHeight | null) {
+export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlotEnum, fxl: IwidthHeight | null | undefined): boolean {
+    let changed = false;
 
     const v = fxl ? JSON.stringify(fxl).replace(/{/g, "").replace(/}/g, "").replace(/"/g, "") : "NO FXL";
     debug("setWebViewStyle fxl: " + v);
 
     if (fxl) {
+        if (wv.READIUM2.fxlViewportWidth !== fxl.width ||
+            wv.READIUM2.fxlViewportHeight !== fxl.height ||
+            wv.READIUM2.fxlViewportScale !== fxl.scale ||
+            wv.READIUM2.fxlViewportTX !== fxl.tx ||
+            wv.READIUM2.fxlViewportTY !== fxl.ty) {
+            debug("setWebViewStyle CHANGED (FXL data)", v);
+            changed = true;
+        }
+        wv.READIUM2.fxlViewportWidth = fxl.width;
+        wv.READIUM2.fxlViewportHeight = fxl.height;
+        wv.READIUM2.fxlViewportScale = fxl.scale;
+        wv.READIUM2.fxlViewportTX = fxl.tx;
+        wv.READIUM2.fxlViewportTY = fxl.ty;
 
         let wvSlot_ = wv.getAttribute("data-wv-slot") as WebViewSlotEnum;
-        if (!wvSlot_) {
+        // if (!wvSlot_) {
+        //     wvSlot_ = wvSlot;
+        // }
+        if (wvSlot_ !== wvSlot) {
+            debug("setWebViewStyle CHANGED SLOT)", wvSlot_, wvSlot);
             wvSlot_ = wvSlot;
+            changed = true;
+            wv.setAttribute("data-wv-slot", wvSlot);
+            debug("wvSlot_ !== wvSlot ??!"); // this should never happen?
         }
 
         // fxl.tx can only be negative for WebViewSlotEnum.left and WebViewSlotEnum.center
@@ -112,28 +133,60 @@ export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlot
 
         // tslint:disable-next-line:max-line-length
         const cxx = ` width:${fxl.width * fxl.scale}px; height:${fxl.height * fxl.scale}px; transform-origin: 0 0; transform: translate(${tx}px, ${ty}px) scale(${"1"});`;
-        wv.setAttribute("style",
-            wvSlot_ === WebViewSlotEnum.center ? webviewStyleCenter_ + cxx :
-                (wvSlot_ === WebViewSlotEnum.left ? webviewStyleLeft_ + cxx :
-                webviewStyleRight_ + cxx),
-        );
+        const attrStyleVal = wvSlot_ === WebViewSlotEnum.center ? webviewStyleCenter_ + cxx :
+            (wvSlot_ === WebViewSlotEnum.left ? webviewStyleLeft_ + cxx :
+                webviewStyleRight_ + cxx);
+        if (wv.getAttribute("style") !== attrStyleVal) {
+            debug("setWebViewStyle CHANGED (style)", wv.getAttribute("style"), attrStyleVal);
+            changed = true;
+        }
+        wv.setAttribute("style", attrStyleVal);
 
+        // if (wv.getAttribute("data-wv-fxl") !== v) {
+        //     debug("setWebViewStyle CHANGED (FXL)", wv.getAttribute("data-wv-fxl"), v);
+        //     changed = true;
+        // }
+        if (!wv.hasAttribute("data-wv-fxl")) {
+            debug("setWebViewStyle CHANGED (FXL)", v);
+            changed = true;
+        }
         wv.setAttribute("data-wv-fxl", v);
     } else {
-        wv.setAttribute("style",
-            wvSlot === WebViewSlotEnum.center ? webviewStyleCenter :
-                (wvSlot === WebViewSlotEnum.left ? webviewStyleLeft :
-                webviewStyleRight),
-        );
+        const attrStyleVal = wvSlot === WebViewSlotEnum.center ? webviewStyleCenter :
+            (wvSlot === WebViewSlotEnum.left ? webviewStyleLeft :
+                webviewStyleRight);
+        if (wv.getAttribute("style") !== attrStyleVal) {
+            debug("setWebViewStyle CHANGED (style)", wv.getAttribute("style"), attrStyleVal);
+            changed = true;
+        }
+        wv.setAttribute("style", attrStyleVal);
 
-        wv.removeAttribute("data-wv-fxl");
+        if (fxl === null) { // when undefined, preserve same-link FXL (loadLink() same doc)
 
-        wv.setAttribute("data-wv-slot",
-            wvSlot === WebViewSlotEnum.center ? "center" :
-                (wvSlot === WebViewSlotEnum.left ? "left" :
-                "right"),
-        );
+            wv.READIUM2.fxlViewportWidth = 0;
+            wv.READIUM2.fxlViewportHeight = 0;
+            wv.READIUM2.fxlViewportScale = 1;
+            wv.READIUM2.fxlViewportTX = 0;
+            wv.READIUM2.fxlViewportTY = 0;
+
+            if (wv.hasAttribute("data-wv-fxl")) {
+                debug("setWebViewStyle CHANGED (FXL)", wv.getAttribute("data-wv-fxl"));
+                changed = true;
+            }
+            wv.removeAttribute("data-wv-fxl");
+        }
+
+        const val = wvSlot === WebViewSlotEnum.center ? "center" :
+            (wvSlot === WebViewSlotEnum.left ? "left" :
+                "right");
+        if (wv.getAttribute("data-wv-slot") !== val) {
+            debug("setWebViewStyle CHANGED SLOT)", wv.getAttribute("data-wv-slot"), val);
+            changed = true;
+        }
+        wv.setAttribute("data-wv-slot", val);
     }
+
+    return changed;
 }
 
 // export function stealFocusDisable(doDisable: boolean) {
@@ -951,6 +1004,22 @@ function loadLink(
             }
         });
 
+        const sameFXL = isFixedLayout(pubLink) && activeWebView.READIUM2.link === pubLink;
+        const fxl: IwidthHeight = sameFXL &&
+            activeWebView.READIUM2.fxlViewportWidth &&
+            activeWebView.READIUM2.fxlViewportHeight &&
+            typeof activeWebView.READIUM2.fxlViewportTX === "number" &&
+            typeof activeWebView.READIUM2.fxlViewportTY === "number" &&
+            typeof activeWebView.READIUM2.fxlViewportScale === "number"
+            ?
+        {
+            width: activeWebView.READIUM2.fxlViewportWidth,
+            height: activeWebView.READIUM2.fxlViewportHeight,
+            tx: activeWebView.READIUM2.fxlViewportTX,
+            ty: activeWebView.READIUM2.fxlViewportTY,
+            scale: activeWebView.READIUM2.fxlViewportScale,
+        } : undefined;
+
         const prev = previous ? true : false;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const page = (pubLink as any).__notInSpreadForced ? PageEnum.Center : pubLink.Properties?.Page;
@@ -985,9 +1054,12 @@ function loadLink(
             }
             if (activeWebView) {
                 debug("loadLink LEFT ... setWebViewStyle");
-                win.READIUM2.domRootElement.style.opacity = "0";
-                win.READIUM2.opacityMaskCounter++;
-                setWebViewStyle(activeWebView, WebViewSlotEnum.left);
+                const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.left, sameFXL ? fxl : null);
+                debug("DOM OPACITY ZERO 3: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
+                if (activeWebView.READIUM2.link !== pubLink) { //  || changed
+                    win.READIUM2.domRootElement.style.opacity = "0";
+                    win.READIUM2.opacityMaskCounter++;
+                }
             }
         } else if (page === PageEnum.Right) {
             webViewSlot = WebViewSlotEnum.right;
@@ -1020,17 +1092,23 @@ function loadLink(
             }
             if (activeWebView) {
                 debug("loadLink RIGHT ... setWebViewStyle");
-                win.READIUM2.domRootElement.style.opacity = "0";
-                win.READIUM2.opacityMaskCounter++;
-                setWebViewStyle(activeWebView, WebViewSlotEnum.right);
+                const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.right, sameFXL ? fxl : null);
+                debug("DOM OPACITY ZERO 4: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
+                if (activeWebView.READIUM2.link !== pubLink) { //  || changed
+                    win.READIUM2.domRootElement.style.opacity = "0";
+                    win.READIUM2.opacityMaskCounter++;
+                }
             }
         } else {
             webViewSlot = WebViewSlotEnum.center;
             if (activeWebView) {
                 debug("loadLink CENTER ... setWebViewStyle");
-                win.READIUM2.domRootElement.style.opacity = "0";
-                win.READIUM2.opacityMaskCounter++;
-                setWebViewStyle(activeWebView, WebViewSlotEnum.center);
+                const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.center, sameFXL ? fxl : null);
+                debug("DOM OPACITY ZERO 5: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
+                if (activeWebView.READIUM2.link !== pubLink) { //  || changed
+                    win.READIUM2.domRootElement.style.opacity = "0";
+                    win.READIUM2.opacityMaskCounter++;
+                }
             }
         }
     }

commit d58b187365f270f1627ae690e5cb04a816205844

fix: Fixed Layout 2-page spread layout was broken (regression from 357c9223508c2660ff0c9ffc68b0825aa3df7d41), also improves mixed reflow/FXL support, also fixes incorrect webview / page positioning on load-after-first (for example when hyperlinking into an already-rendered page) (Fixes #3742 )

src/r2-xxx-js/r2-navigator-js/electron/common/events.ts
@@ -60,8 +60,8 @@ export const R2_EVENT_READIUMCSS = "R2_EVENT_READIUMCSS";
 export interface IEventPayload_R2_EVENT_READIUMCSS {
     setCSS: IReadiumCSS | undefined;
     isFixedLayout?: boolean;
-    fixedLayoutWebViewWidth?: number;
-    fixedLayoutWebViewHeight?: number;
+    fixedLayoutAvailableWebViewWidth?: number;
+    fixedLayoutAvailableWebViewHeight?: number;
     fixedLayoutZoomPercent?: number;
     urlRoot?: string;
 }

src/r2-xxx-js/r2-navigator-js/electron/common/readium-css-inject.ts
@@ -40,17 +40,18 @@ const debug = debug_("r2:navigator#electron/common/readium-css-inject");
 
 // import { ReadiumElectronWebviewWindow } from "../renderer/webview/state";
 // (global.window as ReadiumElectronWebviewWindow).READIUM2.DEBUG_VISUALS
-function isDEBUG_VISUALS(documant: Document): boolean {
+function isDEBUG_VISUALS(_documant: Document): boolean {
     if (!IS_DEV) {
         return false;
     }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if (documant.defaultView && (documant.defaultView as any).READIUM2 &&
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (documant.defaultView as any).READIUM2.DEBUG_VISUALS) {
-        return true;
-    }
-    return false;
+    return true;
+    // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // if (documant.defaultView && (documant.defaultView as any).READIUM2 &&
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //     (documant.defaultView as any).READIUM2.DEBUG_VISUALS) {
+    //     return true;
+    // }
+    // return false;
 }
 
 export function isDocVertical(documant: Document): boolean {
@@ -705,6 +706,12 @@ export function configureFixedLayout(
     if (!documant || !documant.head || !documant.body) {
         return undefined;
     }
+    debug("configureFixedLayout isFixedLayout ", isFixedLayout);
+    debug("configureFixedLayout fxlViewportWidth ", fxlViewportWidth);
+    debug("configureFixedLayout fxlViewportHeight ", fxlViewportHeight);
+    debug("configureFixedLayout innerWidth ", innerWidth);
+    debug("configureFixedLayout innerHeight ", innerHeight);
+    debug("configureFixedLayout wvSlot ", wvSlot);
     debug("configureFixedLayout zoomPercent ", zoomPercent);
 
     let wh: IwidthHeight | undefined;

src/r2-xxx-js/r2-navigator-js/electron/renderer/dom.ts
@@ -387,11 +387,13 @@ function createWebViewInternal(READIUM2: IReadiumElectronWebviewState, preloadSc
             webview.style.opacity = "1";
         } else if (event.channel === R2_EVENT_FXL_CONFIGURE) {
             const payload = event.args[0] as IEventPayload_R2_EVENT_FXL_CONFIGURE;
-            debug("R2_EVENT_FXL_CONFIGURE ... setWebViewStyle");
+            const wvSlot = webview.getAttribute("data-wv-slot") as WebViewSlotEnum;
+            debug("R2_EVENT_FXL_CONFIGURE ... setWebViewStyle", wvSlot, JSON.stringify(payload.fxl, null, 4));
             if (payload.fxl) {
-                setWebViewStyle(webview, WebViewSlotEnum.center, payload.fxl);
+                setWebViewStyle(webview, wvSlot ? wvSlot : WebViewSlotEnum.center, payload.fxl);
             } else {
-                setWebViewStyle(webview, WebViewSlotEnum.center, undefined);
+                // setWebViewStyle(webview, wvSlot ? wvSlot : WebViewSlotEnum.center, undefined);
+                setWebViewStyle(webview, WebViewSlotEnum.center, null);
             }
 
             debug("DOM OPACITY ONE 2 (IPC R2_EVENT_FXL_CONFIGURE): ", win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);

src/r2-xxx-js/r2-navigator-js/electron/renderer/location.ts
@@ -96,7 +96,8 @@ export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlot
             wv.READIUM2.fxlViewportHeight !== fxl.height ||
             wv.READIUM2.fxlViewportScale !== fxl.scale ||
             wv.READIUM2.fxlViewportTX !== fxl.tx ||
-            wv.READIUM2.fxlViewportTY !== fxl.ty) {
+            wv.READIUM2.fxlViewportTY !== fxl.ty ||
+            wv.READIUM2.webViewSlot !== wvSlot) {
             debug("setWebViewStyle CHANGED (FXL data)", v);
             changed = true;
         }
@@ -117,6 +118,8 @@ export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlot
             wv.setAttribute("data-wv-slot", wvSlot);
             debug("wvSlot_ !== wvSlot ??!"); // this should never happen?
         }
+
+        wv.READIUM2.webViewSlot = wvSlot;
 
         // fxl.tx can only be negative for WebViewSlotEnum.left and WebViewSlotEnum.center
         // (WebViewSlotEnum.right is always aligned on the middle line of the spread)
@@ -168,6 +171,7 @@ export function setWebViewStyle(wv: IReadiumElectronWebview, wvSlot: WebViewSlot
             wv.READIUM2.fxlViewportScale = 1;
             wv.READIUM2.fxlViewportTX = 0;
             wv.READIUM2.fxlViewportTY = 0;
+            wv.READIUM2.webViewSlot = wvSlot;
 
             if (wv.hasAttribute("data-wv-fxl")) {
                 debug("setWebViewStyle CHANGED (FXL)", wv.getAttribute("data-wv-fxl"));
@@ -1053,7 +1057,7 @@ function loadLink(
                 }
             }
             if (activeWebView) {
-                debug("loadLink LEFT ... setWebViewStyle");
+                debug("loadLink LEFT ... setWebViewStyle", sameFXL, JSON.stringify(fxl, null, 4));
                 const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.left, sameFXL ? fxl : null);
                 debug("DOM OPACITY ZERO 3: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                 if (activeWebView.READIUM2.link !== pubLink) { //  || changed
@@ -1091,7 +1095,7 @@ function loadLink(
                 }
             }
             if (activeWebView) {
-                debug("loadLink RIGHT ... setWebViewStyle");
+                debug("loadLink RIGHT ... setWebViewStyle", sameFXL, JSON.stringify(fxl, null, 4));
                 const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.right, sameFXL ? fxl : null);
                 debug("DOM OPACITY ZERO 4: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                 if (activeWebView.READIUM2.link !== pubLink) { //  || changed
@@ -1102,7 +1106,7 @@ function loadLink(
         } else {
             webViewSlot = WebViewSlotEnum.center;
             if (activeWebView) {
-                debug("loadLink CENTER ... setWebViewStyle");
+                debug("loadLink CENTER ... setWebViewStyle", sameFXL, JSON.stringify(fxl, null, 4));
                 const changed = setWebViewStyle(activeWebView, WebViewSlotEnum.center, sameFXL ? fxl : null);
                 debug("DOM OPACITY ZERO 5: ", changed, win.READIUM2.domRootElement.style.opacity, win.READIUM2.opacityMaskCounter);
                 if (activeWebView.READIUM2.link !== pubLink) { //  || changed

src/r2-xxx-js/r2-navigator-js/electron/renderer/readium-css.ts
@@ -9,6 +9,7 @@ import { Link } from "@r2-shared-js/models/publication-link";
 
 import { IEventPayload_R2_EVENT_READIUMCSS } from "../common/events";
 import { ReadiumElectronBrowserWindow, IReadiumElectronWebview } from "./webview/state";
+import { WebViewSlotEnum } from "../common/styles";
 
 const win = global.window as ReadiumElectronBrowserWindow;
 
@@ -79,9 +80,26 @@ export function adjustReadiumCssJsonMessageForFixedLayout(
 
     if (isFixedLayout(pubLink)) {
 
+        // console.log("webview.clientWidth", webview.clientWidth);
+        // console.log("webview.clientHeight", webview.clientHeight);
+        // console.log("webview.offsetWidth", webview.offsetWidth);
+        // console.log("webview.offsetHeight", webview.offsetHeight);
+        // console.log("webview.scrollWidth", webview.scrollWidth);
+        // console.log("webview.scrollHeight", webview.scrollHeight);
+
+        // console.log("win.READIUM2.domRootElement.clientWidth", win.READIUM2.domRootElement.clientWidth);
+        // console.log("win.READIUM2.domRootElement.clientHeight", win.READIUM2.domRootElement.clientHeight);
+        // console.log("win.READIUM2.domRootElement.offsetWidth", win.READIUM2.domRootElement.offsetWidth);
+        // console.log("win.READIUM2.domRootElement.offsetHeight", win.READIUM2.domRootElement.offsetHeight);
+        // console.log("win.READIUM2.domRootElement.scrollWidth", win.READIUM2.domRootElement.scrollWidth);
+        // console.log("win.READIUM2.domRootElement.scrollHeight", win.READIUM2.domRootElement.scrollHeight);
+
+        const fixedLayoutAvailableWebViewWidth = !webview.READIUM2.webViewSlot ? webview.clientWidth : webview.READIUM2.webViewSlot === WebViewSlotEnum.center ? win.READIUM2.domRootElement.clientWidth : win.READIUM2.domRootElement.clientWidth / 2;
+        const fixedLayoutAvailableWebViewHeight = win.READIUM2.domRootElement.clientHeight || webview.clientHeight;
+
         return {
-            fixedLayoutWebViewHeight: webview.clientHeight,
-            fixedLayoutWebViewWidth: webview.clientWidth,
+            fixedLayoutAvailableWebViewHeight,
+            fixedLayoutAvailableWebViewWidth,
             fixedLayoutZoomPercent: win.READIUM2.fixedLayoutZoomPercent,
             isFixedLayout: true,
             setCSS: undefined,

src/r2-xxx-js/r2-navigator-js/electron/renderer/webview/preload.ts
@@ -2233,6 +2233,7 @@ win.addEventListener("DOMContentLoaded", () => {
     }
 
     if (readiumcssJson) {
+        debug("readiumcssJson::=", JSON.stringify(readiumcssJson, null, 4));
         win.READIUM2.isFixedLayout = (typeof readiumcssJson.isFixedLayout !== "undefined") ?
             readiumcssJson.isFixedLayout : false;
     }
@@ -2267,13 +2268,15 @@ win.addEventListener("DOMContentLoaded", () => {
     // testReadiumCSS(readiumcssJson);
 
     // innerWidth/Height can be zero at this rendering stage! :(
-    const w = (readiumcssJson && readiumcssJson.fixedLayoutWebViewWidth) || win.innerWidth;
-    const h = (readiumcssJson && readiumcssJson.fixedLayoutWebViewHeight) || win.innerHeight;
+    const w = (readiumcssJson && readiumcssJson.fixedLayoutAvailableWebViewWidth) || win.innerWidth;
+    const h = (readiumcssJson && readiumcssJson.fixedLayoutAvailableWebViewHeight) || win.innerHeight;
     win.READIUM2.fxlZoomPercent = (readiumcssJson && readiumcssJson.fixedLayoutZoomPercent) || 0;
+
+    debug("configureFixedLayout...(DOMContentLoaded)", readiumcssJson?.fixedLayoutAvailableWebViewWidth, win.innerWidth, readiumcssJson?.fixedLayoutAvailableWebViewHeight, win.innerHeight);
     const wh = configureFixedLayout(win.document, win.READIUM2.isFixedLayout,
         win.READIUM2.fxlViewportWidth, win.READIUM2.fxlViewportHeight,
         w, h, win.READIUM2.webViewSlot, win.READIUM2.fxlZoomPercent);
-    if (wh) {
+    if (wh && win.READIUM2.isFixedLayout) {
         win.READIUM2.fxlViewportWidth = wh.width;
         win.READIUM2.fxlViewportHeight = wh.height;
         win.READIUM2.fxlViewportScale = wh.scale;
@@ -2285,6 +2288,12 @@ win.addEventListener("DOMContentLoaded", () => {
         };
         ipcRenderer.sendToHost(R2_EVENT_FXL_CONFIGURE, payload);
     } else {
+        win.READIUM2.fxlViewportWidth = 0;
+        win.READIUM2.fxlViewportHeight = 0;
+        win.READIUM2.fxlViewportScale = 1;
+        win.READIUM2.fxlViewportTX = 0;
+        win.READIUM2.fxlViewportTY = 0;
+
         const payload: IEventPayload_R2_EVENT_FXL_CONFIGURE = {
             fxl: null,
         };
@@ -3462,12 +3471,13 @@ function loaded(forced: boolean) {
             return;
         }
 
+        debug("configureFixedLayout...(R2_EVENT_WINDOW_RESIZE)", win.innerWidth, win.innerHeight);
         const wh = configureFixedLayout(win.document, win.READIUM2.isFixedLayout,
             win.READIUM2.fxlViewportWidth, win.READIUM2.fxlViewportHeight,
             win.innerWidth, win.innerHeight, win.READIUM2.webViewSlot,
             win.READIUM2.fxlZoomPercent);
 
-        if (wh) {
+        if (wh && win.READIUM2.isFixedLayout) { // win.READIUM2.isFixedLayout always true here
             win.READIUM2.fxlViewportWidth = wh.width;
             win.READIUM2.fxlViewportHeight = wh.height;
             win.READIUM2.fxlViewportScale = wh.scale;
@@ -3479,6 +3489,12 @@ function loaded(forced: boolean) {
             };
             ipcRenderer.sendToHost(R2_EVENT_FXL_CONFIGURE, payload);
         } else {
+            win.READIUM2.fxlViewportWidth = 0;
+            win.READIUM2.fxlViewportHeight = 0;
+            win.READIUM2.fxlViewportScale = 1;
+            win.READIUM2.fxlViewportTX = 0;
+            win.READIUM2.fxlViewportTY = 0;
+
             const payload: IEventPayload_R2_EVENT_FXL_CONFIGURE = {
                 fxl: null,
             };

src/r2-xxx-js/r2-navigator-js/electron/renderer/webview/state.ts
@@ -99,6 +99,8 @@ export interface IReadiumElectronWebviewState {
     fxlViewportScale?: number;
     fxlViewportTX?: number;
     fxlViewportTY?: number;
+
+    webViewSlot?: WebViewSlotEnum;
 }
 export interface IReadiumElectronWebview extends Electron.WebviewTag {
     READIUM2: IReadiumElectronWebviewState;