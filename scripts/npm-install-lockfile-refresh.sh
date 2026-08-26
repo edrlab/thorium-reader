11495fd085c5dae70abcdea7f7b593e3d4b0d5ec
Daniel Weck <daniel.weck@gmail.com>
2026-08-26 18:50:13 +0100

    added PAC proxy agent NPM package

--- scripts/npm-install-lockfile-refresh.sh
+++ scripts/npm-install-lockfile-refresh.sh
@@ -15,3 +15,5 @@ npm outdated
 
 cd node_modules/electron && DEBUG=@electron/get* force_no_cache=true node install.js && cd -
 #cd node_modules/fsevents && DEBUG=@electron/get* force_no_cache=true node install.js && cd -
+
+# set -xv ; container --version ; container system stop ; container system start ; container system status ; container stop test-container ; container rm --force test-container ; container prune ; container list --all ; container run --cpus 4 --memory 2g --platform linux/arm64 --name test-container --volume ${PWD}:/MOUNT -w /MOUNT registry.access.redhat.com/hi/nodejs:latest sh -c 'set -xv ; node --version ; npm --version ; echo $PATH ; env ; npm install --global npm ; ls -als /usr/local/lib/node_modules ; export NODE_PATH=/usr/local/lib/node_modules ; export PATH="/usr/loca/bin:$PATH" ; echo $PATH ; npm --version ; npm config get cache ; npm config get prefix ; npm install --ignore-scripts --foreground-scripts --min-release-age=3 --allow-git=root pac-proxy-agent' ; container list --all ; container stop test-container ; container rm --force test-container ; container prune ; container system status ; container system stop ; set +xv
